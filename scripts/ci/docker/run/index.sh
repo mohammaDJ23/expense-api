9ba3fa942e17a372d5038d5c8348b5f02088b7fa
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2026-01-26 14:28:42 +0330

    config: 786383 read the variables from vars script

--- scripts/ci/docker/run/index.sh
+++ scripts/ci/docker/run/index.sh
@@ -2,10 +2,10 @@
 
 set -euo pipefail
 
-export COMPOSE_FILE="${COMPOSE_FILE:-docker-compose.swarm.ci.yml}"
-export STACK_NAME="${STACK_NAME:-expense-api-ci}"
-export IMAGE_NAME="${IMAGE_NAME:-expense-api-ci:latest}"
-export SECRETS_DIR="${SECRETS_DIR:-./.ci.secrets}"
-export ENVIRONMENT="${ENVIRONMENT:-production}"
+export COMPOSE_FILE="${COMPOSE_FILE}"
+export STACK_NAME="${STACK_NAME}"
+export IMAGE_NAME="${IMAGE_NAME}"
+export SECRETS_DIR="${SECRETS_DIR}"
+export ENVIRONMENT="${ENVIRONMENT}"
 
 source /usr/local/bin/start.sh
