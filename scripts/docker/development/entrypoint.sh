dc27e74a3113b34805d675c9f57421965e36c19f
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2026-01-26 13:47:32 +0330

    config: 786383 add the start script for docker dev

--- scripts/docker/development/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-docker-compose -f docker-compose.development.yml up --build --watch
---
+++ scripts/docker/development/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euo pipefail
+
+export COMPOSE_FILE="${COMPOSE_FILE:-docker-compose.swarm.development.yml}"
+export STACK_NAME="${STACK_NAME:-expense-api-development}"
+export IMAGE_NAME="${IMAGE_NAME:-expense-api-development:latest}"
+export SECRETS_DIR="${SECRETS_DIR:-./secrets}"
+export WATCH_MODE="${WATCH_MODE:-true}"
+export ENVIRONMENT="${ENVIRONMENT:-development}"
+
+source ./scripts/common/startDockerSwarm.sh
