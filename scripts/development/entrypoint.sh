5903a964cbfc1b6c27e018e4c610d13d7b1309d0
mohammaDJ23 <mohammad.ha9bi8@gmail.com>
2026-02-01 14:30:59 +0330

    chore: 326422 use local vars for docker

--- scripts/development/entrypoint.sh
+++ scripts/development/entrypoint.sh
@@ -2,25 +2,31 @@
 
 set -eu
 
+APP_NAME=$(source ./scripts/common/appName.sh)
+HASH=$(source ./scripts/common/createHash.sh)
+
 SECRETS=""
 
 if [ -d "./secrets" ]; then
-  for file in ./secrets/*.txt; do
-    [ -f "$file" ] || continue
-    key=$(basename "$file" .txt)
-    value=$(cat "$file" | tr -d '\n\r')
-    
-    if [ -n "$SECRETS" ]; then
-      SECRETS="${SECRETS};${key}=${value}"
-    else
-      SECRETS="${key}=${value}"
-    fi
-  done
+    for file in ./secrets/*.txt; do
+        [ -f "${file}" ] || continue
+        key=$(basename "${file}" .txt)
+        value=$(cat "${file}" | tr -d '\n\r')
+        
+        if [ -n "${SECRETS}" ]; then
+            SECRETS="${SECRETS};${key}=${value}"
+        else
+            SECRETS="${key}=${value}"
+        fi
+    done
 fi
 
-export ENVIRONMENT="development"
 export MODE="development"
+export ENVIRONMENT="development"
+export COMPOSE_FILE="docker-compose.swarm.${ENVIRONMENT}.yml"
+export STACK_NAME="${APP_NAME}-${ENVIRONMENT}"
+export SERVICE_NAME="${APP_NAME}-${ENVIRONMENT}_${APP_NAME}"
+export IMAGE_NAME="docker-username-development/${APP_NAME}-${ENVIRONMENT}:${HASH}"
 export SECRETS="${SECRETS}"
-export CLEANUP_ON_SUCCESS="false"
 
-source ./scripts/common/entrypoint.sh
+source ./scripts/common/docker/entrypoint.sh
