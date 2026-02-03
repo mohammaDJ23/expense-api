60e52340085222c8ff9d021769474ad66fb089d0
mohammad nowresideh <mohammad.ha9bi8@gmail.com>
2026-02-03 21:26:43 +0330

    chore: 782129 add some global constants and utils

---
+++ src/common/constants/index.ts
@@ -0,0 +1,10 @@
+export const PORT = 4000;
+
+export const DATABASE_NAME = 'postgres';
+export const DATABASE_PORT = 5432;
+
+export const NESTJS_WEBSITE_NAME = 'nestjs-website';
+
+export const REDIS_NAME = 'redis';
+export const REDIS_PORT = 6374;
+export const REDIS_DB = 0;
