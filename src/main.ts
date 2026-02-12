03c6bc039956c2881a23c393e09f34ab5fc33bd2
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-02-12 17:45:23 +0330

    chore: 782129 enable the versionning

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
-
-const PORT = 4000;
+import { PORT } from './common/constants';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.enableVersioning({
+        type: VersioningType.URI,
+        prefix: 'v',
+    });
+
     await app.listen(process.env.PORT ?? PORT);
 }
 bootstrap();
