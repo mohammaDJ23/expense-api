21d0dd49af8cdb8fec7668ac63718892b53d4186
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-02-12 17:16:35 +0330

    chore: 782129 use 6379 for the redis port

--- src/common/constants/index.ts
+++ src/common/constants/index.ts
@@ -6,5 +6,5 @@ export const DATABASE_PORT = 5432;
 export const NESTJS_WEBSITE_NAME = 'nestjs-website';
 
 export const REDIS_NAME = 'redis';
-export const REDIS_PORT = 6374;
+export const REDIS_PORT = 6379;
 export const REDIS_DB = 0;
