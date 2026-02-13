46b426c1220c644509245510ff245330fc3ba098
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-02-13 21:47:35 +0330

    chore: 782129 remove the extra vars

--- scripts/common/docker/vars.sh
+++ scripts/common/docker/vars.sh
@@ -4,12 +4,10 @@ set -euo pipefail
 
 init_vars() {
     readonly MODE="${INPUT_MODE:-${MODE:-}}"
-    readonly ENVIRONMENT="${INPUT_ENVIRONMENT:-${ENVIRONMENT:-}}"
     readonly COMPOSE_FILE="${INPUT_COMPOSE_FILE:-${COMPOSE_FILE:-}}"
     readonly IMAGE_NAME="${INPUT_IMAGE_NAME:-${IMAGE_NAME:-}}"
 
     export IMAGE_NAME
-    export ENVIRONMENT
 
     return 0
 }
