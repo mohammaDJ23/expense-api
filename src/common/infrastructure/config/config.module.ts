803fb7ea2946c6d10d2214a6b15e5e344f3bdd15
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-02-13 15:18:39 +0330

    chore: 782129 add ignoreEnvFile option

--- src/common/infrastructure/config/config.module.ts
+++ src/common/infrastructure/config/config.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule as CM } from '@nestjs/config';
 
 @Module({
-    imports: [CM.forRoot({ isGlobal: true })],
+    imports: [CM.forRoot({ isGlobal: true, ignoreEnvFile: true })],
 })
 export class ConfigModule {}
