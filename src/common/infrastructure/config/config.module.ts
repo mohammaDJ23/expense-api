0816f0ac0733e0f3edacb95b2bf0b346bdff92ed
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-02-13 11:51:18 +0330

    chore: 782129 remove the env path

--- src/common/infrastructure/config/config.module.ts
+++ src/common/infrastructure/config/config.module.ts
@@ -1,21 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule as CM } from '@nestjs/config';
 
-import { isDevelopment, isProduction } from 'src/common/utils/environments.util';
-
-function getEnvFilePath(): string {
-    let envFilePath = '';
-
-    if (isDevelopment()) {
-        envFilePath = '.env';
-    } else if (isProduction()) {
-        envFilePath = '.env.production';
-    }
-
-    return envFilePath;
-}
-
 @Module({
-    imports: [CM.forRoot({ envFilePath: getEnvFilePath(), isGlobal: true })],
+    imports: [CM.forRoot({ isGlobal: true })],
 })
 export class ConfigModule {}
