56ad33828fc7bdca070a9ad0ea45f5d6c10dab75
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-02-14 20:54:55 +0330

    chore: 782129 add an step to use the images inside github output

--- scripts/common/docker/vars.sh
+++ scripts/common/docker/vars.sh
@@ -6,6 +6,7 @@ init_vars() {
     readonly MODE="${INPUT_MODE:-${MODE:-}}"
     readonly COMPOSE_FILE="${INPUT_COMPOSE_FILE:-${COMPOSE_FILE:-}}"
     readonly IMAGE_NAME="${INPUT_IMAGE_NAME:-${IMAGE_NAME:-}}"
+    LATEST_IMAGE=""
 
     export IMAGE_NAME
 
