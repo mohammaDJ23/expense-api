f67b1bb211f0a1dc55a59fa8c711a30964219d43
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-02-17 23:46:17 +0330

    chore: 880392 move the docker scripts to the docker folder

--- scripts/ci/entrypoint.ci.production.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-MODE="ci_production"
-COMPOSE_FILE="docker-compose.ci.production.yml"
-
-source ./scripts/common/docker/createImageName.sh
-source ./scripts/common/docker/entrypoint.sh
