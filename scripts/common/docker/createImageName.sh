441aa157e1e6850b2c0069301a98901db7e4941e
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-02-18 00:40:16 +0330

    chore: 880392 add some logs for creating the image

--- scripts/common/docker/createImageName.sh
+++ scripts/common/docker/createImageName.sh
@@ -7,11 +7,17 @@ source ./scripts/common/logs.sh
 create_image_name() {
     local APP_NAME=$(source ./scripts/common/appName.sh)
 
+    log_info "Create the image name..." >&2
+
     local ENVIRONMENT="${ENVIRONMENT:?ERROR ENVIRONMENT is required as env}"
     local DOCKER_USERNAME="${DOCKER_USERNAME:?ERROR DOCKER_USERNAME is required as env}"
     local TAG="${TAG:-latest}"
     
-    echo "${DOCKER_USERNAME}/${APP_NAME}-${ENVIRONMENT}:${TAG}"
+    local IMAGE_NAME="${DOCKER_USERNAME}/${APP_NAME}-${ENVIRONMENT}:${TAG}"
+
+    log_success "The image name create: ${IMAGE_NAME}" >&2
+
+    echo "${IMAGE_NAME}"
 }
 
 IMAGE_NAME=$(create_image_name)
