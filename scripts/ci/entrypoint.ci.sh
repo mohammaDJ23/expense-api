70e5eb6eabb444c2e17f66342283c7d299b6bffe
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-02-17 22:28:07 +0330

    chore: 880392 remove the extra variables

--- scripts/ci/entrypoint.ci.sh
+++ scripts/ci/entrypoint.ci.sh
@@ -2,12 +2,8 @@
 
 set -euo pipefail
 
-APP_NAME=$(source ./scripts/common/appName.sh)
-
-MODE="${MODE:-ci}"
-DOCKER_USERNAME="${DOCKER_USERNAME:-docker-username}"
-TAG="${TAG:-latest}"
+MODE="ci"
 COMPOSE_FILE="docker-compose.${MODE}.yml"
-export IMAGE_NAME="${DOCKER_USERNAME}/${APP_NAME}-${MODE}:${TAG}"
 
+source ./scripts/common/docker/createImageName.sh
 source ./scripts/common/docker/entrypoint.sh
--- scripts/production/entrypoint.sh
+++ scripts/production/entrypoint.sh
@@ -2,12 +2,8 @@
 
 set -euo pipefail
 
-APP_NAME=$(source ./scripts/common/appName.sh)
-
-MODE="${MODE:-production}"
-DOCKER_USERNAME="${DOCKER_USERNAME:?ERROR DOCKER_USERNAME is required as an env}"
-TAG="${TAG:-latest}"
+MODE="production"
 COMPOSE_FILE="docker-compose.${MODE}.yml"
-export IMAGE_NAME="${DOCKER_USERNAME}/${APP_NAME}-${MODE}:${TAG}"
 
+source ./scripts/common/docker/createImageName.sh
 source ./scripts/common/docker/entrypoint.sh
