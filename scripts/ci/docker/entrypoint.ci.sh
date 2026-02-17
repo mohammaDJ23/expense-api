d3f317a03b72b573cc7a14c85a721126aa3277c5
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-02-18 00:08:47 +0330

    fix: 880392 fix the scripts name

--- scripts/ci/docker/entrypoint.ci.sh
+++ scripts/ci/docker/entrypoint.ci.sh
@@ -6,5 +6,5 @@ MODE="ci"
 COMPOSE_FILE="docker-compose.${MODE}.yml"
 
 source ./scripts/common/docker/createImageName.sh
-source ./scrits/common/docker/setImageNameOutput.sh
+source ./scripts/common/docker/setImageNameOutput.sh
 source ./scripts/common/docker/entrypoint.sh
