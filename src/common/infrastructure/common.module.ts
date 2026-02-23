c5af38e890d6b24e5c1f0baf00c718c30ff8f7bc
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-02-23 14:44:56 +0330

    fix: 125823 fix the modules imported from the related pacakges

--- src/common/infrastructure/common.module.ts
+++ src/common/infrastructure/common.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { SwaggerModule } from '@nestjs/swagger';
-import { RedisModule } from '@liaoliaots/nestjs-redis';
 
+import { ConfigModule } from '@/common/infrastructure/config/config.module';
 import { DatabaseModule } from '@/common/infrastructure/database/database.module';
 import { ExceptionModule } from '@/common/infrastructure/exception/exception.module';
+import { RedisModule } from '@/common/infrastructure/redis/redis.module';
+import { SwaggerModule } from '@/common/infrastructure/swagger/swagger.module';
 import { VersionModule } from '@/common/infrastructure/version/version.module';
 
 @Module({
