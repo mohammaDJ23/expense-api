40f42882d14bf6f936f53cf2ca5d0419175bfb9a
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-02-26 17:26:17 +0330

    fix: 393315 rename HealthService

--- src/modules/health/interfaces/controllers/v1.controller.ts
+++ src/modules/health/interfaces/controllers/v1.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 
-import { HealthSerice } from '@/modules/health/applications/services/health.service';
+import { HealthService } from '@/modules/health/applications/services/health.service';
 
 import type { HealthCheckResult } from '@nestjs/terminus';
 
 @Controller({ version: '1', path: 'api/health' })
 export class HealthController {
-    constructor(private readonly healthService: HealthSerice) {}
+    constructor(private readonly healthService: HealthService) {}
 
     @Get()
     getHealth(): Promise<HealthCheckResult> {
