9e08bfaeb3d07a61a26b606d319adbebc727c75c
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-02-27 08:42:45 +0330

    chore: 271190 export the version module and the provider name

--- src/common/infrastructure/version/version.module.ts
+++ src/common/infrastructure/version/version.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 
+import { VERSION_PROVIDER } from '@/common/constants/version.constants';
+
 import { VersionProvider } from './version.provider';
 
 @Module({
     providers: [VersionProvider],
+    exports: [VersionModule, VERSION_PROVIDER],
 })
 export class VersionModule {}
