26fff559e6a5580aa21c4af61f92741f14d3f870
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-05-04 22:28:57 +0330

    chore: 875092 use the response entity in the exception

--- src/common/infrastructure/exception/hostHandler.interface.ts
+++ src/common/infrastructure/exception/hostHandler.interface.ts
@@ -1,7 +1,8 @@
+import type { ResponseEntity } from '@/common/kernel/entities/response.entity';
 import type { IAppException } from '@/common/kernel/exceptions/app/exception.interface';
 import type { ArgumentsHost } from '@nestjs/common';
 
 export interface IHostHandler {
     canHandle: (host: ArgumentsHost) => boolean;
-    send: (host: ArgumentsHost, response: IAppException) => void;
+    send: (host: ArgumentsHost, response: ResponseEntity<IAppException>) => void;
 }
