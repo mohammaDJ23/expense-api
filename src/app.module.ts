7eefa2c0ae923f4e4c2bab1dc763943c35196b8e
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-05-04 16:50:39 +0330

    feature: 875092 add the user entity to the user module and add the user module to the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 
 import { CommonModule } from '@/common/infrastructure/common.module';
 import { HealthModule } from '@/modules/health/health.module';
+import { UserModule } from '@/modules/user/user.module';
 
 @Module({
-    imports: [CommonModule, HealthModule],
+    imports: [CommonModule, HealthModule, UserModule],
 })
 export class AppModule {}
---
+++ src/modules/user/user.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { UserEntity } from '@/modules/user/infrastructure/entities/user.entity';
+
+@Module({
+    imports: [TypeOrmModule.forFeature([UserEntity])],
+})
+export class UserModule {}
