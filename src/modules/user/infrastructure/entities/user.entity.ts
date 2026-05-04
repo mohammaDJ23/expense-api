285fcc8c20465edbd12d66d98fa36a00bf0bff85
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-05-04 16:50:39 +0330

    feature: 875092 add the user entity to the user module and add the user module to the app module

--- src/modules/user/infrastructure/entities/user.entity.ts
+++ src/modules/user/infrastructure/entities/user.entity.ts
@@ -11,7 +11,7 @@ import { UserRoles } from '@/modules/user/domain/enums/userRoles.enum';
 import type { IUser } from '@/modules/user/domain/interfaces/user.interface';
 
 @Entity('users')
-export class User implements IUser {
+export class UserEntity implements IUser {
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
