e04a93d9da6ae2b9867662b6204c312cf238d017
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-05-04 18:07:31 +0330

    feature: 875092 add the user orm entity and repository

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { UserEntity } from '@/modules/user/infrastructure/entities/user.entity';
+import { UserOrmEntity } from '@/modules/user/infrastructure/entities/user.orm.entity';
+import { UserRepository } from '@/modules/user/infrastructure/repositories/user.repository';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([UserEntity])],
+    imports: [TypeOrmModule.forFeature([UserOrmEntity])],
+    providers: [UserRepository],
 })
 export class UserModule {}
