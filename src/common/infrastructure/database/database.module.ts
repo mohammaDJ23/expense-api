b6c0485fec2b971f7bedfdc348ba774428afbe33
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-05-07 01:59:53 +0330

    feature: 875092 add the extensions of queryBuilder

--- src/common/infrastructure/database/database.module.ts
+++ src/common/infrastructure/database/database.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import '@/common/infrastructure/database/typeorm/queryBuilder.extension';
+
 import { DatabaseConfigService } from './databaseConfig.service';
 
 @Module({
