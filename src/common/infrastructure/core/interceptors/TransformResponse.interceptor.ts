0453f90626b650f7e15b3f09b25b88d995c9cf76
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-05-06 06:29:04 +0330

    chore: 875092 simplify the response entity dasta

--- src/common/infrastructure/core/interceptors/TransformResponse.interceptor.ts
+++ src/common/infrastructure/core/interceptors/TransformResponse.interceptor.ts
@@ -1,5 +1,4 @@
 import {
-    BadRequestException,
     HttpStatus,
     Injectable,
     type CallHandler,
@@ -24,56 +23,25 @@ export class TransformResponseInterceptor<T> implements NestInterceptor<T, Respo
 
     intercept(context: ExecutionContext, next: CallHandler): Observable<ResponseEntity<T>> {
         const handler = context.getHandler();
-        const controller = context.getClass();
 
-        const controllerName = controller.name;
-        const handlerName = handler.name;
-
-        const message = this.getMessage(controllerName, handlerName, handler);
-        const statusCode = this.getStatusCode(controllerName, handlerName, handler);
-
-        // eslint-disable-next-line import-x/no-deprecated
-        return next.handle().pipe(map(this.transformResponse(message, statusCode)));
-    }
-
-    private getMessage(controllerName: string, handlerName: string, handler: Function): string {
         const message = this.reflector.get<string | undefined, string>(
             RESPONSE_MESSAGE_METADATA_KEY,
             handler,
         );
-        if (!message) {
-            const errorMessage = `Missing @ResponseMessage() decorator or empty mesasge on ${controllerName}.${handlerName}() route. Please add @ResponseMessage('Your message') to this endpoint.`;
-            throw new BadRequestException(errorMessage);
-        }
-        return message;
-    }
-
-    private getStatusCode(
-        controllerName: string,
-        handlerName: string,
-        handler: Function,
-    ): HttpStatus {
         const statusCode = this.reflector.get<HttpStatus | undefined, string>(
             RESPONSE_STATUS_METADATA_KEY,
             handler,
         );
-        if (!statusCode) {
-            const errorMessage = `Missing @ResponseStatus() decorator or empty status code on ${controllerName}.${handlerName}() route. Please add @ResponseStatus(HttpStatus.XXX) to this endpoint.`;
-            throw new BadRequestException(errorMessage);
-        }
-        return statusCode;
-    }
 
-    private transformResponse(
-        message: string,
-        statusCode: HttpStatus,
-    ): (data: T) => ResponseEntity<T> {
-        return function response(data): ResponseEntity<T> {
-            return ResponseEntity.success({
-                message,
-                statusCode,
-                data,
-            });
-        };
+        return next.handle().pipe(
+            // eslint-disable-next-line import-x/no-deprecated
+            map((data): ResponseEntity<T> => {
+                return ResponseEntity.success({
+                    message,
+                    statusCode,
+                    data,
+                });
+            }),
+        );
     }
 }
