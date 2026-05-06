1f7965027ad20c7f94fb976a676dba43f9bb5166
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-05-06 22:04:54 +0330

    chore: 875092 remove the transforming validatorPipe

--- src/common/infrastructure/core/core.module.ts
+++ src/common/infrastructure/core/core.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 
 import { FallbackHostHandler } from '@/common/infrastructure/core/filters/globalException/fallbackHost.handler';
 import { GlobalExceptionFilter } from '@/common/infrastructure/core/filters/globalException/globalException.filter';
 import { HttpHostHandler } from '@/common/infrastructure/core/filters/globalException/httpHost.handler';
 import { TransformResponseInterceptor } from '@/common/infrastructure/core/interceptors/transformResponse.interceptor';
-import { TransformValidationResponsePipe } from '@/common/infrastructure/core/pipes/transformValidationResponse.pipe';
 
 @Module({
     providers: [
@@ -18,10 +17,6 @@ import { TransformValidationResponsePipe } from '@/common/infrastructure/core/pi
         {
             provide: APP_INTERCEPTOR,
             useClass: TransformResponseInterceptor,
-        },
-        {
-            provide: APP_PIPE,
-            useClass: TransformValidationResponsePipe,
         },
     ],
 })
