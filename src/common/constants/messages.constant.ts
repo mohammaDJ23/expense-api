7c8eb05219f12f2288564b3973394c9b94e5d912
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-05-06 21:16:28 +0330

    chore: 875092 add VALIDATION_FAILED_MESSAGE

--- src/common/constants/messages.constant.ts
+++ src/common/constants/messages.constant.ts
@@ -1,3 +1,4 @@
 export const INTERNAL_SERVER_ERROR_MESSAGE = 'Internal server error';
 export const SUCCESS_MESSAGE = 'The operation completed successfully';
 export const ERROR_MESSAGE = 'The operation was not completed successfully';
+export const VALIDATION_FAILED_MESSAGE = 'Validation failed';
