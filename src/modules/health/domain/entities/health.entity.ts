6095d434b02313d3181c74cda9a844242500c02e
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-05-06 06:12:29 +0330

    chore: 875092 add the health entity interface

--- src/modules/health/domain/entities/health.entity.ts
+++ src/modules/health/domain/entities/health.entity.ts
@@ -1,24 +1,32 @@
 import { getCurrentUTCTimestamp } from '@/common/utils/getCurrentUTCTimestamp.util';
 
+import type { IHealth } from '@/modules/health/domain/interfaces/healthEntity.interface';
 import type { HealthIndicatorResult, HealthIndicatorStatus } from '@nestjs/terminus';
 
-export class HealthEntity {
-    private readonly timestamp: string;
+export class HealthEntity implements IHealth {
+    public readonly timestamp: string;
+    public readonly name: string;
+    public readonly status: HealthIndicatorStatus;
+    public readonly details: Record<string, unknown>;
 
-    private constructor(
-        private readonly name: string,
-        private readonly status: HealthIndicatorStatus,
-        private readonly details: Record<string, unknown> = {},
-    ) {
-        this.timestamp = getCurrentUTCTimestamp();
+    private constructor(data: IHealth) {
+        this.timestamp = data.timestamp;
+        this.name = data.name;
+        this.status = data.status;
+        this.details = data.details;
     }
 
     static create(
         name: string,
         status: HealthIndicatorStatus,
-        details?: Record<string, unknown>,
+        details: Record<string, unknown> = {},
     ): HealthEntity {
-        return new HealthEntity(name, status, details);
+        return new HealthEntity({
+            name,
+            status,
+            details,
+            timestamp: getCurrentUTCTimestamp(),
+        });
     }
 
     static up(name: string, details?: Record<string, unknown>): HealthEntity {
