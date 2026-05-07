aec7d849308f1686aff71541007ef92b0a1faf1e
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-05-07 04:27:50 +0330

    chore: 875092 rename passwordHashing to passwordHasher

--- src/modules/authentication/applications/commands/signup/signup.handler.ts
+++ src/modules/authentication/applications/commands/signup/signup.handler.ts
@@ -2,7 +2,7 @@ import { CommandBus, CommandHandler, type ICommandHandler } from '@nestjs/cqrs';
 
 import { SignupCommand } from '@/modules/authentication/applications/commands/signup/signup.command';
 import { CreateUserCommand } from '@/modules/user/applications/commands/createUser/createUser.command';
-import { PasswordHasherService } from '@/modules/user/applications/services/passwordHashing.service';
+import { PasswordHasherService } from '@/modules/user/applications/services/passwordHasher.service';
 
 import type { UserEntity } from '@/modules/user/domain/entities/user.entity';
 
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { PasswordHasherService } from '@/modules/user/applications/services/passwordHashing.service';
+import { PasswordHasherService } from '@/modules/user/applications/services/passwordHasher.service';
 import { UserOrmEntity } from '@/modules/user/infrastructure/entities/user.orm.entity';
 import { UserRepository } from '@/modules/user/infrastructure/repositories/user.repository';
 
