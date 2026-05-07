1536ee170b84b185ce3c68f032f8f89933ecd19f
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-05-07 08:03:29 +0330

    chore: 875092 add the core module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 
+import { CoreModule } from '@/core/core.module';
 import { InfrastructureModule } from '@/infrastructure/infrastructure.module';
 import { AuthenticationModule } from '@/modules/authentication/authentication.module';
 import { HealthModule } from '@/modules/health/health.module';
 import { UserModule } from '@/modules/user/user.module';
 
 @Module({
-    imports: [InfrastructureModule, HealthModule, AuthenticationModule, UserModule],
+    imports: [InfrastructureModule, CoreModule, HealthModule, AuthenticationModule, UserModule],
 })
 export class AppModule {}
