0b7bc5b796beade69b8fc3867ef99930b2e1b563
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-05-07 04:13:52 +0330

    feature: 875092 add PasswordHasherService

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { PasswordHasherService } from '@/modules/user/applications/services/passwordHashing.service';
 import { UserOrmEntity } from '@/modules/user/infrastructure/entities/user.orm.entity';
 import { UserRepository } from '@/modules/user/infrastructure/repositories/user.repository';
 
 @Module({
     imports: [TypeOrmModule.forFeature([UserOrmEntity])],
-    providers: [UserRepository],
+    providers: [UserRepository, PasswordHasherService],
+    exports: [PasswordHasherService],
 })
 export class UserModule {}
