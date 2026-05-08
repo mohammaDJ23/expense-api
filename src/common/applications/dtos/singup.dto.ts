ab94c313ca66da56bc9c6fdfd51d37b72507b2b9
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-05-09 00:29:35 +0330

    chore: 875092 add SignupDto

---
+++ src/common/applications/dtos/singup.dto.ts
@@ -0,0 +1,19 @@
+import { Matches, Length } from 'class-validator';
+
+export class SignupDto {
+    @Matches(
+        // eslint-disable-next-line security/detect-unsafe-regex, sonarjs/regex-complexity
+        /^[\w-]+(\.[\w-]+)*@([a-z0-9-]+(\.[a-z0-9-]+)*?\.[a-z]{2,6}|(\d{1,3}\.){3}\d{1,3})(:\d{4})?$/,
+        {
+            message: 'Invalid email',
+        },
+    )
+    email: string;
+
+    // eslint-disable-next-line sonarjs/concise-regex
+    @Matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,45}$/, {
+        message: 'The password should be strong',
+    })
+    @Length(6, 45)
+    password: string;
+}
