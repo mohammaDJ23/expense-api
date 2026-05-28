6466fd9ff6a4df785694b91c05f802ef3797b8ed
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-05-28 10:48:47 +0330

    chore: 875092 use toEntityOrThrow for the user creation

--- src/modules/user/infrastructure/repositories/user.repository.ts
+++ src/modules/user/infrastructure/repositories/user.repository.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { eq, sql } from 'drizzle-orm';
 
-import { isExists, toEntity } from '@/infrastructure/database/drizzle/drizzle.transformer';
+import { isExists, toEntityOrThrow } from '@/infrastructure/database/drizzle/drizzle.transformer';
 import { DrizzleClientService } from '@/infrastructure/database/drizzle/drizzleClient.service';
 import { users } from '@/modules/user/infrastructure/entities/user.orm.entity';
 
@@ -13,7 +13,10 @@ export class UserRepository implements IUserRepository {
     constructor(private readonly drizzleClientService: DrizzleClientService) {}
 
     create(data: UserEntity): Promise<UserEntity> {
-        return toEntity(this.drizzleClientService.db.insert(users).values(data).returning());
+        return toEntityOrThrow(
+            this.drizzleClientService.db.insert(users).values(data).returning(),
+            'Failed to create a new user',
+        );
     }
 
     isExistsByEmail(email: string): Promise<boolean> {
