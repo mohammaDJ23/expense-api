27a075d05c0dfc6d128445a3dae5881afd94df08
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-05-28 14:47:40 +0330

    fix: 875092 use actual SignupDto not as a type

--- src/modules/authentication/interface/controllers/v1.controller.ts
+++ src/modules/authentication/interface/controllers/v1.controller.ts
@@ -3,8 +3,8 @@ import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
 import { Response } from '@/core/decorators/Response.decorator';
 import { AuthenticationService } from '@/modules/authentication/applications/services/authentication.service';
 import { SUCCESS_SIGNUP_MESSAGE } from '@/modules/authentication/interface/constants/messages.constant';
+import { SignupDto } from '@/modules/authentication/interface/dtos/singup.dto';
 
-import type { SignupDto } from '@/modules/authentication/interface/dtos/singup.dto';
 import type { UserEntity } from '@/modules/user/domain/entities/user.entity';
 
 @Controller({ version: '1', path: 'api/authentication' })
