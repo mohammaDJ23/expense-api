3041b47e2d2cd2be1f401c42c0f95b430af4d511
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-02 02:06:51 +0330

    chore: 875092 use the TRequiredInsertUser type and ignore id when a new user wants to create

--- src/modules/authentication/applications/services/authentication.service.ts
+++ src/modules/authentication/applications/services/authentication.service.ts
@@ -5,7 +5,7 @@ import { CreateUserCommand } from '@/modules/user/applications/commands/createUs
 import { PasswordHasherService } from '@/modules/user/applications/services/passwordHasher.service';
 
 import type { SignupDto } from '@/modules/authentication/interface/dtos/signup.dto';
-import type { UserEntity } from '@/modules/user/domain/entities/user.entity';
+import type { TRequiredInsertUser } from '@/modules/user/infrastructure/schemas/user.schema';
 
 @Injectable()
 export class AuthenticationService {
@@ -14,7 +14,7 @@ export class AuthenticationService {
         private readonly passwordHasherService: PasswordHasherService,
     ) {}
 
-    async signup(data: SignupDto): Promise<UserEntity> {
+    async signup(data: SignupDto): Promise<TRequiredInsertUser> {
         const hashedPassword = await this.passwordHasherService.hash(data.password);
         const createUserCommand = new CreateUserCommand(data.email, hashedPassword);
         return this.commandBus.execute(createUserCommand);
--- src/modules/user/applications/commands/createUser/createUser.handler.ts
+++ src/modules/user/applications/commands/createUser/createUser.handler.ts
@@ -5,18 +5,18 @@ import { CreateUserCommand } from '@/modules/user/applications/commands/createUs
 import { UserEntity } from '@/modules/user/domain/entities/user.entity';
 import { UserRepository } from '@/modules/user/infrastructure/repositories/user.repository';
 
+import type { TRequiredInsertUser } from '@/modules/user/infrastructure/schemas/user.schema';
+
 @CommandHandler(CreateUserCommand)
 export class CreateUserHandler implements ICommandHandler<CreateUserCommand> {
     constructor(private readonly userRepository: UserRepository) {}
 
-    async execute(command: CreateUserCommand): Promise<UserEntity> {
+    async execute(command: CreateUserCommand): Promise<TRequiredInsertUser> {
         const isExists = await this.userRepository.isExistsByEmail(command.email);
-
         if (isExists) {
             throw new ConflictException('The Email already exists.');
         }
-
         const userEntity = UserEntity.create(command);
-        return this.userRepository.create(userEntity);
+        return this.userRepository.create(userEntity.toInsert());
     }
 }
