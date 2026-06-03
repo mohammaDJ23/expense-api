7208ef5fbbbc5ab87112fcf413ffd626ae58d99f
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-04 03:16:54 +0330

    chore: 875092 remove the password hasher service

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 
 import { CreateUserHandler } from '@/modules/user/applications/commands/createUser/createUser.handler';
-import { PasswordHasherService } from '@/modules/user/applications/services/passwordHasher.service';
 import { UserRepository } from '@/modules/user/infrastructure/repositories/user.repository';
 
 @Module({
     imports: [CqrsModule],
-    providers: [UserRepository, PasswordHasherService, CreateUserHandler],
-    exports: [PasswordHasherService, CqrsModule],
+    providers: [UserRepository, CreateUserHandler],
+    exports: [CqrsModule],
 })
 export class UserModule {}
