58d80cd352540fb8478404ba93e9f0ecdd7e5147
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-04 03:26:48 +0330

    chore: 875092 rename emailVerificationMailer and emailVerificationToken

--- src/modules/authentication/authentication.module.ts
+++ src/modules/authentication/authentication.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 
 import { AuthenticationService } from '@/modules/authentication/applications/services/authentication.service';
-import { EmailVerificationMailerService } from '@/modules/authentication/applications/services/EmailVerificationMailer.service';
-import { EmailVerificationTokenService } from '@/modules/authentication/applications/services/EmailVerificationToken.service';
+import { EmailVerificationMailerService } from '@/modules/authentication/applications/services/emailVerificationMailer.service';
+import { EmailVerificationTokenService } from '@/modules/authentication/applications/services/emailVerificationToken.service';
 import { PasswordHasherService } from '@/modules/authentication/applications/services/passwordHasher.service';
 import { AuthenticationController } from '@/modules/authentication/interface/controllers/v1.controller';
 import { UserModule } from '@/modules/user/user.module';
