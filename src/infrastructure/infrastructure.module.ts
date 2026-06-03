eda9eab1cee5551afcf9693e74167ed37666460b
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-04 03:07:19 +0330

    chore: 875092 add the jwt and mailer module and services

--- src/infrastructure/infrastructure.module.ts
+++ src/infrastructure/infrastructure.module.ts
@@ -4,6 +4,8 @@ import { ApiVersioningModule } from '@/infrastructure/apiVersioning/apiVersionin
 import { AppInstanceModule } from '@/infrastructure/appInstance/appInstance.module';
 import { ConfigModule } from '@/infrastructure/config/config.module';
 import { DatabaseModule } from '@/infrastructure/database/database.module';
+import { JwtModule } from '@/infrastructure/jwt/jwt.module';
+import { MailerModule } from '@/infrastructure/mailer/mailer.module';
 import { LoggerModule } from '@/infrastructure/pino/pino.module';
 import { RedisModule } from '@/infrastructure/redis/redis.module';
 import { SwaggerModule } from '@/infrastructure/swagger/swagger.module';
@@ -21,6 +23,8 @@ import { VersionModule } from '@/infrastructure/version/version.module';
         ThrottlerModule,
         LoggerModule,
         ApiVersioningModule,
+        MailerModule,
+        JwtModule,
     ],
 })
 export class InfrastructureModule {}
