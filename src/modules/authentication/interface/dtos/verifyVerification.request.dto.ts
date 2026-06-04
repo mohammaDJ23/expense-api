5ed90a710097c40958d273920d891d33d096efcb
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-04 18:03:54 +0330

    chore: 875092 split the request and response dtos

--- src/modules/authentication/interface/dtos/verifyVerification.request.dto.ts
+++ src/modules/authentication/interface/dtos/verifyVerification.request.dto.ts
@@ -1,6 +1,6 @@
 import { IsJWT, IsNotEmpty } from 'class-validator';
 
-export class VerifyVerificationDto {
+export class VerifyVerificationRequestDto {
     @IsJWT()
     @IsNotEmpty()
     token: string;
