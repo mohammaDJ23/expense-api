5fd292d4ef5876073cd2116f37a40e1a9ba70b61
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-04 11:05:42 +0330

    feature: 875092 add the sending email verification token route and the related services

---
+++ src/modules/authentication/interface/dtos/emailVerificationToken.dto.ts
@@ -0,0 +1,12 @@
+import { Matches } from 'class-validator';
+
+export class EmailVerificationTokenDto {
+    @Matches(
+        // eslint-disable-next-line security/detect-unsafe-regex, sonarjs/regex-complexity
+        /^[\w-]+(\.[\w-]+)*@([a-z0-9-]+(\.[a-z0-9-]+)*?\.[a-z]{2,6}|(\d{1,3}\.){3}\d{1,3})(:\d{4})?$/,
+        {
+            message: 'Invalid email',
+        },
+    )
+    email: string;
+}
