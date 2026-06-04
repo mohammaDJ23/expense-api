0133b5010696f8ffebd7267647199a3f540c7e51
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-04 11:03:21 +0330

    feature: 875092 add the getUserByEmail query

--- src/modules/user/domain/interfaces/userRepository.interface.ts
+++ src/modules/user/domain/interfaces/userRepository.interface.ts
@@ -1,7 +1,12 @@
 import type { UserEntity } from '@/modules/user/domain/entities/user.entity';
-import type { TRequiredInsertUser } from '@/modules/user/infrastructure/schemas/user.schema';
+import type {
+    TRequiredInsertUser,
+    TSelectUser,
+} from '@/modules/user/infrastructure/schemas/user.schema';
 
 export interface IUserRepository {
     create(data: Omit<UserEntity, 'id'>): Promise<TRequiredInsertUser>;
     isExistsByEmail(email: string): Promise<boolean>;
+    getByEmailOrThrow(email: string): Promise<TSelectUser>;
+    getByEmail(email: string): Promise<TSelectUser | null>;
 }
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 
 import { CreateUserHandler } from '@/modules/user/applications/commands/createUser/createUser.handler';
+import { GetUserByEmailHandler } from '@/modules/user/applications/queries/getUserByEmail/getUserByEmail.handler';
 import { UserRepository } from '@/modules/user/infrastructure/repositories/user.repository';
 
 @Module({
     imports: [CqrsModule],
-    providers: [UserRepository, CreateUserHandler],
+    providers: [UserRepository, CreateUserHandler, GetUserByEmailHandler],
     exports: [CqrsModule],
 })
 export class UserModule {}
