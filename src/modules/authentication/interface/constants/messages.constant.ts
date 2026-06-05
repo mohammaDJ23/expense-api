b1d0c0d948f25d4a33331e863026375c093f43e8
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-05 18:54:34 +0330

    fix: 875092 do not expose the not found user error in the forgot password service

--- src/modules/authentication/interface/constants/messages.constant.ts
+++ src/modules/authentication/interface/constants/messages.constant.ts
@@ -2,8 +2,9 @@ export const SUCCESS_SIGNUP_MESSAGE =
     'Your account has been registered and we sent a verification link to your email account please verify it';
 export const SUCCESS_LOGIN_MESSAGE = 'Welcome back! You have successfully logged in';
 export const SUCCESS_SEND_VERIFICATION_MESSAGE =
-    'If your account exists, the email verification token will be send to your email account';
-export const SUCCESS_VERIFY_VERIFICATION_MESSAGE = 'If your account exists, we verify it';
+    'If your account exists and not verified, the email verification token will be send to your email account';
+export const SUCCESS_VERIFY_VERIFICATION_MESSAGE =
+    'If your account exists and not verified, we verify it';
 export const SUCCESS_FORGOT_PASSWORD_MESSAGE =
     // eslint-disable-next-line sonarjs/no-hardcoded-passwords
-    'Please check your email account for the further process';
+    'If your account exists and verified, we send a link to your email account';
