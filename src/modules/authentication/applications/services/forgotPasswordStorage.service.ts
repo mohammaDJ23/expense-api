3f515ca59a6bee893f54fc1783546b5e08396072
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-06 02:07:41 +0330

    chore: 875092 add the forgot password storage

---
+++ src/modules/authentication/applications/services/forgotPasswordStorage.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { RedisService } from '@liaoliaots/nestjs-redis';
+
+@Injectable()
+export class ForgotPasswordStorageService {
+    private readonly prefixKey = 'forgot-password';
+
+    constructor(private readonly redisService: RedisService) {}
+
+    async set(key: string, value: string): Promise<void> {
+        const redis = this.redisService.getOrThrow();
+        await redis.setex(
+            `${this.prefixKey}:${key}`,
+            // this time should be syncing with the token expiration time
+            1 * 60 * 10,
+            value,
+        );
+    }
+
+    get(key: string): Promise<string | null> {
+        const redis = this.redisService.getOrThrow();
+        return redis.get(`${this.prefixKey}:${key}`);
+    }
+
+    async delete(key: string): Promise<void> {
+        const redis = this.redisService.getOrThrow();
+        await redis.del(`${this.prefixKey}:${key}`);
+    }
+}
