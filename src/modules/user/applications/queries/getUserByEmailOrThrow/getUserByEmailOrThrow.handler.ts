764a62e841c78b662d24307aec2ab13ab57773dc
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-05 15:36:14 +0330

    feature: 875092 add getUserByEmailOrThrowQuery

--- src/modules/user/applications/queries/getUserByEmailOrThrow/getUserByEmailOrThrow.handler.ts
+++ src/modules/user/applications/queries/getUserByEmailOrThrow/getUserByEmailOrThrow.handler.ts
@@ -1,15 +1,15 @@
 import { QueryHandler, type IQueryHandler } from '@nestjs/cqrs';
 
-import { GetUserByEmailQuery } from '@/modules/user/applications/queries/getUserByEmail/getUserByEmail.query';
+import { GetUserByEmailOrThrowQuery } from '@/modules/user/applications/queries/getUserByEmailOrThrow/getUserByEmailOrThrow.query';
 import { UserRepository } from '@/modules/user/infrastructure/repositories/user.repository';
 
 import type { TSelectUser } from '@/modules/user/infrastructure/schemas/user.schema';
 
-@QueryHandler(GetUserByEmailQuery)
-export class GetUserByEmailHandler implements IQueryHandler<GetUserByEmailQuery> {
+@QueryHandler(GetUserByEmailOrThrowQuery)
+export class GetUserByEmailOrThrowHandler implements IQueryHandler<GetUserByEmailOrThrowQuery> {
     constructor(private readonly userRepository: UserRepository) {}
 
-    execute(query: GetUserByEmailQuery): Promise<TSelectUser> {
+    execute(query: GetUserByEmailOrThrowQuery): Promise<TSelectUser> {
         return this.userRepository.getByEmailOrThrow(query.email);
     }
 }
