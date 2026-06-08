ef206f0c832b5bc3298d9d465390d26845826aa1
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-08 19:06:45 +0330

    chore: 875092 conver to object type

--- src/common/utils/omitUndefined.util.ts
+++ src/common/utils/omitUndefined.util.ts
@@ -1,6 +1,6 @@
-export function omitUndefined<T extends Record<string, unknown>>(source: T): Partial<T> {
+export function omitUndefined<T extends object>(source: T): T {
     return Object.fromEntries(
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         Object.entries(source).filter(([_, value]) => value !== undefined),
-    ) as Partial<T>;
+    ) as T;
 }
