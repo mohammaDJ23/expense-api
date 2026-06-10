4e82e9cd7a81b299ec22063b40c3bdb98a749a44
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-11 00:00:46 +0330

    chore: 785298 use the cqrs module and drizzle repository

--- src/modules/health/health.module.ts
+++ src/modules/health/health.module.ts
@@ -1,8 +1,8 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
-import { CqrsModule } from '@nestjs/cqrs';
 import { TerminusModule } from '@nestjs/terminus';
 
+import { CqrsModule } from '@/infrastructure/cqrs/cqrs.module';
 import { GetHealthHandler } from '@/modules/health/applications/queries/getHealth/getHealth.handler';
 import { HealthService } from '@/modules/health/applications/services/health.service';
 import { DatabaseIndicator } from '@/modules/health/infrastructure/indicators/database.indicator';
@@ -10,7 +10,7 @@ import { RedisIndicator } from '@/modules/health/infrastructure/indicators/redis
 import { HealthController } from '@/modules/health/interfaces/controllers/v1.controller';
 
 @Module({
-    imports: [CqrsModule, HttpModule, TerminusModule],
+    imports: [HttpModule, TerminusModule, CqrsModule],
     controllers: [HealthController],
     providers: [HealthService, GetHealthHandler, DatabaseIndicator, RedisIndicator],
 })
