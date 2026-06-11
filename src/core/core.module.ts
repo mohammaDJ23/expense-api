2509a81e53286151b0f981d2648d842007958334
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-11 13:10:43 +0330

    chore: 692263 add the google files

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -3,24 +3,19 @@ import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 
 import { ExceptionNormalizerModule } from '@/core/exceptions/normalizer/exceptionNormalizer.module';
 import { FilterModule } from '@/core/filters/filter.module';
-import { GoogleAuthGuard } from '@/core/guards/googleAuth.guard';
+import { GoogleModule } from '@/core/google/google.module';
 import { JwtAuthGuard } from '@/core/guards/jwtAuth.guard';
 import { TransformResponseInterceptor } from '@/core/interceptors/transformResponse.interceptor';
 import { ApiVersioningService } from '@/core/services/apiVersioning.service';
 import { AppInstanceService } from '@/core/services/appInstance.service';
 import { VersionService } from '@/core/services/version.service';
-import { GoogleAuthStrategy } from '@/core/strategies/googleAuth.strategy';
 import { JwtAuthStrategy } from '@/core/strategies/jwtAuth.strategy';
-import { CqrsModule } from '@/infrastructure/cqrs/cqrs.module';
-import { UserModule } from '@/modules/user/user.module';
 
 @Module({
-    imports: [UserModule, CqrsModule, ExceptionNormalizerModule, FilterModule],
+    imports: [ExceptionNormalizerModule, FilterModule, GoogleModule],
     providers: [
         JwtAuthGuard,
         JwtAuthStrategy,
-        GoogleAuthGuard,
-        GoogleAuthStrategy,
         ApiVersioningService,
         AppInstanceService,
         VersionService,
