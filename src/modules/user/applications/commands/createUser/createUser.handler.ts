5da3ffda4537d4c4c912ef4a237cb83a5199dd90
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-11 15:45:40 +0330

    chore: 895031 use TselectUser

--- src/modules/user/applications/commands/createUser/createUser.handler.ts
+++ src/modules/user/applications/commands/createUser/createUser.handler.ts
@@ -3,13 +3,13 @@ import { CommandHandler, type ICommandHandler } from '@nestjs/cqrs';
 import { CreateUserCommand } from '@/modules/user/applications/commands/createUser/createUser.command';
 import { UserRepository } from '@/modules/user/infrastructure/repositories/user.repository';
 
-import type { IUserAbstract } from '@/modules/user/domain/interfaces/userAbstract.interface';
+import type { TSelectUser } from '@/modules/user/infrastructure/schemas/user.schema';
 
 @CommandHandler(CreateUserCommand)
 export class CreateUserHandler implements ICommandHandler<CreateUserCommand> {
     constructor(private readonly userRepository: UserRepository) {}
 
-    execute(command: CreateUserCommand): Promise<IUserAbstract> {
+    execute(command: CreateUserCommand): Promise<TSelectUser> {
         return this.userRepository.create(command);
     }
 }
