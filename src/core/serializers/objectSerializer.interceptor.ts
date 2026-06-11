0eac412f02aed4e863e609350b9c76ebe0f790a8
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-11 14:20:04 +0330

    chore: 692263 use constants better

--- src/core/serializers/objectSerializer.interceptor.ts
+++ src/core/serializers/objectSerializer.interceptor.ts
@@ -8,7 +8,7 @@ import { plainToClass } from 'class-transformer';
 // eslint-disable-next-line import-x/no-deprecated
 import { map, type Observable } from 'rxjs';
 
-import type { TClassConstructor } from '@/common/types';
+import type { TClassConstructor } from '@/common/common.types';
 
 export class ObjectSerializerInterceptor<T> implements NestInterceptor {
     constructor(private readonly dto: TClassConstructor<T>) {}
