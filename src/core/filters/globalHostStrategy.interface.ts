03c30de5cfed3125d1713458af968b3b49da134c
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-11 20:16:45 +0330

    chore: 895031 rename the user and reponses files

--- src/core/filters/globalHostStrategy.interface.ts
+++ src/core/filters/globalHostStrategy.interface.ts
@@ -1,5 +1,5 @@
 import type { ExceptionNormalizerEntity } from '@/core/exceptions/normalizer/exceptionNormalizer.entity';
-import type { HttpResponseEntity } from '@/core/httpResponse/httpResponse.entity';
+import type { HttpResponseEntity } from '@/core/responses/http/httpResponse.entity';
 import type { ArgumentsHost } from '@nestjs/common';
 
 export interface IGlobalHostStrategy {
