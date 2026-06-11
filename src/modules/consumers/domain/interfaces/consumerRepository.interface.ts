41dc0e911b484c08c5abfbd8b14939644247b74b
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-11 20:26:59 +0330

    chore: 895031 rename consumers to consumer

--- src/modules/consumers/domain/interfaces/consumerRepository.interface.ts
+++
@@ -1,9 +0,0 @@
-import type {
-    TInsertConsumer,
-    TSelectConsumer,
-} from '@/modules/consumers/infrastructure/schemas/consumer.schema';
-
-export interface IConsumerRepository {
-    create(data: TInsertConsumer): Promise<TSelectConsumer>;
-    getByIdOrNull(id: string): Promise<TSelectConsumer | null>;
-}
