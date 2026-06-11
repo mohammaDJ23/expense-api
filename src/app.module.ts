67761fbcc15feacd8aa56afc2e1ff2c85607b9ed
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-11 21:08:28 +0330

    chore: 895031 add the consumer, location and receiver modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,22 @@ import { Module } from '@nestjs/common';
 import { CoreModule } from '@/core/core.module';
 import { InfrastructureModule } from '@/infrastructure/infrastructure.module';
 import { AuthenticationModule } from '@/modules/authentication/authentication.module';
+import { ConsumerModule } from '@/modules/consumer/consumer.module';
 import { HealthModule } from '@/modules/health/health.module';
+import { LocationModule } from '@/modules/location/location.module';
+import { ReceiverModule } from '@/modules/receiver/location.module';
 import { UserModule } from '@/modules/user/user.module';
 
 @Module({
-    imports: [InfrastructureModule, CoreModule, HealthModule, AuthenticationModule, UserModule],
+    imports: [
+        InfrastructureModule,
+        CoreModule,
+        HealthModule,
+        AuthenticationModule,
+        UserModule,
+        ConsumerModule,
+        LocationModule,
+        ReceiverModule,
+    ],
 })
 export class AppModule {}
