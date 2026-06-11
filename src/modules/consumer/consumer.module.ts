2f1d29431fa24f5b047817b8f5eea394a9c3c6d2
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-11 20:26:59 +0330

    chore: 895031 rename consumers to consumer

---
+++ src/modules/consumer/consumer.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+
+import { CqrsModule } from '@/infrastructure/cqrs/cqrs.module';
+import { CreateConsumerHandler } from '@/modules/consumer/applications/commands/createConsumer/createConsumer.handler';
+import { GetConsumerByIdOrNullHandler } from '@/modules/consumer/applications/queries/getConsumerByIdOrNull/getConsumerByIdOrNull.handler';
+import { ConsumerRepository } from '@/modules/consumer/infrastructure/repositories/consumer.repository';
+
+@Module({
+    imports: [CqrsModule],
+    providers: [CreateConsumerHandler, GetConsumerByIdOrNullHandler, ConsumerRepository],
+})
+export class ConsumerModule {}
