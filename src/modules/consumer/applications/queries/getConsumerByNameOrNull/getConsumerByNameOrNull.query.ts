f5b06aab03530daf09f9be035a1adf6f927e0636
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-12 12:15:49 +0330

    chore: 895031 remove CreateConsumer command and GetConsumerByNameOrNull query and their repos

--- src/modules/consumer/applications/queries/getConsumerByNameOrNull/getConsumerByNameOrNull.query.ts
+++
@@ -1,3 +0,0 @@
-export class GetConsumerByNameOrNullQuery {
-    constructor(public readonly name: string) {}
-}
--- src/modules/consumer/domain/interfaces/consumerRepository.interface.ts
+++ src/modules/consumer/domain/interfaces/consumerRepository.interface.ts
@@ -4,8 +4,6 @@ import type {
 } from '@/modules/consumer/infrastructure/schemas/consumer.schema';
 
 export interface IConsumerRepository {
-    create(data: TInsertConsumer): Promise<TSelectConsumer>;
     createMany(data: TInsertConsumer[]): Promise<TSelectConsumer[]>;
-    getByNameOrNull(name: string): Promise<TSelectConsumer | null>;
     getManyByName(names: string[]): Promise<TSelectConsumer[]>;
 }
