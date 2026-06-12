1900b0729cad57491a695d315062bce8c54db5b0
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-12 10:20:50 +0330

    chore: 895031 add the create bill dto

---
+++ src/modules/bill/interface/dtos/createBill.request.dto.ts
@@ -0,0 +1,52 @@
+import {
+    IsString,
+    IsNumberString,
+    Length,
+    IsArray,
+    ArrayMinSize,
+    ArrayMaxSize,
+    Matches,
+    ValidateIf,
+    IsDateString,
+} from 'class-validator';
+
+export class CreateBillRequestDto {
+    @IsNumberString()
+    @Length(1, 12)
+    @Matches(/^[1-9]\d*$/, { message: 'Amount must be a positive integer without decimal' })
+    amount: string;
+
+    @IsString()
+    @Length(3, 50)
+    // eslint-disable-next-line security/detect-unsafe-regex
+    @Matches(/^[a-zA-Z_]+( [a-zA-Z_]+)*$/, { message: 'Invalid receiver.' })
+    receiver: string;
+
+    @IsArray()
+    @IsString({ each: true })
+    @Length(3, 50, { each: true, message: 'Invalid consumer' })
+    @ArrayMinSize(1)
+    @ArrayMaxSize(20)
+    // eslint-disable-next-line security/detect-unsafe-regex
+    @Matches(/^[a-zA-Z_]+( [a-zA-Z_]+)*$/, { each: true, message: 'Invalid consumers' })
+    consumers: string[];
+
+    @IsString()
+    @Length(3, 500)
+    // eslint-disable-next-line security/detect-unsafe-regex
+    @Matches(/^[^\s]+(\s+[^\s]+)*$/, { message: 'Invalid description.' })
+    description: string;
+
+    @ValidateIf((_object, value) => value !== undefined && value !== null)
+    @IsDateString(
+        { strictSeparator: true },
+        { message: 'PurchasedAt must be in ISO 8601 format: YYYY-MM-DDThh:mm:ss.sssZ' },
+    )
+    purchasedAt: string | null;
+
+    @IsString()
+    @Length(3, 50)
+    // eslint-disable-next-line security/detect-unsafe-regex
+    @Matches(/^[a-zA-Z_]+( [a-zA-Z_]+)*$/, { message: 'Invalid location.' })
+    location: string;
+}
