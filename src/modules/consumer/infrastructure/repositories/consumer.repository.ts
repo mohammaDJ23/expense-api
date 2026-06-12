8275eec7cfd3fda6b563f4aea1ca2287cf0d8fa2
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-12 12:15:49 +0330

    chore: 895031 remove CreateConsumer command and GetConsumerByNameOrNull query and their repos

--- src/modules/consumer/infrastructure/repositories/consumer.repository.ts
+++ src/modules/consumer/infrastructure/repositories/consumer.repository.ts
@@ -1,12 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { eq, inArray } from 'drizzle-orm';
+import { inArray } from 'drizzle-orm';
 
 import { DrizzleRepository } from '@/infrastructure/database/drizzle/drizzle.repository';
-import {
-    toEntities,
-    toEntityOrNull,
-    toEntityOrThrow,
-} from '@/infrastructure/database/drizzle/drizzle.transformer';
+import { toEntities } from '@/infrastructure/database/drizzle/drizzle.transformer';
 import {
     consumers,
     type TInsertConsumer,
@@ -17,19 +13,8 @@ import type { IConsumerRepository } from '@/modules/consumer/domain/interfaces/c
 
 @Injectable()
 export class ConsumerRepository extends DrizzleRepository implements IConsumerRepository {
-    create(data: TInsertConsumer): Promise<TSelectConsumer> {
-        return toEntityOrThrow(
-            this.db.insert(consumers).values(data).returning(),
-            'Unable to create',
-        );
-    }
-
     createMany(data: TInsertConsumer[]): Promise<TSelectConsumer[]> {
         return toEntities(this.db.insert(consumers).values(data).returning());
-    }
-
-    getByNameOrNull(name: string): Promise<TSelectConsumer | null> {
-        return toEntityOrNull(this.db.select().from(consumers).where(eq(consumers.name, name)));
     }
 
     getManyByName(names: string[]): Promise<TSelectConsumer[]> {
