edf5186705f038ec9a5023961a6d50cc60a55dc1
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-12 13:28:31 +0330

    chore: 895031 add the receiver service

---
+++ src/modules/receiver/applications/services/receiver.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+
+import { ProcessFailedInternalServerErrorException } from '@/core/exceptions/processFailedInternalServerError.exception';
+import { CreateReceiverCommand } from '@/modules/receiver/applications/commands/createReceiver/createReceiver.command';
+import { GetReceiverByNameOrNullQuery } from '@/modules/receiver/applications/queries/getReceiverByNameOrNull/getReceiverByNameOrNull.query';
+
+import type { TSelectReceiver } from '@/modules/receiver/infrastructure/schemas/receiver.schema';
+
+@Injectable()
+export class ReceiverService {
+    constructor(
+        private readonly queryBus: QueryBus,
+        private readonly commandBus: CommandBus,
+    ) {}
+
+    create(name: string): Promise<TSelectReceiver> {
+        try {
+            const createReceiverCommand = new CreateReceiverCommand({
+                name,
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            });
+            return this.commandBus.execute<CreateReceiverCommand, TSelectReceiver>(
+                createReceiverCommand,
+            );
+        } catch {
+            throw new ProcessFailedInternalServerErrorException();
+        }
+    }
+
+    getByNameOrNull(name: string): Promise<TSelectReceiver | null> {
+        try {
+            const getReceiverByNameOrNullQuery = new GetReceiverByNameOrNullQuery(name);
+            return this.queryBus.execute<GetReceiverByNameOrNullQuery, TSelectReceiver | null>(
+                getReceiverByNameOrNullQuery,
+            );
+        } catch {
+            throw new ProcessFailedInternalServerErrorException();
+        }
+    }
+}
--- src/modules/receiver/receiver.module.ts
+++ src/modules/receiver/receiver.module.ts
@@ -5,12 +5,14 @@ import { CreateReceiverHandler } from '@/modules/receiver/applications/commands/
 import { CreateUserReceiverHandler } from '@/modules/receiver/applications/commands/createUserReceiver/createUserReceiver.handler';
 import { GetReceiverByNameOrNullHandler } from '@/modules/receiver/applications/queries/getReceiverByNameOrNull/getReceiverByNameOrNull.handler';
 import { GetUserReceiverByIdOrNullHandler } from '@/modules/receiver/applications/queries/getUserReceiverByIdOrNull/getUserReceiverByIdOrNull.handler';
+import { ReceiverService } from '@/modules/receiver/applications/services/receiver.service';
 import { ReceiverRepository } from '@/modules/receiver/infrastructure/repositories/receiver.repository';
 import { UserReceiverRepository } from '@/modules/receiver/infrastructure/repositories/userReceiver.repository';
 
 @Module({
     imports: [CqrsModule],
     providers: [
+        ReceiverService,
         CreateReceiverHandler,
         CreateUserReceiverHandler,
         GetReceiverByNameOrNullHandler,
