2305d42c2fc7112949e74dff46ce7a5d4c5a603f
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-12 09:14:35 +0330

    chore: 895031 add the bill abstract and interface

---
+++ src/modules/bill/domain/abstracts/bill.abstract.ts
@@ -0,0 +1,27 @@
+import type { IBillAbstract } from '@/modules/bill/domain/interfaces/billAbstract.interface';
+
+export abstract class BillAbstract implements Partial<IBillAbstract> {
+    public readonly id?: string;
+    public readonly amount?: string;
+    public readonly description?: string;
+    public readonly purchasedAt?: Date | null;
+    public readonly createdAt?: Date;
+    public readonly updatedAt?: Date;
+    public readonly userId?: string;
+    public readonly consumerId?: string;
+    public readonly receiverId?: string;
+    public readonly locationId?: string;
+
+    constructor(data: Partial<IBillAbstract>) {
+        this.id = data.id;
+        this.amount = data.amount;
+        this.description = data.description;
+        this.purchasedAt = data.purchasedAt;
+        this.createdAt = data.createdAt;
+        this.updatedAt = data.updatedAt;
+        this.userId = data.userId;
+        this.consumerId = data.consumerId;
+        this.receiverId = data.receiverId;
+        this.locationId = data.locationId;
+    }
+}
---
+++ src/modules/bill/domain/interfaces/billAbstract.interface.ts
@@ -0,0 +1,3 @@
+import type { TSelectBill } from '@/modules/bill/infrastructure/schemas/bill.schema';
+
+export interface IBillAbstract extends TSelectBill {}
