bd275b5257e2f30a0ee62e4208757b0d97e91bc4
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-12 12:16:21 +0330

    chore: 895031 remove CreateConsumer command and GetConsumerByNameOrNull query and their repos

--- src/modules/consumer/consumer.module.ts
+++ src/modules/consumer/consumer.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 
 import { CqrsModule } from '@/infrastructure/cqrs/cqrs.module';
-import { CreateConsumerHandler } from '@/modules/consumer/applications/commands/createConsumer/createConsumer.handler';
 import { CreateManyConsumersHandler } from '@/modules/consumer/applications/commands/createManyConsumers/createManyConsumers.handler';
 import { CreateUserConsumerHandler } from '@/modules/consumer/applications/commands/createUserConsumer/createUserConsumer.handler';
-import { GetConsumerByNameOrNullHandler } from '@/modules/consumer/applications/queries/getConsumerByNameOrNull/getConsumerByNameOrNull.handler';
 import { GetManyConsumersByNameHandler } from '@/modules/consumer/applications/queries/getManyConsumersByName/getManyConsumersByName.handler';
 import { GetUserConsumerByIdOrNullHandler } from '@/modules/consumer/applications/queries/getUserConsumerByIdOrNull/getUserConsumerByIdOrNull.handler';
 import { ConsumerRepository } from '@/modules/consumer/infrastructure/repositories/consumer.repository';
@@ -13,10 +11,8 @@ import { UserConsumerRepository } from '@/modules/consumer/infrastructure/reposi
 @Module({
     imports: [CqrsModule],
     providers: [
-        CreateConsumerHandler,
         CreateUserConsumerHandler,
         CreateManyConsumersHandler,
-        GetConsumerByNameOrNullHandler,
         GetUserConsumerByIdOrNullHandler,
         GetManyConsumersByNameHandler,
         ConsumerRepository,
