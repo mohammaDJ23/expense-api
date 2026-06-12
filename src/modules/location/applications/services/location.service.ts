82f6045c442d3ccf0bb95fccba8c5e7e93752e0c
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-12 13:22:01 +0330

    chore: 895031 add the location service

---
+++ src/modules/location/applications/services/location.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+
+import { ProcessFailedInternalServerErrorException } from '@/core/exceptions/processFailedInternalServerError.exception';
+import { CreateLocationCommand } from '@/modules/location/applications/commands/createLocation/createLocation.command';
+import { GetLocationByNameOrNullQuery } from '@/modules/location/applications/queries/getLocationByNameOrNull/getLocationByNameOrNull.query';
+
+import type { TSelectLocation } from '@/modules/location/infrastructure/schemas/location.schema';
+
+@Injectable()
+export class LocationService {
+    constructor(
+        private readonly queryBus: QueryBus,
+        private readonly commandBus: CommandBus,
+    ) {}
+
+    create(name: string): Promise<TSelectLocation> {
+        try {
+            const createLocationCommand = new CreateLocationCommand({
+                name,
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            });
+            return this.commandBus.execute<CreateLocationCommand, TSelectLocation>(
+                createLocationCommand,
+            );
+        } catch {
+            throw new ProcessFailedInternalServerErrorException();
+        }
+    }
+
+    getByNameOrNull(name: string): Promise<TSelectLocation | null> {
+        try {
+            const getLocationByNameOrNullQuery = new GetLocationByNameOrNullQuery(name);
+            return this.queryBus.execute<GetLocationByNameOrNullQuery, TSelectLocation | null>(
+                getLocationByNameOrNullQuery,
+            );
+        } catch {
+            throw new ProcessFailedInternalServerErrorException();
+        }
+    }
+}
