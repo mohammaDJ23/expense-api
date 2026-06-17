9dcc7fe69516e897c9b26f1209c688f7bb3e7cd8
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-17 08:34:55 +0330

    breaking: 970655 use IServiceHandler for LocalForgotPasswordService

---
+++ src/modules/authentication/applications/services/localForgotPassword.service.ts
@@ -0,0 +1,67 @@
+import { Injectable, ServiceUnavailableException } from '@nestjs/common';
+import { QueryBus } from '@nestjs/cqrs';
+
+import { LocalAuthProviderForbiddenException } from '@/core/exceptions/localAuthProviderForbidden.exception';
+import { ProcessFailedInternalServerErrorException } from '@/core/exceptions/processFailedInternalServerError.exception';
+import { GetUserByEmailOrNullQuery } from '@/modules/user/applications/queries/getUserByEmailOrNull/getUserByEmailOrNull.query';
+import { AuthProvider } from '@/modules/user/domain/enums/authProvider.enum';
+
+import { PasswordMailerService } from './passwordMailer.service';
+import { PasswordStorageService } from './passwordStorage.service';
+import { PasswordTokenService } from './passwordToken.service';
+
+import type { IServiceHandler } from '@/core/interfaces/serviceHandler.interface';
+import type { LocalForgotPasswordRequestDto } from '@/modules/authentication/interface/dtos/localForgotPassword.request.dto';
+import type { TSelectUser } from '@/modules/user/infrastructure/schemas/user.schema';
+
+@Injectable()
+export class LocalForgotPasswordService implements IServiceHandler {
+    constructor(
+        private readonly queryBus: QueryBus,
+        private readonly passwordMailerService: PasswordMailerService,
+        private readonly passwordTokenService: PasswordTokenService,
+        private readonly passwordStorageService: PasswordStorageService,
+    ) {}
+
+    async execute(data: LocalForgotPasswordRequestDto): Promise<boolean> {
+        let user: TSelectUser | null;
+        try {
+            const storedToken = await this.passwordStorageService.get(data.email);
+            if (storedToken) {
+                return true;
+            }
+
+            const getUserByEmailOrNullQuery = new GetUserByEmailOrNullQuery(data.email);
+            user = await this.queryBus.execute<GetUserByEmailOrNullQuery, TSelectUser | null>(
+                getUserByEmailOrNullQuery,
+            );
+        } catch {
+            throw new ProcessFailedInternalServerErrorException();
+        }
+
+        if (!user) {
+            return true;
+        }
+
+        if (user.authProvider !== AuthProvider.LOCAL) {
+            throw new LocalAuthProviderForbiddenException();
+        }
+
+        if (user.verifiedAt) {
+            try {
+                const token = this.passwordTokenService.sign(user);
+                await this.passwordStorageService.set(user.email, token);
+                await this.passwordMailerService.execute(user, token);
+            } catch {
+                try {
+                    await this.passwordStorageService.delete(user.email);
+                    // eslint-disable-next-line no-empty
+                } catch {}
+
+                throw new ServiceUnavailableException('Could not send you a verification link');
+            }
+        }
+
+        return true;
+    }
+}
