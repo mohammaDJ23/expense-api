7d070a575bd8bd64d3a4ca5fd6e0a75174ad9170
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-17 08:25:02 +0330

    breaking: 970655 use IServiceHandler for AccessTokenService

--- src/modules/authentication/applications/services/accessToken.service.ts
+++ src/modules/authentication/applications/services/accessToken.service.ts
@@ -3,14 +3,15 @@ import { JwtService } from '@nestjs/jwt';
 
 import { getCurrentUTCTimestamp } from '@/common/utils/getCurrentUTCTimestamp.util';
 
+import type { IServiceHandler } from '@/core/interfaces/serviceHandler.interface';
 import type { IAccessTokenPayload } from '@/modules/authentication/domain/interfaces/accessTokenPayload.interface';
 import type { TSelectUser } from '@/modules/user/infrastructure/schemas/user.schema';
 
 @Injectable()
-export class AccessTokenService {
+export class AccessTokenService implements IServiceHandler {
     constructor(private readonly jwtService: JwtService) {}
 
-    sign(user: TSelectUser): string {
+    execute(user: TSelectUser): string {
         return this.jwtService.sign<IAccessTokenPayload>(
             {
                 id: user.id,
