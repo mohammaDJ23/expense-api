e90ded6b3c74d737930ecc54b1182467b0fdd3fa
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-18 20:58:24 +0330

    refactor: 614766 remove the receiver api

--- src/modules/receiver/interfaces/controllers/v1.controller.ts
+++ src/modules/receiver/interfaces/controllers/v1.controller.ts
@@ -1,32 +1,4 @@
-import { Controller, Get, HttpStatus, Param, UseGuards } from '@nestjs/common';
-
-import { JwtAuthGuard } from '@/core/authentication/jwtAuth.guard';
-import { HttpResponse } from '@/core/responses/http/httpResponse.decorator';
-import { SerializerInterceptor } from '@/core/serializers/serializerInterceptor.decorator';
-import { CurrentUser } from '@/core/user/currentUser.decorator';
-import { GetReceiverByIdAndUserIdOrThrowService } from '@/modules/receiver/applications/services/getReceiverByIdAndUserIdOrThrow.service';
-import { GetReceiverRequestDto } from '@/modules/receiver/interfaces/dtos/getReceiver.request.dto';
-import { ReceiverResponseDto } from '@/modules/receiver/interfaces/dtos/receiver.response.dto';
-
-import { SUCCESS_GET_RECEIVER_MESSAGE } from './controllers.constants';
-
-import type { ICurrentUser } from '@/core/user/currentUser.interface';
-import type { TSelectReceiver } from '@/modules/receiver/infrastructure/schemas/receiver.schema';
+import { Controller } from '@nestjs/common';
 
 @Controller({ version: '1', path: 'api/receivers' })
-export class ReceiverController {
-    constructor(
-        private readonly getReceiverByIdAndUserIdOrThrowService: GetReceiverByIdAndUserIdOrThrowService,
-    ) {}
-
-    @Get(':id')
-    @UseGuards(JwtAuthGuard)
-    @SerializerInterceptor(ReceiverResponseDto)
-    @HttpResponse(SUCCESS_GET_RECEIVER_MESSAGE, HttpStatus.OK)
-    getByIdAndUserId(
-        @Param() param: GetReceiverRequestDto,
-        @CurrentUser() user: ICurrentUser,
-    ): Promise<TSelectReceiver> {
-        return this.getReceiverByIdAndUserIdOrThrowService.execute(user.id, param.id);
-    }
-}
+export class ReceiverController {}
--- src/modules/receiver/receiver.module.ts
+++ src/modules/receiver/receiver.module.ts
@@ -4,13 +4,11 @@ import { CqrsModule } from '@nestjs/cqrs';
 import { AuthenticationModule } from '@/core/authentication/authentication.module';
 import { CreateReceiverHandler } from '@/modules/receiver/applications/commands/createReceiver/createReceiver.handler';
 import { CreateUserReceiverHandler } from '@/modules/receiver/applications/commands/createUserReceiver/createUserReceiver.handler';
-import { GetReceiverByIdAndUserIdOrThrowHandler } from '@/modules/receiver/applications/queries/getReceiverByIdAndUserIdOrThrow/getReceiverByIdAndUserIdOrThrow.handler';
 import { GetReceiverByNameOrNullHandler } from '@/modules/receiver/applications/queries/getReceiverByNameOrNull/getReceiverByNameOrNull.handler';
 import { GetUserReceiverByIdOrNullHandler } from '@/modules/receiver/applications/queries/getUserReceiverByIdOrNull/getUserReceiverByIdOrNull.handler';
 import { CreateReceiverService } from '@/modules/receiver/applications/services/createReceiver.service';
 import { CreateUserReceiverService } from '@/modules/receiver/applications/services/createUserReceiver.service';
 import { CreateUserReceiverIfNotExistsService } from '@/modules/receiver/applications/services/createUserReceiverIfNotExists.service';
-import { GetReceiverByIdAndUserIdOrThrowService } from '@/modules/receiver/applications/services/getReceiverByIdAndUserIdOrThrow.service';
 import { GetReceiverByNameOrCreateService } from '@/modules/receiver/applications/services/getReceiverByNameOrCreate.service';
 import { GetReceiverByNameOrNullService } from '@/modules/receiver/applications/services/getReceiverByNameOrNull.service';
 import { GetUserReceiverByIdOrNullService } from '@/modules/receiver/applications/services/getUserReceiverByIdOrNull.service';
@@ -28,8 +26,6 @@ import { ReceiverController } from '@/modules/receiver/interfaces/controllers/v1
         CreateUserReceiverService,
         CreateUserReceiverIfNotExistsService,
         GetUserReceiverByIdOrNullService,
-        GetReceiverByIdAndUserIdOrThrowHandler,
-        GetReceiverByIdAndUserIdOrThrowService,
         CreateReceiverHandler,
         CreateUserReceiverHandler,
         GetReceiverByNameOrNullHandler,
