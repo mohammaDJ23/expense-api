a4f299a643a7942260f57f9be9ecb26a71c1734a
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-20 23:43:09 +0330

    breaking: 874920 rename the service of the health  module

--- src/modules/health/health.module.ts
+++ src/modules/health/health.module.ts
@@ -1,10 +1,10 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
-import { CqrsModule } from '@nestjs/cqrs';
 import { TerminusModule } from '@nestjs/terminus';
 
+import { CqrsModule } from '@/infrastructure/cqrs/cqrs.module';
 import { GetHealthHandler } from '@/modules/health/applications/queries/getHealth/getHealth.handler';
-import { GetHealthService } from '@/modules/health/applications/services/getHealth.service';
+import { HealthService } from '@/modules/health/applications/services/health.service';
 import { DatabaseIndicator } from '@/modules/health/infrastructure/indicators/database.indicator';
 import { RedisIndicator } from '@/modules/health/infrastructure/indicators/redis.indicator';
 import { HealthController } from '@/modules/health/interfaces/controllers/v1.controller';
@@ -12,6 +12,6 @@ import { HealthController } from '@/modules/health/interfaces/controllers/v1.con
 @Module({
     imports: [HttpModule, TerminusModule, CqrsModule],
     controllers: [HealthController],
-    providers: [GetHealthService, GetHealthHandler, DatabaseIndicator, RedisIndicator],
+    providers: [HealthService, GetHealthHandler, DatabaseIndicator, RedisIndicator],
 })
 export class HealthModule {}
