3ff819601c7d2ae1fbae4d88dfd02ac195cb646b
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-23 10:15:51 +0330

    feature: 968638 add the location creation api

--- src/modules/location/interfaces/controllers/controllers.constants.ts
+++ src/modules/location/interfaces/controllers/controllers.constants.ts
@@ -1,2 +1,3 @@
 export const SUCCESS_GET_LOCATIONS_MESSAGE = 'You have received the locations successfully';
 export const SUCCESS_GET_LOCATION_MESSAGE = 'You have received the location successfully';
+export const SUCCESS_CREATE_LOCATION_MESSAGE = 'You have created a new location successfully';
