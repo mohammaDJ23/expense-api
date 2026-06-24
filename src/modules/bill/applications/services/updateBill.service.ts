de8c85a213fb660e4ee2760994c93a4e9cb3c173
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-24 19:03:07 +0330

    refactor: 542092 use blocks to organize the code

--- src/modules/bill/applications/services/updateBill.service.ts
+++ src/modules/bill/applications/services/updateBill.service.ts
@@ -4,6 +4,7 @@ import { Transactional } from '@nestjs-cls/transactional';
 
 import { getCurrentUTCTimestamp } from '@/common/utils/getCurrentUTCTimestamp.util';
 import { IdEntity } from '@/core/entities/id.entity';
+import { ProcessFailedInternalServerErrorException } from '@/core/exceptions/processFailedInternalServerError.exception';
 import { UpdateBillCommand } from '@/modules/bill/applications/commands/updateBill/updateBill.command';
 import { IsBillExistsByUserIdAndIdQuery } from '@/modules/bill/applications/queries/isBillExistsByUserIdAndId/isBillExistsByUserIdAndId.query';
 import { CreateManyBillsConsumersCommand } from '@/modules/consumer/applications/commands/createManyBillsConsumers/createManyBillsConsumers.command';
@@ -60,61 +61,72 @@ export class UpdateBillService implements IServiceHandler {
                 FindManyBillsConsumersByRefIdQuery,
                 ISelectBillConsumer[]
             >(new FindManyBillsConsumersByRefIdQuery(data.id));
-            const existenceConsumerIds = billsConsumers.map(
-                (billConsumer) => billConsumer.consumerId,
-            );
 
-            const receivedConsumerIdsSet = new Set<string>(data.consumerIds);
-            const existenceConsumerIdsSet = new Set<string>(existenceConsumerIds);
+            if (billsConsumers.length <= 0) {
+                throw new ProcessFailedInternalServerErrorException();
+            }
 
-            const idsToCreate = data.consumerIds.filter(
-                (consumerId) => !existenceConsumerIdsSet.has(consumerId),
-            );
-            const idsToDelete = existenceConsumerIds.filter(
-                (existenceId) => !receivedConsumerIdsSet.has(existenceId),
-            );
+            {
+                const existenceConsumerIds = billsConsumers.map(
+                    (billConsumer) => billConsumer.consumerId,
+                );
 
-            const billConsumerCommands: Promise<ISelectBillConsumer[]>[] = [];
+                const receivedConsumerIdsSet = new Set<string>(data.consumerIds);
+                const existenceConsumerIdsSet = new Set<string>(existenceConsumerIds);
 
-            if (idsToCreate.length > 0) {
-                billConsumerCommands.push(
-                    this.commandBus.execute<CreateManyBillsConsumersCommand, ISelectBillConsumer[]>(
-                        new CreateManyBillsConsumersCommand(
-                            idsToCreate.map((id) => ({
-                                billId: data.id,
-                                consumerId: id,
-                                createdAt: getCurrentUTCTimestamp(),
-                            })),
+                const idsToCreate = data.consumerIds.filter(
+                    (consumerId) => !existenceConsumerIdsSet.has(consumerId),
+                );
+                const idsToDelete = existenceConsumerIds.filter(
+                    (existenceId) => !receivedConsumerIdsSet.has(existenceId),
+                );
+
+                const billConsumerCommands: Promise<ISelectBillConsumer[]>[] = [];
+
+                if (idsToCreate.length > 0) {
+                    billConsumerCommands.push(
+                        this.commandBus.execute<
+                            CreateManyBillsConsumersCommand,
+                            ISelectBillConsumer[]
+                        >(
+                            new CreateManyBillsConsumersCommand(
+                                idsToCreate.map((id) => ({
+                                    billId: data.id,
+                                    consumerId: id,
+                                    createdAt: getCurrentUTCTimestamp(),
+                                })),
+                            ),
                         ),
+                    );
+                }
+
+                if (idsToDelete.length > 0) {
+                    billConsumerCommands.push(
+                        this.commandBus.execute<
+                            DeleteManyBillsConsumersCommand,
+                            ISelectBillConsumer[]
+                        >(new DeleteManyBillsConsumersCommand(data.id, idsToDelete)),
+                    );
+                }
+
+                await Promise.all([
+                    ...billConsumerCommands,
+                    this.commandBus.execute<UpdateBillCommand, ISelectBill>(
+                        new UpdateBillCommand({
+                            id: data.id,
+                            amount: data.amount,
+                            description: data.description,
+                            purchasedAt: data.purchasedAt,
+                            updatedAt: getCurrentUTCTimestamp(),
+                            userId,
+                            receiverId: data.receiverId,
+                            locationId: data.locationId,
+                        }),
                     ),
-                );
+                ]);
+
+                return IdEntity.create(data.id);
             }
-
-            if (idsToDelete.length > 0) {
-                billConsumerCommands.push(
-                    this.commandBus.execute<DeleteManyBillsConsumersCommand, ISelectBillConsumer[]>(
-                        new DeleteManyBillsConsumersCommand(data.id, idsToDelete),
-                    ),
-                );
-            }
-
-            await Promise.all([
-                ...billConsumerCommands,
-                this.commandBus.execute<UpdateBillCommand, ISelectBill>(
-                    new UpdateBillCommand({
-                        id: data.id,
-                        amount: data.amount,
-                        description: data.description,
-                        purchasedAt: data.purchasedAt,
-                        updatedAt: getCurrentUTCTimestamp(),
-                        userId,
-                        receiverId: data.receiverId,
-                        locationId: data.locationId,
-                    }),
-                ),
-            ]);
-
-            return IdEntity.create(data.id);
         }
     }
 }
