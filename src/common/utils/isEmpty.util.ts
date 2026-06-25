22653ad51d319e439860043fcd6082b993b68192
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-25 23:15:08 +0330

    refactor: 054250 use generic type

--- src/common/utils/isEmpty.util.ts
+++ src/common/utils/isEmpty.util.ts
@@ -1,3 +1,4 @@
-export function isEmpty(arr: unknown[]): boolean {
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-parameters
+export function isEmpty<T>(arr: T[]): boolean {
     return arr.length <= 0;
 }
