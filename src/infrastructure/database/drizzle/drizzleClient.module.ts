e2dc5395099e9b62832c4746bed01f68e160992f
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-26 11:17:13 +0330

    chore: 724123 check the connection

--- src/infrastructure/database/drizzle/drizzleClient.module.ts
+++ src/infrastructure/database/drizzle/drizzleClient.module.ts
@@ -1,7 +1,7 @@
 import { Global, Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { drizzle, type NodePgDatabase } from 'drizzle-orm/node-postgres';
-import { Pool } from 'pg';
+import { Pool, type PoolClient } from 'pg';
 
 import { readSecret } from '@/common/utils/readSecret.util';
 
@@ -22,11 +22,23 @@ import { DRIZZLE_CLIENT_TOKEN } from './drizzle.constants';
                     ),
                     database: configService.getOrThrow<string>('DATABASE_NAME'),
                 });
+
+                let client: PoolClient | undefined;
                 try {
-                    await pool.connect();
-                    return drizzle({ client: pool, casing: 'camelCase' });
+                    client = await pool.connect();
+                    await client.query('SELECT 1');
+
+                    return drizzle({
+                        client: pool,
+                        casing: 'camelCase',
+                    });
                 } catch {
+                    await pool.end();
                     throw new Error('Could not initiate the database');
+                } finally {
+                    if (client) {
+                        client.release();
+                    }
                 }
             },
             inject: [ConfigService],
