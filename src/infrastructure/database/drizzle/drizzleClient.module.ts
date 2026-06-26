fe7229cc3dc74e30584c5ec53b2156495ddc1165
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-26 12:03:28 +0330

    chore: 724123 add some options to pool

--- src/infrastructure/database/drizzle/drizzleClient.module.ts
+++ src/infrastructure/database/drizzle/drizzleClient.module.ts
@@ -21,6 +21,24 @@ import { DRIZZLE_CLIENT_TOKEN } from './drizzle.constants';
                         configService.getOrThrow<string>('DATABASE_PASSWORD_FILE'),
                     ),
                     database: configService.getOrThrow<string>('DATABASE_NAME'),
+                    min: parseInt(configService.getOrThrow<string>('DATABASE_POOL_MIN'), 10),
+                    max: parseInt(configService.getOrThrow<string>('DATABASE_POOL_MAX'), 10),
+                    connectionTimeoutMillis: parseInt(
+                        configService.getOrThrow<string>('DATABASE_CONNECTION_TIMEOUT'),
+                        10,
+                    ),
+                    idleTimeoutMillis: parseInt(
+                        configService.getOrThrow<string>('DATABASE_IDLE_TIMEOUT'),
+                        10,
+                    ),
+                    maxLifetimeSeconds: parseInt(
+                        configService.getOrThrow<string>('DATABASE_MAX_LIFETIME'),
+                        10,
+                    ),
+                    ssl:
+                        configService.getOrThrow<string>('DATABASE_SSL') === 'true'
+                            ? { rejectUnauthorized: false }
+                            : false,
                 });
 
                 let client: PoolClient | undefined;
