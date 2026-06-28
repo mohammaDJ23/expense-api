0251df2aa1035d1f7b06b19f91d98ee5e7488003
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-28 09:39:42 +0330

    breaking: 700990 add the related cqrs, service and apis of consumer

--- src/modules/consumer/consumer.module.ts
+++ src/modules/consumer/consumer.module.ts
@@ -4,43 +4,41 @@ import { AuthenticationModule } from '@/core/authentication/authentication.modul
 import { CqrsModule } from '@/infrastructure/cqrs/cqrs.module';
 import { CreateConsumerHandler } from '@/modules/consumer/applications/commands/createConsumer/createConsumer.handler';
 import { CreateManyBillsConsumersHandler } from '@/modules/consumer/applications/commands/createManyBillsConsumers/createManyBillsConsumers.handler';
-import { CreateUserConsumerHandler } from '@/modules/consumer/applications/commands/createUserConsumer/createUserConsumer.handler';
 import { DeleteManyBillsConsumersHandler } from '@/modules/consumer/applications/commands/deleteManyBillsConsumers/deleteManyBillsConsumers.handler';
-import { FindConsumerByNameOrNullHandler } from '@/modules/consumer/applications/queries/findConsumerByNameOrNull/findConsumerByNameOrNull.handler';
+import { FindConsumerByUserIdAndIdOrNullHandler } from '@/modules/consumer/applications/queries/findConsumerByUserIdAndIdOrNull/findConsumerByUserIdAndIdOrNull.handler';
+import { FindConsumerByUserIdAndIdOrThrowHandler } from '@/modules/consumer/applications/queries/findConsumerByUserIdAndIdOrThrow/findConsumerByUserIdAndIdOrThrow.handler';
+import { FindConsumerListByUserIdHandler } from '@/modules/consumer/applications/queries/findConsumerListByUserId/findConsumerListByUserId.handler';
 import { FindManyBillConsumerTargetsByRefIdsHandler } from '@/modules/consumer/applications/queries/findManyBillConsumerTargetsByRefIds/findManyBillConsumerTargetsByRefIds.handler';
 import { FindManyBillsConsumersByRefIdHandler } from '@/modules/consumer/applications/queries/findManyBillsConsumersByRefId/findManyBillsConsumersByRefId.handler';
-import { FindUserConsumerByRefIdAndTargetIdOrNullHandler } from '@/modules/consumer/applications/queries/findUserConsumerByRefIdAndTargetIdOrNull/findUserConsumerByRefIdAndTargetIdOrNull.handler';
-import { FindUserConsumerTargetByRefIdAndTargetIdOrThrowHandler } from '@/modules/consumer/applications/queries/findUserConsumerTargetByRefIdAndTargetIdOrThrow/findUserConsumerTargetByRefIdAndTargetIdOrThrow.handler';
-import { FindUserConsumerTargetListByRefIdHandler } from '@/modules/consumer/applications/queries/findUserConsumerTargetListByRefId/findUserConsumerTargetListByRefId.handler';
-import { IsUserConsumerExistsByRefIdAndTargetIdsHandler } from '@/modules/consumer/applications/queries/isUserConsumerExistsByRefIdAndTargetIds/isUserConsumerExistsByRefIdAndTargetIds.handler';
+import { FindManyConsumersByUserIdAndIdsHandler } from '@/modules/consumer/applications/queries/findManyConsumersByUserIdAndIds/findManyConsumersByUserIdAndIds.handler';
+import { IsConsumerExistsByUserIdAndIdHandler } from '@/modules/consumer/applications/queries/isConsumerExistsByUserIdAndId/isConsumerExistsByUserIdAndId.handler';
 import { ConsumerService } from '@/modules/consumer/applications/services/consumer.service';
 import { CreateConsumerService } from '@/modules/consumer/applications/services/createConsumer.service';
-import { UserConsumerService } from '@/modules/consumer/applications/services/userConsumer.service';
+import { DeleteConsumerService } from '@/modules/consumer/applications/services/deleteConsumer.service';
+import { UpdateConsumerService } from '@/modules/consumer/applications/services/updateConsumer.service';
 import { BillConsumerRepository } from '@/modules/consumer/infrastructure/repositories/billConsumer.repository';
 import { ConsumerRepository } from '@/modules/consumer/infrastructure/repositories/consumer.repository';
-import { UserConsumerRepository } from '@/modules/consumer/infrastructure/repositories/userConsumer.repository';
 import { ConsumerController } from '@/modules/consumer/interfaces/controllers/v1.controller';
 
 @Module({
     imports: [CqrsModule, AuthenticationModule],
     controllers: [ConsumerController],
     providers: [
-        ConsumerService,
         CreateConsumerService,
-        UserConsumerService,
-        CreateUserConsumerHandler,
+        DeleteConsumerService,
+        UpdateConsumerService,
+        FindConsumerByUserIdAndIdOrNullHandler,
+        FindConsumerByUserIdAndIdOrThrowHandler,
+        FindConsumerListByUserIdHandler,
+        FindManyConsumersByUserIdAndIdsHandler,
+        IsConsumerExistsByUserIdAndIdHandler,
+        ConsumerService,
         DeleteManyBillsConsumersHandler,
         CreateConsumerHandler,
         CreateManyBillsConsumersHandler,
         FindManyBillConsumerTargetsByRefIdsHandler,
-        FindUserConsumerByRefIdAndTargetIdOrNullHandler,
-        FindUserConsumerTargetByRefIdAndTargetIdOrThrowHandler,
-        IsUserConsumerExistsByRefIdAndTargetIdsHandler,
         FindManyBillsConsumersByRefIdHandler,
-        FindUserConsumerTargetListByRefIdHandler,
-        FindConsumerByNameOrNullHandler,
         ConsumerRepository,
-        UserConsumerRepository,
         BillConsumerRepository,
     ],
 })
