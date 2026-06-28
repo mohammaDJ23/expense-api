aa86854021e17b91e9b915b410a85fada84256d7
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-06-28 11:02:08 +0330

    chore: 700990 update some names

--- src/modules/bill/interface/controllers/controllers.constants.ts
+++ src/modules/bill/interface/controllers/controllers.constants.ts
@@ -1,4 +1,4 @@
 export const SUCCESS_CREATE_BILL_MESSAGE = 'A new bill created successfully';
 export const SUCCESS_UPDATE_BILL_MESSAGE = 'The update operation was successful';
-export const SUCCESS_FIND_MANY_MESSAGE = 'You have received the bills successfully';
+export const SUCCESS_FIND_BILLS_MESSAGE = 'You have received the bills successfully';
 export const SUCCESS_FIND_BILL_MESSAGE = 'You have received the bill successfully';
