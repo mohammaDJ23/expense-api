149ca18b3d5a37814be86c417d0d2b9eacf8b88c
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-07-02 13:13:37 +0330

    chore: 686199 add the events inside the receiver modules

--- src/core/interfaces/cqrs/createCqrs.interface.ts
+++ src/core/interfaces/cqrs/createCqrs.interface.ts
@@ -1,8 +1,6 @@
 interface ICreateEntity {
     id?: string;
     createdAt?: string;
-    updatedAt?: string;
 }
 
-export type TCreate<T extends ICreateEntity> = Omit<T, 'id'> &
-    Required<Pick<T, 'createdAt' | 'updatedAt'>>;
+export type TCreate<T extends ICreateEntity> = Omit<T, 'id'> & Required<Pick<T, 'createdAt'>>;
--- src/modules/receiver/applications/services/updateReceiver.service.ts
+++ src/modules/receiver/applications/services/updateReceiver.service.ts
@@ -1,13 +1,17 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { Transactional } from '@nestjs-cls/transactional';
 
 import { getCurrentUTCTimestamp } from '@/common/utils/getCurrentUTCTimestamp.util';
 import { IdEntity } from '@/core/entities/id.entity';
+import { CreateOutboxEventCommand } from '@/modules/outbox/applications/commands/createOutboxEvent/createOutboxEvent.command';
 import { UpdateReceiverCommand } from '@/modules/receiver/applications/commands/updateReceiver/updateReceiver.command';
 import { FindReceiverByUserIdAndNameOrNullQuery } from '@/modules/receiver/applications/queries/findReceiverByUserIdAndNameOrNull/findReceiverByUserIdAndNameOrNull.query';
 import { IsReceiverExistsByUserIdAndIdQuery } from '@/modules/receiver/applications/queries/isReceiverExistsByUserIdAndId/isReceiverExistsByUserIdAndId.query';
 
 import type { IServiceHandler } from '@/core/interfaces/serviceHandler.interface';
+import type { ISelectOutboxEvent } from '@/modules/outbox/infrastructure/schemas/outboxEvent.schema';
+import type { IReceiverOutboxEvent } from '@/modules/receiver/domain/interfaces/receiverOutboxEvent.interface';
 import type { ISelectReceiver } from '@/modules/receiver/infrastructure/schemas/receiver.schema';
 import type { UpdateReceiverRequestDto } from '@/modules/receiver/interfaces/dtos/updateReceiver.request.dto';
 
@@ -18,6 +22,7 @@ export class UpdateReceiverService implements IServiceHandler {
         private readonly commandBus: CommandBus,
     ) {}
 
+    @Transactional()
     async execute(userId: string, data: UpdateReceiverRequestDto): Promise<IdEntity> {
         {
             const [isExists, foundedByName] = await Promise.all([
@@ -51,6 +56,20 @@ export class UpdateReceiverService implements IServiceHandler {
                     updatedAt: getCurrentUTCTimestamp(),
                 }),
             );
+
+            await this.commandBus.execute<
+                CreateOutboxEventCommand<IReceiverOutboxEvent>,
+                ISelectOutboxEvent
+            >(
+                new CreateOutboxEventCommand<IReceiverOutboxEvent>({
+                    aggregateId: updatedReceiver.id,
+                    aggregateType: 'receivers',
+                    eventType: 'updated',
+                    payload: updatedReceiver,
+                    createdAt: getCurrentUTCTimestamp(),
+                }),
+            );
+
             return IdEntity.create(updatedReceiver.id);
         }
     }
