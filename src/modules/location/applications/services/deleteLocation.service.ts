1d44081d5951a8b1fb81ea32750a8283e1bf6f82
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-07-02 13:29:53 +0330

    chore: 686199 add the events inside the location modules

--- src/modules/location/applications/services/deleteLocation.service.ts
+++ src/modules/location/applications/services/deleteLocation.service.ts
@@ -1,12 +1,17 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { Transactional } from '@nestjs-cls/transactional';
 
+import { getCurrentUTCTimestamp } from '@/common/utils/getCurrentUTCTimestamp.util';
 import { IdEntity } from '@/core/entities/id.entity';
 import { DeleteLocationCommand } from '@/modules/location/applications/commands/deleteLocation/deleteLocation.command';
 import { IsLocationExistsByUserIdAndIdQuery } from '@/modules/location/applications/queries/isLocationExistsByUserIdAndId/isLocationExistsByUserIdAndId.query';
+import { CreateOutboxEventCommand } from '@/modules/outbox/applications/commands/createOutboxEvent/createOutboxEvent.command';
 
 import type { IServiceHandler } from '@/core/interfaces/serviceHandler.interface';
+import type { ILocationOutboxEvent } from '@/modules/location/domain/interfaces/locationOutboxEvent.interface';
 import type { ISelectLocation } from '@/modules/location/infrastructure/schemas/location.schema';
+import type { ISelectOutboxEvent } from '@/modules/outbox/infrastructure/schemas/outboxEvent.schema';
 
 @Injectable()
 export class DeleteLocationService implements IServiceHandler {
@@ -15,6 +20,7 @@ export class DeleteLocationService implements IServiceHandler {
         private readonly commandBus: CommandBus,
     ) {}
 
+    @Transactional()
     async execute(userId: string, locationId: string): Promise<IdEntity> {
         {
             const isExists = await this.queryBus.execute<
@@ -32,6 +38,20 @@ export class DeleteLocationService implements IServiceHandler {
                 DeleteLocationCommand,
                 ISelectLocation
             >(new DeleteLocationCommand(userId, locationId));
+
+            await this.commandBus.execute<
+                CreateOutboxEventCommand<ILocationOutboxEvent>,
+                ISelectOutboxEvent
+            >(
+                new CreateOutboxEventCommand<ILocationOutboxEvent>({
+                    aggregateId: deletedLocation.id,
+                    aggregateType: 'locations',
+                    eventType: 'deleted',
+                    payload: deletedLocation,
+                    createdAt: getCurrentUTCTimestamp(),
+                }),
+            );
+
             return IdEntity.create(deletedLocation.id);
         }
     }
--- src/modules/location/applications/services/updateLocation.service.ts
+++ src/modules/location/applications/services/updateLocation.service.ts
@@ -1,15 +1,19 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { Transactional } from '@nestjs-cls/transactional';
 
 import { getCurrentUTCTimestamp } from '@/common/utils/getCurrentUTCTimestamp.util';
 import { IdEntity } from '@/core/entities/id.entity';
 import { UpdateLocationCommand } from '@/modules/location/applications/commands/updateLocation/updateLocation.command';
 import { FindLocationByUserIdAndNameOrNullQuery } from '@/modules/location/applications/queries/findLocationByUserIdAndNameOrNull/findLocationByUserIdAndNameOrNull.query';
 import { IsLocationExistsByUserIdAndIdQuery } from '@/modules/location/applications/queries/isLocationExistsByUserIdAndId/isLocationExistsByUserIdAndId.query';
+import { CreateOutboxEventCommand } from '@/modules/outbox/applications/commands/createOutboxEvent/createOutboxEvent.command';
 
 import type { IServiceHandler } from '@/core/interfaces/serviceHandler.interface';
+import type { ILocationOutboxEvent } from '@/modules/location/domain/interfaces/locationOutboxEvent.interface';
 import type { ISelectLocation } from '@/modules/location/infrastructure/schemas/location.schema';
 import type { UpdateLocationRequestDto } from '@/modules/location/interfaces/dtos/updateLocation.request.dto';
+import type { ISelectOutboxEvent } from '@/modules/outbox/infrastructure/schemas/outboxEvent.schema';
 
 @Injectable()
 export class UpdateLocationService implements IServiceHandler {
@@ -18,6 +22,7 @@ export class UpdateLocationService implements IServiceHandler {
         private readonly commandBus: CommandBus,
     ) {}
 
+    @Transactional()
     async execute(userId: string, data: UpdateLocationRequestDto): Promise<IdEntity> {
         {
             const [isExists, foundedByName] = await Promise.all([
@@ -51,6 +56,20 @@ export class UpdateLocationService implements IServiceHandler {
                     updatedAt: getCurrentUTCTimestamp(),
                 }),
             );
+
+            await this.commandBus.execute<
+                CreateOutboxEventCommand<ILocationOutboxEvent>,
+                ISelectOutboxEvent
+            >(
+                new CreateOutboxEventCommand<ILocationOutboxEvent>({
+                    aggregateId: updatedLocation.id,
+                    aggregateType: 'locations',
+                    eventType: 'updated',
+                    payload: updatedLocation,
+                    createdAt: getCurrentUTCTimestamp(),
+                }),
+            );
+
             return IdEntity.create(updatedLocation.id);
         }
     }
