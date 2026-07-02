8acd070f5256518ffc5fc12f846bd05df6287966
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-07-02 13:16:07 +0330

    chore: 686199 remove cqrs types

--- src/core/interfaces/cqrs/createCqrs.interface.ts
+++
@@ -1,6 +0,0 @@
-interface ICreateEntity {
-    id?: string;
-    createdAt?: string;
-}
-
-export type TCreate<T extends ICreateEntity> = Omit<T, 'id'> & Required<Pick<T, 'createdAt'>>;
--- src/core/interfaces/cqrs/deleteCqrs.interface.ts
+++
@@ -1,5 +0,0 @@
-interface IDeleteEntity {
-    id?: string;
-}
-
-export type TDelete<T extends IDeleteEntity> = Pick<T, 'id'>;
