3d24f8fc7fd9aabba95d71e7bc64de9b8b799d06
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-07-02 13:16:07 +0330

    chore: 686199 remove cqrs types

--- src/core/interfaces/cqrs/updateCqrs.interface.ts
+++
@@ -1,7 +0,0 @@
-interface IUpdateEntity {
-    id?: string;
-    updatedAt?: string;
-}
-
-export type TUpdate<T extends IUpdateEntity> = Pick<T, 'id' | 'updatedAt'> &
-    Partial<Omit<T, 'id' | 'updatedAt'>>;
--- src/modules/outbox/applications/commands/createOutboxEvent/createOutboxEvent.command.ts
+++ src/modules/outbox/applications/commands/createOutboxEvent/createOutboxEvent.command.ts
@@ -1,6 +1,5 @@
-import type { TCreate } from '@/core/interfaces/cqrs/createCqrs.interface';
 import type { IInsertOutboxEvent } from '@/modules/outbox/infrastructure/schemas/outboxEvent.schema';
 
 export class CreateOutboxEventCommand<T extends IInsertOutboxEvent = IInsertOutboxEvent> {
-    constructor(public readonly props: TCreate<T>) {}
+    constructor(public readonly props: Omit<T, 'id'>) {}
 }
