0f1f24d9ab72476b8c08582cae48d420db72bc5f
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-07-02 12:01:30 +0330

    chore: 686199 add the updatedAt to ICreateEntity

--- src/core/interfaces/cqrs/createCqrs.interface.ts
+++ src/core/interfaces/cqrs/createCqrs.interface.ts
@@ -1,6 +1,8 @@
 interface ICreateEntity {
     id?: string;
     createdAt?: string;
+    updatedAt?: string;
 }
 
-export type TCreate<T extends ICreateEntity> = Omit<T, 'id'> & Required<Pick<T, 'createdAt'>>;
+export type TCreate<T extends ICreateEntity> = Omit<T, 'id'> &
+    Required<Pick<T, 'createdAt' | 'updatedAt'>>;
