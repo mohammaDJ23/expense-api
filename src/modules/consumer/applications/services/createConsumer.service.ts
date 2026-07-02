f1d89284c41280184c292015bef8458cd73e27b9
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-07-02 13:36:52 +0330

    chore: 686199 add the events inside the consumer module

--- src/modules/consumer/applications/services/createConsumer.service.ts
+++ src/modules/consumer/applications/services/createConsumer.service.ts
@@ -1,13 +1,17 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { Transactional } from '@nestjs-cls/transactional';
 
 import { getCurrentUTCTimestamp } from '@/common/utils/getCurrentUTCTimestamp.util';
 import { IdEntity } from '@/core/entities/id.entity';
 import { CreateConsumerCommand } from '@/modules/consumer/applications/commands/createConsumer/createConsumer.command';
 import { FindConsumerByUserIdAndNameOrNullQuery } from '@/modules/consumer/applications/queries/findConsumerByUserIdAndNameOrNull/findConsumerByUserIdAndNameOrNull.query';
+import { CreateOutboxEventCommand } from '@/modules/outbox/applications/commands/createOutboxEvent/createOutboxEvent.command';
 
 import type { IServiceHandler } from '@/core/interfaces/serviceHandler.interface';
+import type { IConsumerOutboxEvent } from '@/modules/consumer/domain/interfaces/consumerOutboxEvent.interface';
 import type { ISelectConsumer } from '@/modules/consumer/infrastructure/schemas/consumer.schema';
+import type { ISelectOutboxEvent } from '@/modules/outbox/infrastructure/schemas/outboxEvent.schema';
 
 @Injectable()
 export class CreateConsumerService implements IServiceHandler {
@@ -16,6 +20,7 @@ export class CreateConsumerService implements IServiceHandler {
         private readonly commandBus: CommandBus,
     ) {}
 
+    @Transactional()
     async execute(userId: string, name: string): Promise<IdEntity> {
         const consumer = await this.queryBus.execute<
             FindConsumerByUserIdAndNameOrNullQuery,
@@ -34,6 +39,20 @@ export class CreateConsumerService implements IServiceHandler {
                     updatedAt: getCurrentUTCTimestamp(),
                 }),
             );
+
+            await this.commandBus.execute<
+                CreateOutboxEventCommand<IConsumerOutboxEvent>,
+                ISelectOutboxEvent
+            >(
+                new CreateOutboxEventCommand<IConsumerOutboxEvent>({
+                    aggregateId: createdConsumer.id,
+                    aggregateType: 'consumers',
+                    eventType: 'created',
+                    payload: createdConsumer,
+                    createdAt: getCurrentUTCTimestamp(),
+                }),
+            );
+
             return IdEntity.create(createdConsumer.id);
         }
 
--- src/modules/consumer/applications/services/deleteConsumer.service.ts
+++ src/modules/consumer/applications/services/deleteConsumer.service.ts
@@ -1,12 +1,17 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { Transactional } from '@nestjs-cls/transactional';
 
+import { getCurrentUTCTimestamp } from '@/common/utils/getCurrentUTCTimestamp.util';
 import { IdEntity } from '@/core/entities/id.entity';
 import { DeleteConsumerCommand } from '@/modules/consumer/applications/commands/deleteConsumer/deleteConsumer.command';
 import { IsConsumerExistsByUserIdAndIdQuery } from '@/modules/consumer/applications/queries/isConsumerExistsByUserIdAndId/isConsumerExistsByUserIdAndId.query';
+import { CreateOutboxEventCommand } from '@/modules/outbox/applications/commands/createOutboxEvent/createOutboxEvent.command';
 
 import type { IServiceHandler } from '@/core/interfaces/serviceHandler.interface';
+import type { IConsumerOutboxEvent } from '@/modules/consumer/domain/interfaces/consumerOutboxEvent.interface';
 import type { ISelectConsumer } from '@/modules/consumer/infrastructure/schemas/consumer.schema';
+import type { ISelectOutboxEvent } from '@/modules/outbox/infrastructure/schemas/outboxEvent.schema';
 
 @Injectable()
 export class DeleteConsumerService implements IServiceHandler {
@@ -15,6 +20,7 @@ export class DeleteConsumerService implements IServiceHandler {
         private readonly commandBus: CommandBus,
     ) {}
 
+    @Transactional()
     async execute(userId: string, consumerId: string): Promise<IdEntity> {
         {
             const isExists = await this.queryBus.execute<
@@ -32,6 +38,20 @@ export class DeleteConsumerService implements IServiceHandler {
                 DeleteConsumerCommand,
                 ISelectConsumer
             >(new DeleteConsumerCommand(userId, consumerId));
+
+            await this.commandBus.execute<
+                CreateOutboxEventCommand<IConsumerOutboxEvent>,
+                ISelectOutboxEvent
+            >(
+                new CreateOutboxEventCommand<IConsumerOutboxEvent>({
+                    aggregateId: deletedConsumer.id,
+                    aggregateType: 'consumers',
+                    eventType: 'deleted',
+                    payload: deletedConsumer,
+                    createdAt: getCurrentUTCTimestamp(),
+                }),
+            );
+
             return IdEntity.create(deletedConsumer.id);
         }
     }
