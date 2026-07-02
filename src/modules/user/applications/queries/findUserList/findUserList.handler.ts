307028d7af65ea81375726ab03a84053a0abb364
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-07-02 16:28:55 +0330

    chore: 339587 update the FindUserListQuery arg

--- src/modules/user/applications/queries/findUserList/findUserList.handler.ts
+++ src/modules/user/applications/queries/findUserList/findUserList.handler.ts
@@ -13,7 +13,7 @@ export class FindUserListHandler implements IQueryHandler<FindUserListQuery, ISe
 
     async execute(query: FindUserListQuery): Promise<ISelectUser[]> {
         try {
-            return await this.userRepository.findList(query);
+            return await this.userRepository.findList(query.props);
         } catch {
             throw new ProcessFailedInternalServerErrorException();
         }
