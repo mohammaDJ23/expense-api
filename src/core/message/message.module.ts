66647fbcc74742e6eadd62d52674f68550c53dda
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-07-05 08:26:32 +0330

    chore: 287588 add discovery service into message and remove from kafka

--- src/core/message/message.module.ts
+++ src/core/message/message.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { DiscoveryService } from '@nestjs/core';
 
 import { MessageDiscoveryService } from '@/core/message/messageDiscovery.service';
 import { MessageRegistryService } from '@/core/message/messageRegistry.service';
 
 @Module({
-    providers: [MessageDiscoveryService, MessageRegistryService],
+    providers: [MessageDiscoveryService, MessageRegistryService, DiscoveryService],
     exports: [MessageRegistryService],
 })
 export class MessageModule {}
