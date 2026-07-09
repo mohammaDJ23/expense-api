bc69c4aed6143cd6046a17cae5bb195e462db82d
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-07-10 00:42:17 +0330

    chore: 157169 add the update location message

---
+++ src/modules/location/applications/messages/updateLocation/updateLocationMessageElasticsearch.processor.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+
+import { ElasticSearchService } from '@/infrastructure/elasticsearch/elasticsearch.service';
+
+import type { IMessageBatch } from '@/core/message/messageBatch.interface';
+import type { IMessageProcessor } from '@/core/message/messageProcessor.interface';
+import type { ISelectLocation } from '@/modules/location/infrastructure/schemas/location.schema';
+import type { estypes } from '@elastic/elasticsearch';
+
+@Injectable()
+export class UpdateLocationMessageElasticsearchProcessor implements IMessageProcessor<ISelectLocation> {
+    constructor(private readonly elasticsearchService: ElasticSearchService) {}
+
+    async process(batch: IMessageBatch<ISelectLocation>[]): Promise<void> {
+        const operations = batch.flatMap<estypes.BulkOperationContainer | ISelectLocation>(
+            (item) => [
+                {
+                    index: {
+                        _index: item.aggregateType,
+                        _id: item.aggregateId,
+                    },
+                },
+                item.payload,
+            ],
+        );
+
+        await this.elasticsearchService.bulk({ operations });
+    }
+}
