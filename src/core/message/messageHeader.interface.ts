93ec7282b5f73fe39cb46a94b8948d8ea9eb4436
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-07-09 10:35:24 +0330

    chore: 157169 use TOutboxEventType

--- src/core/message/messageHeader.interface.ts
+++ src/core/message/messageHeader.interface.ts
@@ -1,6 +1,8 @@
+import type { TOutboxEventType } from '@/modules/outbox/domain/interfaces/outboxEventType.interface';
+
 export interface IMessageHeader {
     aggregateType: string;
     aggregateId: string;
-    eventType: string;
+    eventType: TOutboxEventType;
     createdAt: string;
 }
