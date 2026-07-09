5317603b1b5dc6b530af12b6956b40ce5fad89e7
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-07-09 13:12:04 +0330

    chore: 157169 defining each fields

--- src/modules/outbox/infrastructure/schemas/outboxEvent.schema.ts
+++ src/modules/outbox/infrastructure/schemas/outboxEvent.schema.ts
@@ -1,19 +1,25 @@
 import { sql } from 'drizzle-orm';
 import { jsonb, pgTable, timestamp, uuid, varchar } from 'drizzle-orm/pg-core';
 
+import type { TOutboxEventAggregateType } from '@/modules/outbox/domain/interfaces/outboxEventAggregateType.interface';
 import type { TOutboxEventPayload } from '@/modules/outbox/domain/interfaces/outboxEventPayload.interface';
+import type { TOutboxEventRoute } from '@/modules/outbox/domain/interfaces/outboxEventRoute.interface';
+import type { TOutboxEventType } from '@/modules/outbox/domain/interfaces/outboxEventType.interface';
 
 export const outboxEvents = pgTable('outbox_events', {
     id: uuid('id').primaryKey().defaultRandom().notNull(),
-    aggregateType: varchar('aggregate_type', { length: 100 }).notNull(),
+    aggregateType: varchar('aggregate_type', { length: 100 })
+        .notNull()
+        .$type<TOutboxEventAggregateType>(),
     aggregateId: uuid('aggregate_id').notNull(),
-    eventType: varchar('event_type', { length: 150 }).notNull(),
+    eventType: varchar('event_type', { length: 150 }).notNull().$type<TOutboxEventType>(),
     route: varchar('route', { length: 255 })
         .notNull()
         .generatedAlwaysAs(
             () => sql`${outboxEvents.aggregateType} || '.' || ${outboxEvents.eventType}`,
-        ),
-    payload: jsonb('payload').$type<TOutboxEventPayload>().notNull(),
+        )
+        .$type<TOutboxEventRoute>(),
+    payload: jsonb('payload').notNull().$type<TOutboxEventPayload>(),
     createdAt: timestamp('created_at', { withTimezone: true, mode: 'string' }).notNull(),
 });
 
