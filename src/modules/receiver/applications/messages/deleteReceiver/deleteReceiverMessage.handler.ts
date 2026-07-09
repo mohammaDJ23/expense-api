aebae4f800500cce0fd256cc3fdb3adb47bd85d0
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-07-09 23:50:52 +0330

    chore: 157169 add deleteReceiver message

---
+++ src/modules/receiver/applications/messages/deleteReceiver/deleteReceiverMessage.handler.ts
@@ -0,0 +1,25 @@
+import { MessageHandler } from '@/core/message/messageHandler.decorator';
+
+import { DeleteReceiverMessageElasticsearchProcessor } from './deleteReceiverMessageElasticsearch.processor';
+
+import type { IMessageBatch } from '@/core/message/messageBatch.interface';
+import type { IMessageHandler } from '@/core/message/messageHandler.interface';
+import type { IMessageProcessor } from '@/core/message/messageProcessor.interface';
+import type { TOutboxEventRoute } from '@/modules/outbox/domain/interfaces/outboxEventRoute.interface';
+import type { ISelectReceiver } from '@/modules/receiver/infrastructure/schemas/receiver.schema';
+
+@MessageHandler()
+export class DeleteReceiverMessageHandler implements IMessageHandler<ISelectReceiver> {
+    route: TOutboxEventRoute = 'receivers.deleted';
+
+    constructor(
+        private readonly deleteReceiverMessageElasticsearch: DeleteReceiverMessageElasticsearchProcessor,
+    ) {}
+
+    async execute(batch: IMessageBatch<ISelectReceiver>[]): Promise<void> {
+        const processors: IMessageProcessor<ISelectReceiver>[] = [
+            this.deleteReceiverMessageElasticsearch,
+        ];
+        await Promise.all(processors.map((processor) => processor.process(batch)));
+    }
+}
