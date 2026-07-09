4370ededd5a847f54d3ea91f2c7eee120438aceb
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-07-09 13:13:06 +0330

    chore: 157169 remove the extra type of outbox

--- src/modules/receiver/domain/interfaces/receiverOutboxEvent.interface.ts
+++ src/modules/receiver/domain/interfaces/receiverOutboxEvent.interface.ts
@@ -1,12 +1,4 @@
 import type { IInsertOutboxEvent } from '@/modules/outbox/infrastructure/schemas/outboxEvent.schema';
 import type { ISelectReceiver } from '@/modules/receiver/infrastructure/schemas/receiver.schema';
 
-type TReceiverEventType = 'created' | 'updated' | 'deleted';
-
-export interface IReceiverOutboxEvent extends Omit<
-    IInsertOutboxEvent<ISelectReceiver>,
-    'aggregateType' | 'eventType'
-> {
-    aggregateType: 'receivers';
-    eventType: TReceiverEventType;
-}
+export interface IReceiverOutboxEvent extends IInsertOutboxEvent<ISelectReceiver> {}
