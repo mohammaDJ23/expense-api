09aed9c7022a36a4744ad5da9303ff5d015dc7ba
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-07-09 09:30:21 +0330

    chore: 157169 create the message of create receiver

--- src/modules/receiver/applications/messages/createReceiver/createReceiverMessage.handler.ts
+++ src/modules/receiver/applications/messages/createReceiver/createReceiverMessage.handler.ts
@@ -1,7 +1,10 @@
 import { MessageHandler } from '@/core/message/messageHandler.decorator';
 
+import { CreateReceiverMessageElasticsearchProcessor } from './createReceiverMessageElasticsearch.processor';
+
 import type { IMessageBatch } from '@/core/message/messageBatch.interface';
 import type { IMessageHandler } from '@/core/message/messageHandler.interface';
+import type { IMessageProcessor } from '@/core/message/messageProcessor.interface';
 import type { TOutboxEventAggregateType } from '@/modules/outbox/domain/interfaces/outboxEventAggregateType.interface';
 import type { ISelectReceiver } from '@/modules/receiver/infrastructure/schemas/receiver.schema';
 
@@ -9,7 +12,14 @@ import type { ISelectReceiver } from '@/modules/receiver/infrastructure/schemas/
 export class CreateReceiverMessageHandler implements IMessageHandler<ISelectReceiver> {
     aggregateType: TOutboxEventAggregateType = 'receivers';
 
+    constructor(
+        private readonly createReceiverMessageElasticsearch: CreateReceiverMessageElasticsearchProcessor,
+    ) {}
+
     async execute(batch: IMessageBatch<ISelectReceiver>[]): Promise<void> {
-        await Promise.allSettled([]);
+        const processors: IMessageProcessor<ISelectReceiver>[] = [
+            this.createReceiverMessageElasticsearch,
+        ];
+        await Promise.all(processors.map((processor) => processor.process(batch)));
     }
 }
--- src/modules/receiver/receiver.module.ts
+++ src/modules/receiver/receiver.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { AuthenticationModule } from '@/core/authentication/authentication.module';
 import { CqrsModule } from '@/infrastructure/cqrs/cqrs.module';
+import { ElasticsearchModule } from '@/infrastructure/elasticsearch/elasticsearch.module';
 import { CreateReceiverHandler } from '@/modules/receiver/applications/commands/createReceiver/createReceiver.handler';
 import { DeleteReceiverHandler } from '@/modules/receiver/applications/commands/deleteReceiver/deleteReceiver.handler';
 import { UpdateReceiverHandler } from '@/modules/receiver/applications/commands/updateReceiver/updateReceiver.handler';
+import { CreateReceiverMessageHandler } from '@/modules/receiver/applications/messages/createReceiver/createReceiverMessage.handler';
+import { CreateReceiverMessageElasticsearchProcessor } from '@/modules/receiver/applications/messages/createReceiver/createReceiverMessageElasticsearch.processor';
 import { ExistsReceiverByUserIdAndExcludingIdAndNameHandler } from '@/modules/receiver/applications/queries/existsReceiverByUserIdAndExcludingIdAndName/existsReceiverByUserIdAndExcludingIdAndName.handler';
 import { ExistsReceiverByUserIdAndIdHandler } from '@/modules/receiver/applications/queries/existsReceiverByUserIdAndId/existsReceiverByUserIdAndId.handler';
 import { FindManyReceiversByUserIdAndIdsHandler } from '@/modules/receiver/applications/queries/findManyReceiversByUserIdAndIds/findManyReceiversByUserIdAndIds.handler';
@@ -20,7 +23,7 @@ import { ReceiverRepository } from '@/modules/receiver/infrastructure/repositori
 import { ReceiverController } from '@/modules/receiver/interfaces/controllers/v1.controller';
 
 @Module({
-    imports: [CqrsModule, AuthenticationModule],
+    imports: [CqrsModule, AuthenticationModule, ElasticsearchModule],
     controllers: [ReceiverController],
     providers: [
         CreateReceiverHandler,
@@ -33,6 +36,8 @@ import { ReceiverController } from '@/modules/receiver/interfaces/controllers/v1
         FindReceiverListByUserIdHandler,
         ExistsReceiverByUserIdAndIdHandler,
         ExistsReceiverByUserIdAndExcludingIdAndNameHandler,
+        CreateReceiverMessageElasticsearchProcessor,
+        CreateReceiverMessageHandler,
         ReceiverService,
         CreateReceiverService,
         UpdateReceiverService,
