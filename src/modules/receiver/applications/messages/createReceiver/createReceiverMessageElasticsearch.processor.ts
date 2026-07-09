50e8228dc1c04ce40e19cd726cede76f745e4963
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-07-09 09:30:21 +0330

    chore: 157169 create the message of create receiver

---
+++ src/modules/receiver/applications/messages/createReceiver/createReceiverMessageElasticsearch.processor.ts
@@ -0,0 +1,38 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+
+import { isNotEmpty } from '@/common/utils/isNotEmpty.util';
+import { ElasticSearchService } from '@/infrastructure/elasticsearch/elasticsearch.service';
+
+import type { IMessageBatch } from '@/core/message/messageBatch.interface';
+import type { IMessageProcessor } from '@/core/message/messageProcessor.interface';
+import type { ISelectReceiver } from '@/modules/receiver/infrastructure/schemas/receiver.schema';
+import type { estypes } from '@elastic/elasticsearch';
+
+@Injectable()
+export class CreateReceiverMessageElasticsearchProcessor implements IMessageProcessor<ISelectReceiver> {
+    constructor(private readonly elasticsearchService: ElasticSearchService) {}
+
+    async process(batch: IMessageBatch<ISelectReceiver>[]): Promise<void> {
+        try {
+            const operations = batch.flatMap<estypes.BulkOperationContainer | ISelectReceiver>(
+                (item) => [
+                    {
+                        index: {
+                            _index: item.aggregateType,
+                            _id: item.aggregateId,
+                        },
+                    },
+                    item.payload,
+                ],
+            );
+
+            if (isNotEmpty(operations)) {
+                await this.elasticsearchService.client.bulk({
+                    operations,
+                });
+            }
+        } catch (error) {
+            throw new InternalServerErrorException(error);
+        }
+    }
+}
