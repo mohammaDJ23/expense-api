71fd76509c40928e555ce78136bf8b64a8152527
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-07-09 23:50:13 +0330

    chore: 157169 add the custom bulk to hanlde errors

--- src/infrastructure/elasticsearch/elasticsearch.service.ts
+++ src/infrastructure/elasticsearch/elasticsearch.service.ts
@@ -1,5 +1,6 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { Client } from '@elastic/elasticsearch';
+import { Inject, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Client, estypes } from '@elastic/elasticsearch';
+import { isNotEmpty } from 'class-validator';
 
 import { ELASTICSEARCH_PROVIDER } from './elasticsearch.constants';
 
@@ -12,5 +13,26 @@ export class ElasticSearchService {
 
     get client(): Client {
         return this.elasticsearch;
+    }
+
+    async bulk(operations: estypes.BulkOperationContainer[]): Promise<void> {
+        try {
+            if (isNotEmpty(operations)) {
+                const response = await this.client.bulk({
+                    operations,
+                });
+
+                if (response.errors) {
+                    const errors = response.items
+                        .flatMap(Object.values)
+                        .map((item) => item.error)
+                        .filter(Boolean);
+
+                    throw new Error(`Bulk request failed (${errors.length}/${operations.length})`);
+                }
+            }
+        } catch (error) {
+            throw new InternalServerErrorException(error);
+        }
     }
 }
