3d03d040a2e4515d3cefa66a349c30ae71492c52
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-07-11 13:47:25 -0700

    fix: 389615 fix the eslint issues

--- src/modules/user/interfaces/dtos/findUserList.request.dto.ts
+++ src/modules/user/interfaces/dtos/findUserList.request.dto.ts
@@ -1,6 +1,7 @@
-import { MAX_LIST_LIMIT, MIN_LIST_LIMIT } from '@/common/common.constants';
 import { Type } from 'class-transformer';
 import { IsOptional, IsInt, Min, IsString, Max } from 'class-validator';
+
+import { MAX_LIST_LIMIT, MIN_LIST_LIMIT } from '@/common/common.constants';
 
 export class FindUserListRequestDto {
     @IsOptional()
