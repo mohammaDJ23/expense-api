d3f8a7ea614abddaa5f20a65d60069c3c142c339
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-07-11 06:38:48 -0700

    chore: 389615 revise the elasticsearch difinition

--- src/modules/bill/infrastructure/elasticsearch/billElasticsearch.definition.ts
+++ src/modules/bill/infrastructure/elasticsearch/billElasticsearch.definition.ts
@@ -10,187 +10,189 @@ import type { estypes } from '@elastic/elasticsearch';
 export class BillElasticsearchDefinition implements IElasticsearchDefinition {
     index: TOutboxEventAggregateType = 'bills';
 
-    settings: estypes.IndicesIndexSettings = ElasticsearchAnalysisSettings.settings;
-
-    mappings: estypes.MappingTypeMapping = {
-        properties: {
-            userId: {
-                type: 'keyword',
-            },
-            amount: {
-                type: 'text',
-                fields: {
-                    partial: {
-                        type: 'text',
-                        analyzer: 'partial_index',
-                        search_analyzer: 'partial_search',
-                    },
-                },
-            },
-            description: {
-                type: 'text',
-                fields: {
-                    partial: {
-                        type: 'text',
-                        analyzer: 'partial_index',
-                        search_analyzer: 'partial_search',
-                    },
-                },
-            },
-            receiver: {
-                properties: {
-                    name: {
-                        type: 'text',
-                        fields: {
-                            partial: {
-                                type: 'text',
-                                analyzer: 'partial_index',
-                                search_analyzer: 'partial_search',
-                            },
-                        },
-                    },
-                },
-            },
-            location: {
-                properties: {
-                    name: {
-                        type: 'text',
-                        fields: {
-                            partial: {
-                                type: 'text',
-                                analyzer: 'partial_index',
-                                search_analyzer: 'partial_search',
-                            },
-                        },
-                    },
-                },
-            },
-            consumers: {
-                type: 'nested',
-                properties: {
-                    name: {
-                        type: 'text',
-                        fields: {
-                            partial: {
-                                type: 'text',
-                                analyzer: 'partial_index',
-                                search_analyzer: 'partial_search',
-                            },
-                        },
-                    },
-                },
-            },
-        },
-    };
-
-    createSearchQuery(userId: string, query: string): estypes.QueryDslQueryContainer {
+    buildIndex(): estypes.IndicesCreateRequest {
         return {
-            bool: {
-                filter: [
-                    {
-                        term: {
-                            userId,
-                        },
+            index: this.index,
+            settings: ElasticsearchAnalysisSettings.settings,
+            mappings: {
+                properties: {
+                    userId: {
+                        type: 'keyword',
                     },
-                ],
-                should: [
-                    {
-                        match: {
-                            amount: {
-                                query,
-                                boost: 5,
+                    amount: {
+                        type: 'keyword',
+                    },
+                    description: {
+                        type: 'text',
+                        fields: {
+                            partial: {
+                                type: 'text',
+                                analyzer: 'partial_index',
+                                search_analyzer: 'partial_search',
                             },
                         },
                     },
-                    {
-                        match: {
-                            description: {
-                                query,
-                                fuzziness: 'AUTO',
-                                boost: 5,
-                            },
-                        },
-                    },
-                    {
-                        match: {
-                            'amount.partial': {
-                                query,
-                                boost: 3,
-                            },
-                        },
-                    },
-                    {
-                        match: {
-                            'description.partial': {
-                                query,
-                                boost: 3,
-                            },
-                        },
-                    },
-                    {
-                        match: {
-                            'receiver.name': {
-                                query,
-                                fuzziness: 'AUTO',
-                                boost: 4,
-                            },
-                        },
-                    },
-                    {
-                        match: {
-                            'receiver.name.partial': {
-                                query,
-                                boost: 2,
-                            },
-                        },
-                    },
-                    {
-                        match: {
-                            'location.name': {
-                                query,
-                                fuzziness: 'AUTO',
-                                boost: 4,
-                            },
-                        },
-                    },
-                    {
-                        match: {
-                            'location.name.partial': {
-                                query,
-                                boost: 2,
-                            },
-                        },
-                    },
-                    {
-                        nested: {
-                            path: 'consumers',
-                            query: {
-                                bool: {
-                                    should: [
-                                        {
-                                            match: {
-                                                'consumers.name': {
-                                                    query,
-                                                    fuzziness: 'AUTO',
-                                                    boost: 4,
-                                                },
-                                            },
-                                        },
-                                        {
-                                            match: {
-                                                'consumers.name.partial': {
-                                                    query,
-                                                    boost: 2,
-                                                },
-                                            },
-                                        },
-                                    ],
+                    receiver: {
+                        type: 'object',
+                        properties: {
+                            name: {
+                                type: 'text',
+                                fields: {
+                                    partial: {
+                                        type: 'text',
+                                        analyzer: 'partial_index',
+                                        search_analyzer: 'partial_search',
+                                    },
                                 },
                             },
                         },
                     },
-                ],
-                minimum_should_match: 1,
+                    location: {
+                        type: 'object',
+                        properties: {
+                            name: {
+                                type: 'text',
+                                fields: {
+                                    partial: {
+                                        type: 'text',
+                                        analyzer: 'partial_index',
+                                        search_analyzer: 'partial_search',
+                                    },
+                                },
+                            },
+                        },
+                    },
+                    consumers: {
+                        type: 'nested',
+                        properties: {
+                            name: {
+                                type: 'text',
+                                fields: {
+                                    partial: {
+                                        type: 'text',
+                                        analyzer: 'partial_index',
+                                        search_analyzer: 'partial_search',
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        };
+    }
+
+    buildSearch(userId: string, query: string, size: number): estypes.SearchRequest {
+        return {
+            size,
+            index: this.index,
+            query: {
+                bool: {
+                    filter: [
+                        {
+                            term: {
+                                userId,
+                            },
+                        },
+                    ],
+                    must: [
+                        {
+                            bool: {
+                                minimum_should_match: 1,
+                                should: [
+                                    {
+                                        term: {
+                                            amount: {
+                                                value: query,
+                                                boost: 5,
+                                            },
+                                        },
+                                    },
+                                    {
+                                        match: {
+                                            description: {
+                                                query,
+                                                fuzziness: 'AUTO',
+                                                boost: 5,
+                                            },
+                                        },
+                                    },
+                                    {
+                                        match: {
+                                            'description.partial': {
+                                                query,
+                                                boost: 3,
+                                            },
+                                        },
+                                    },
+                                    {
+                                        match: {
+                                            'receiver.name': {
+                                                query,
+                                                fuzziness: 'AUTO',
+                                                boost: 4,
+                                            },
+                                        },
+                                    },
+                                    {
+                                        match: {
+                                            'receiver.name.partial': {
+                                                query,
+                                                boost: 2,
+                                            },
+                                        },
+                                    },
+                                    {
+                                        match: {
+                                            'location.name': {
+                                                query,
+                                                fuzziness: 'AUTO',
+                                                boost: 4,
+                                            },
+                                        },
+                                    },
+                                    {
+                                        match: {
+                                            'location.name.partial': {
+                                                query,
+                                                boost: 2,
+                                            },
+                                        },
+                                    },
+                                    {
+                                        nested: {
+                                            path: 'consumers',
+                                            query: {
+                                                bool: {
+                                                    minimum_should_match: 1,
+                                                    should: [
+                                                        {
+                                                            match: {
+                                                                'consumers.name': {
+                                                                    query,
+                                                                    fuzziness: 'AUTO',
+                                                                    boost: 4,
+                                                                },
+                                                            },
+                                                        },
+                                                        {
+                                                            match: {
+                                                                'consumers.name.partial': {
+                                                                    query,
+                                                                    boost: 2,
+                                                                },
+                                                            },
+                                                        },
+                                                    ],
+                                                },
+                                            },
+                                        },
+                                    },
+                                ],
+                            },
+                        },
+                    ],
+                },
             },
         };
     }
