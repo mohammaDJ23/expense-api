97cfa1d28459ea06ef8cdb437b07c9e45c2d8755
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-07-11 00:08:12 -0700

    chore: 389615 get the actual data from db

--- src/modules/search/applications/services/search.service.ts
+++ src/modules/search/applications/services/search.service.ts
@@ -12,31 +12,39 @@ import type { IServiceHandler } from '@/core/interfaces/serviceHandler.interface
 import type { IElasticsearchDefinition } from '@/infrastructure/elasticsearch/elasticsearchDefinition.interface';
 import type { ISearch } from '@/modules/search/domain/interface/search.interface';
 import type { SearchRequestDto } from '@/modules/search/interfaces/dtos/search.request.dto';
+import type { estypes } from '@elastic/elasticsearch';
+import type { ISelectBill } from '@/modules/bill/infrastructure/schemas/bill.schema';
+import type { ISelectConsumer } from '@/modules/consumer/infrastructure/schemas/consumer.schema';
+import type { ISelectReceiver } from '@/modules/receiver/infrastructure/schemas/receiver.schema';
+import type { ISelectLocation } from '@/modules/location/infrastructure/schemas/location.schema';
+import { whenNotEmpty } from '@/common/utils/whenNotEmpty.util';
+import { QueryBus } from '@nestjs/cqrs';
+import { FindManyConsumersByUserIdAndIdsQuery } from '@/modules/consumer/applications/queries/findManyConsumersByUserIdAndIds/findManyConsumersByUserIdAndIds.query';
+import { FindManyReceiversByUserIdAndIdsQuery } from '@/modules/receiver/applications/queries/findManyReceiversByUserIdAndIds/findManyReceiversByUserIdAndIds.query';
+import { FindManyLocationsByUserIdAndIdsQuery } from '@/modules/location/applications/queries/findManyLocationsByUserIdAndIds/findManyLocationsByUserIdAndIds.query';
 
 @Injectable()
 export class SearchService implements IServiceHandler, OnModuleInit {
-    private readonly definitions: IElasticsearchDefinition[];
-
     // eslint-disable-next-line max-params
     constructor(
+        private readonly queryBus: QueryBus,
         private readonly elasticsearchService: ElasticSearchService,
         private readonly billElasticsearchDefinition: BillElasticsearchDefinition,
         private readonly consumerElasticsearchDefinition: ConsumerElasticsearchDefinition,
         private readonly receiverElasticsearchDefinition: ReceiverElasticsearchDefinition,
         private readonly locationElasticsearchDefinition: LocationElasticsearchDefinition,
-    ) {
-        this.definitions = [
-            this.billElasticsearchDefinition,
-            this.consumerElasticsearchDefinition,
-            this.receiverElasticsearchDefinition,
-            this.locationElasticsearchDefinition,
-        ];
-    }
+    ) {}
 
     async onModuleInit(): Promise<void> {
         try {
+            const definitions = [
+                this.billElasticsearchDefinition,
+                this.consumerElasticsearchDefinition,
+                this.receiverElasticsearchDefinition,
+                this.locationElasticsearchDefinition,
+            ];
             await Promise.all(
-                this.definitions.map((definition) =>
+                definitions.map((definition) =>
                     this.elasticsearchService.client.indices.create(
                         {
                             index: definition.index,
@@ -52,31 +60,105 @@ export class SearchService implements IServiceHandler, OnModuleInit {
         }
     }
 
+    private getParams(data: SearchRequestDto): Required<SearchRequestDto> {
+        const q = data.q ?? '';
+        const limit = data.limit ?? MAX_LIST_LIMIT;
+
+        return { q, limit };
+    }
+
+    private search<T>(
+        definition: IElasticsearchDefinition,
+        userId: string,
+        params: Required<SearchRequestDto>,
+    ) {
+        return this.elasticsearchService.client.search<T>({
+            size: params.limit,
+            index: definition.index,
+            query: {
+                bool: {
+                    should: definition.createSearchQuery(userId, params.q),
+                },
+            },
+        });
+    }
+
+    private extractDocs<T>(response: estypes.SearchResponse<T>): T[] {
+        return response.hits.hits.flatMap((hit) => (hit._source ? [hit._source] : []));
+    }
+
     async execute(userId: string, data: SearchRequestDto): Promise<ISearch> {
         try {
-            const query = data.q ?? '';
-            const size = data.limit ?? MAX_LIST_LIMIT;
-
+            const params = this.getParams(data);
             const [
                 billsSearchResponse,
                 consumersSearchResponse,
                 receiversSearchResponse,
                 locationsSearchResponse,
-            ] = await Promise.all(
-                this.definitions.map((definition) =>
-                    this.elasticsearchService.client.search({
-                        size,
-                        index: definition.index,
-                        query: {
-                            bool: {
-                                should: definition.createSearchQuery(query),
-                            },
-                        },
-                    }),
-                ),
-            );
+            ] = await Promise.all([
+                this.search<ISelectBill>(this.billElasticsearchDefinition, userId, params),
+                this.search<ISelectConsumer>(this.consumerElasticsearchDefinition, userId, params),
+                this.search<ISelectReceiver>(this.receiverElasticsearchDefinition, userId, params),
+                this.search<ISelectLocation>(this.locationElasticsearchDefinition, userId, params),
+            ]);
 
-            return {};
+            {
+                const billDocs = this.extractDocs(billsSearchResponse);
+                const consumerDocs = this.extractDocs(consumersSearchResponse);
+                const receiverDocs = this.extractDocs(receiversSearchResponse);
+                const locationDocs = this.extractDocs(locationsSearchResponse);
+
+                const billIds = billDocs.map((doc) => doc.id);
+                const consumerIds = consumerDocs.map((doc) => doc.id);
+                const receiverIds = receiverDocs.map((doc) => doc.id);
+                const locationIds = locationDocs.map((doc) => doc.id);
+
+                const [bills, consumers, receivers, locations] = await Promise.all([
+                    // NOTE: bill should be implemented later.
+                    [],
+
+                    whenNotEmpty(consumerIds, (consumerIds) =>
+                        this.queryBus.execute<
+                            FindManyConsumersByUserIdAndIdsQuery,
+                            ISelectConsumer[]
+                        >(
+                            new FindManyConsumersByUserIdAndIdsQuery({
+                                userId,
+                                ids: consumerIds,
+                            }),
+                        ),
+                    ),
+                    whenNotEmpty(receiverIds, (receiverIds) =>
+                        this.queryBus.execute<
+                            FindManyReceiversByUserIdAndIdsQuery,
+                            ISelectReceiver[]
+                        >(
+                            new FindManyReceiversByUserIdAndIdsQuery({
+                                userId,
+                                ids: receiverIds,
+                            }),
+                        ),
+                    ),
+                    whenNotEmpty(locationIds, (locationIds) =>
+                        this.queryBus.execute<
+                            FindManyLocationsByUserIdAndIdsQuery,
+                            ISelectLocation[]
+                        >(
+                            new FindManyLocationsByUserIdAndIdsQuery({
+                                userId,
+                                ids: locationIds,
+                            }),
+                        ),
+                    ),
+                ]);
+
+                return {
+                    bills,
+                    consumers,
+                    locations,
+                    receivers,
+                };
+            }
         } catch {
             throw new ProcessFailedInternalServerErrorException();
         }
