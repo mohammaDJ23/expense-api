15dd2d1ae457ace6691a67daff955f0cf159f949
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-07-11 13:36:10 -0700

    chore: 389615 use MIN_LIST_LIMIT & MAX_LIST_LIMIT

--- src/common/common.constants.ts
+++ src/common/common.constants.ts
@@ -1,3 +1,4 @@
 export const PORT = 4000;
 
+export const MIN_LIST_LIMIT = 1;
 export const MAX_LIST_LIMIT = 20;
--- src/modules/search/interfaces/dtos/search.request.dto.ts
+++ src/modules/search/interfaces/dtos/search.request.dto.ts
@@ -1,13 +1,13 @@
 import { Type } from 'class-transformer';
 import { IsOptional, IsInt, Min, IsString, Max } from 'class-validator';
 
-import { MAX_LIST_LIMIT } from '@/common/common.constants';
+import { MAX_LIST_LIMIT, MIN_LIST_LIMIT } from '@/common/common.constants';
 
 export class SearchRequestDto {
     @IsOptional()
     @Type(() => Number)
     @IsInt()
-    @Min(0)
+    @Min(MIN_LIST_LIMIT)
     @Max(MAX_LIST_LIMIT)
     limit?: number;
 
