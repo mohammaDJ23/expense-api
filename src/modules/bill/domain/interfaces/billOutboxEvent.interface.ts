7506f2c9f82283ce86d14a695bf0dfaa85d18afb
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-07-16 15:08:52 +0330

    chore: 638860 remove the unused outbox events

--- src/modules/bill/domain/interfaces/billOutboxEvent.interface.ts
+++
@@ -1,5 +0,0 @@
-import type { IBill } from './bill.interface';
-import type { ISelectBill } from '@/modules/bill/infrastructure/schemas/bill.schema';
-import type { IInsertOutboxEvent } from '@/modules/outbox/infrastructure/schemas/outboxEvent.schema';
-
-export interface IBillOutboxEvent extends IInsertOutboxEvent<IBill | ISelectBill> {}
--- src/modules/receiver/domain/interfaces/receiverOutboxEvent.interface.ts
+++
@@ -1,4 +0,0 @@
-import type { IInsertOutboxEvent } from '@/modules/outbox/infrastructure/schemas/outboxEvent.schema';
-import type { ISelectReceiver } from '@/modules/receiver/infrastructure/schemas/receiver.schema';
-
-export interface IReceiverOutboxEvent extends IInsertOutboxEvent<ISelectReceiver> {}
