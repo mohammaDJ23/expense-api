a2d058081e5f7b6bdb7bbcadda79e1c054c2f150
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-07-16 15:08:52 +0330

    chore: 638860 remove the unused outbox events

--- src/modules/consumer/domain/interfaces/consumerOutboxEvent.interface.ts
+++
@@ -1,4 +0,0 @@
-import type { ISelectConsumer } from '@/modules/consumer/infrastructure/schemas/consumer.schema';
-import type { IInsertOutboxEvent } from '@/modules/outbox/infrastructure/schemas/outboxEvent.schema';
-
-export interface IConsumerOutboxEvent extends IInsertOutboxEvent<ISelectConsumer> {}
--- src/modules/location/domain/interfaces/locationOutboxEvent.interface.ts
+++
@@ -1,4 +0,0 @@
-import type { ISelectLocation } from '@/modules/location/infrastructure/schemas/location.schema';
-import type { IInsertOutboxEvent } from '@/modules/outbox/infrastructure/schemas/outboxEvent.schema';
-
-export interface ILocationOutboxEvent extends IInsertOutboxEvent<ISelectLocation> {}
