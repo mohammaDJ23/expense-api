567dea3828091b3738feffc02b4360b2e884744f
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-07-16 10:14:58 +0330

    chore: 638860 update the types of payload

--- src/modules/outbox/applications/commands/createOutboxEvent/createOutboxEvent.command.ts
+++ src/modules/outbox/applications/commands/createOutboxEvent/createOutboxEvent.command.ts
@@ -1,14 +1,15 @@
 import { createOutboxRoute } from '@/modules/outbox/domain/utils/createOutboxRoute.util';
 
+import type { TOutboxEventPayload } from '@/modules/outbox/domain/interfaces/outboxEventPayload.interface';
 import type { IInsertOutboxEvent } from '@/modules/outbox/infrastructure/schemas/outboxEvent.schema';
 
-export class CreateOutboxEventCommand<T extends IInsertOutboxEvent = IInsertOutboxEvent> {
-    public readonly props: T;
+export class CreateOutboxEventCommand<T = TOutboxEventPayload> {
+    public readonly props: IInsertOutboxEvent<T>;
 
-    constructor(props: Omit<T, 'id' | 'route'>) {
+    constructor(props: Omit<IInsertOutboxEvent<T>, 'id' | 'route'>) {
         this.props = {
             ...props,
             route: createOutboxRoute(props.aggregateType, props.eventType),
-        } as T;
+        };
     }
 }
