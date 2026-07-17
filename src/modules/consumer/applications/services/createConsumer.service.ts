ff9e384ded8488d68cae7135a89768dadbb51a65
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-07-17 00:23:18 -0700

    refactor: 638860 use Iservice

--- src/modules/consumer/applications/services/createConsumer.service.ts
+++ src/modules/consumer/applications/services/createConsumer.service.ts
@@ -8,11 +8,16 @@ import { CreateConsumerCommand } from '@/modules/consumer/applications/commands/
 import { FindConsumerByUserIdAndNameOrNullQuery } from '@/modules/consumer/applications/queries/findConsumerByUserIdAndNameOrNull/findConsumerByUserIdAndNameOrNull.query';
 import { OutboxEventPublisherService } from '@/modules/outbox/applications/services/outboxEventPublisher.service';
 
-import type { IServiceHandler } from '@/core/interfaces/service.interface';
+import type { IService } from '@/core/interfaces/service.interface';
 import type { ISelectConsumer } from '@/modules/consumer/infrastructure/schemas/consumer.schema';
 
+interface IInput {
+    userId: string;
+    name: string;
+}
+
 @Injectable()
-export class CreateConsumerService implements IServiceHandler {
+export class CreateConsumerService implements IService<IInput, IdEntity> {
     constructor(
         private readonly queryBus: QueryBus,
         private readonly commandBus: CommandBus,
@@ -20,11 +25,16 @@ export class CreateConsumerService implements IServiceHandler {
     ) {}
 
     @Transactional()
-    async execute(userId: string, name: string): Promise<IdEntity> {
+    async execute(input: IInput): Promise<IdEntity> {
         const consumer = await this.queryBus.execute<
             FindConsumerByUserIdAndNameOrNullQuery,
             ISelectConsumer | null
-        >(new FindConsumerByUserIdAndNameOrNullQuery({ userId, name }));
+        >(
+            new FindConsumerByUserIdAndNameOrNullQuery({
+                userId: input.userId,
+                name: input.name,
+            }),
+        );
 
         if (!consumer) {
             const createdConsumer = await this.commandBus.execute<
@@ -32,8 +42,8 @@ export class CreateConsumerService implements IServiceHandler {
                 ISelectConsumer
             >(
                 new CreateConsumerCommand({
-                    name,
-                    userId,
+                    name: input.name,
+                    userId: input.userId,
                     createdAt: getCurrentUTCTimestamp(),
                     updatedAt: getCurrentUTCTimestamp(),
                 }),
