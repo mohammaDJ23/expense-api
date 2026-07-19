ea6504786283c9577ef68494248515a7a994a6f6
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-07-18 20:30:02 -0700

    refactor: 638860 change the message text

--- src/modules/authentication/interface/controllers/controllers.constants.ts
+++ src/modules/authentication/interface/controllers/controllers.constants.ts
@@ -1,10 +1,11 @@
 export const SUCCESS_SIGNUP_MESSAGE = 'Your account has been registered and check your email';
 export const SUCCESS_LOGIN_MESSAGE = 'Welcome back! You have successfully logged in';
-export const SUCCESS_SEND_VERIFICATION_MESSAGE = 'The verification link was sent to your email';
+export const SUCCESS_SEND_VERIFICATION_MESSAGE =
+    'The verification link has been sent to your email';
 export const SUCCESS_VERIFY_VERIFICATION_MESSAGE = 'Your account has been verified';
 export const SUCCESS_FORGOT_PASSWORD_MESSAGE =
     // eslint-disable-next-line sonarjs/no-hardcoded-passwords
     'Check your email for the further reset password steps';
 export const SUCCESS_RESET_PASSWORD_MESSAGE =
     // eslint-disable-next-line sonarjs/no-hardcoded-passwords
-    'Your password was changed';
+    'Your password has been changed';
