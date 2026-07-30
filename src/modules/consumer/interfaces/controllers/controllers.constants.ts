c4aaf800668e3b216a36a933908eb7e4248776eb
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-07-30 09:58:43 +0330

    chore: 440883 add the find total of locations api

--- src/modules/consumer/interfaces/controllers/controllers.constants.ts
+++ src/modules/consumer/interfaces/controllers/controllers.constants.ts
@@ -3,4 +3,4 @@ export const SUCCESS_FIND_CONSUMERS_MESSAGE = 'You have received the consumers s
 export const SUCCESS_CREATE_CONSUMER_MESSAGE = 'You have created a new consumer successfully';
 export const SUCCESS_UPDATE_CONSUMER_MESSAGE = 'You have updated the consumer successfully';
 export const SUCCESS_DELETE_CONSUMER_MESSAGE = 'You have deleted the consumer successfully';
-export const SUCCESS_TOTAL_CONSUMER_MESSAGE = 'You have got the total consumers';
+export const SUCCESS_TOTAL_CONSUMERS_MESSAGE = 'You have got the total consumers';
