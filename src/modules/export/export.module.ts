49e13f2fcb2a93271baefc2bf8a645d586b9b2fc
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-04 10:03:29 +0330

    chore: 553939 add the data loader service

--- src/modules/export/export.module.ts
+++ src/modules/export/export.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { BillModule } from '@/modules/bill/bill.module';
+import { BillsExportDataLoaderService } from '@/modules/export/applications/services/billsExportDataLoader.service';
 
 @Module({
     imports: [BillModule],
+    providers: [BillsExportDataLoaderService],
 })
 export class ExportModule {}
