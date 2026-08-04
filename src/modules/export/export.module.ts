f342bffe48b77d85a193cf9c67b9a3fcb337fda2
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-04 15:19:52 +0330

    chore: 553939 add the bills export orchestrator service

--- src/modules/export/export.module.ts
+++ src/modules/export/export.module.ts
@@ -3,9 +3,14 @@ import { Module } from '@nestjs/common';
 import { BillModule } from '@/modules/bill/bill.module';
 import { BillsExportDataLoaderService } from '@/modules/export/applications/services/billsExportDataLoader.service';
 import { BillsExportGeneratorService } from '@/modules/export/applications/services/billsExportGenerator.service';
+import { BillsExportOrchestratorService } from '@/modules/export/applications/services/billsExportOrchestrator.service';
 
 @Module({
     imports: [BillModule],
-    providers: [BillsExportDataLoaderService, BillsExportGeneratorService],
+    providers: [
+        BillsExportDataLoaderService,
+        BillsExportGeneratorService,
+        BillsExportOrchestratorService,
+    ],
 })
 export class ExportModule {}
