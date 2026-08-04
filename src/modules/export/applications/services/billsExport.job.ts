de44e45a83cf461b2e1678a9f0c57bd4c5545aaf
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-04 17:07:58 +0330

    chore: 553939 get the users and generate excel for each of them the make a report as an email

--- src/modules/export/applications/services/billsExport.job.ts
+++ src/modules/export/applications/services/billsExport.job.ts
@@ -1,18 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { QueryBus } from '@nestjs/cqrs';
 import { Cron, CronExpression } from '@nestjs/schedule';
 
+import { FindManyUsersQuery } from '@/modules/user/applications/queries/findManyUsers/findManyUsers.query';
+
 import { BillsExportService } from './billsExport.service';
+import { BillsExportMailerService } from './billsExportMailer.service';
 
 import type { IJob } from '@/core/interfaces/job.interface';
+import type { ISelectUser } from '@/modules/user/infrastructure/schemas/user.schema';
 
 @Injectable()
 export class BillsExportJob implements IJob {
-    constructor(private readonly billsExportService: BillsExportService) {}
+    constructor(
+        private readonly queryBus: QueryBus,
+        private readonly billsExportService: BillsExportService,
+        private readonly billsExportMailerService: BillsExportMailerService,
+    ) {}
 
-    @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
+    @Cron(CronExpression.EVERY_MINUTE)
     async run(): Promise<void> {
-        await this.billsExportService.execute({
-            userId: '',
-        });
+        const users = await this.queryBus.execute<FindManyUsersQuery, ISelectUser[]>(
+            new FindManyUsersQuery(),
+        );
+
+        await Promise.allSettled(
+            users.map(async (user) => {
+                const buffer = await this.billsExportService.execute({
+                    userId: user.id,
+                });
+
+                await this.billsExportMailerService.execute({
+                    email: user.email,
+                    buffer,
+                });
+            }),
+        );
     }
 }
