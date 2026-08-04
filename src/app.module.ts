c6bdc60ea0f3a0e6fa3d7f600da7b8f3b4c231c6
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-04 09:56:23 +0330

    chore: 553939 export the related service of bill and create the module of export

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { InfrastructureModule } from '@/infrastructure/infrastructure.module';
 import { AuthenticationModule } from '@/modules/authentication/authentication.module';
 import { BillModule } from '@/modules/bill/bill.module';
 import { ConsumerModule } from '@/modules/consumer/consumer.module';
+import { ExportModule } from '@/modules/export/export.module';
 import { HealthModule } from '@/modules/health/health.module';
 import { LocationModule } from '@/modules/location/location.module';
 import { OutboxModule } from '@/modules/outbox/outbox.module';
@@ -25,6 +26,7 @@ import { UserModule } from '@/modules/user/user.module';
         ReceiverModule,
         OutboxModule,
         SearchModule,
+        ExportModule,
     ],
 })
 export class AppModule {}
