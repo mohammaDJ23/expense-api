dc314abe0cd36a88390689815843ffa932b874a6
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-04 15:55:40 +0330

    chore: 553939 rename billsExport service

--- src/modules/export/export.module.ts
+++ src/modules/export/export.module.ts
@@ -1,16 +1,12 @@
 import { Module } from '@nestjs/common';
 
 import { BillModule } from '@/modules/bill/bill.module';
+import { BillsExportService } from '@/modules/export/applications/services/billsExport.service';
 import { BillsExportDataLoaderService } from '@/modules/export/applications/services/billsExportDataLoader.service';
 import { BillsExportGeneratorService } from '@/modules/export/applications/services/billsExportGenerator.service';
-import { BillsExportOrchestratorService } from '@/modules/export/applications/services/billsExportOrchestrator.service';
 
 @Module({
     imports: [BillModule],
-    providers: [
-        BillsExportDataLoaderService,
-        BillsExportGeneratorService,
-        BillsExportOrchestratorService,
-    ],
+    providers: [BillsExportDataLoaderService, BillsExportGeneratorService, BillsExportService],
 })
 export class ExportModule {}
