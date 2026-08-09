9d985300b0b282ebf101291689cf291d6275743d
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-10 00:59:36 +0330

    chore: 816381 convert the default limit and offset list logic to cursor

--- src/modules/location/applications/services/findLocationListByUserId.service.ts
+++ src/modules/location/applications/services/findLocationListByUserId.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { QueryBus } from '@nestjs/cqrs';
 
+import { ProcessFailedInternalServerErrorException } from '@/core/exceptions/processFailedInternalServerError.exception';
+import { createCursorPagination } from '@/core/utils/cursor/createCursorPagination.util';
+import { parseCursor } from '@/core/utils/cursor/parseCursor.util';
 import { FindLocationListByUserIdQuery } from '@/modules/location/applications/queries/findLocationListByUserId/findLocationListByUserId.query';
 import { FindTotalLocationsByUserIdQuery } from '@/modules/location/applications/queries/findTotalLocationsByUserId/findTotalLocationsByUserId.query';
 
 import type { IService } from '@/core/interfaces/service.interface';
 import type { IListResult } from '@/core/types/listResult.type';
+import type { ICursor } from '@/core/utils/cursor/cursor.type';
 import type { ISelectLocation } from '@/modules/location/infrastructure/schemas/location.schema';
 import type { FindLocationListRequestDto } from '@/modules/location/interfaces/dtos/findLocationList.request.dto';
 
@@ -26,7 +30,7 @@ export class FindLocationListByUserIdService implements IService<
             this.queryBus.execute<FindLocationListByUserIdQuery, ISelectLocation[]>(
                 new FindLocationListByUserIdQuery({
                     userId: input.userId,
-                    offset: input.query.offset,
+                    cursor: this.parseCursor(input.query.cursor),
                     limit: input.query.limit,
                 }),
             ),
@@ -36,9 +40,20 @@ export class FindLocationListByUserIdService implements IService<
                 }),
             ),
         ]);
+
+        const cursorPagination = createCursorPagination(locations, input.query.limit);
+
         return {
-            items: locations,
+            ...cursorPagination,
             total,
         };
+    }
+
+    parseCursor(cursor: string | null): ICursor | null {
+        try {
+            return parseCursor(cursor);
+        } catch {
+            throw new ProcessFailedInternalServerErrorException();
+        }
     }
 }
