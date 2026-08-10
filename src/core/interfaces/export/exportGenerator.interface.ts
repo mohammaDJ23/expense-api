e7c71df3b9b5df44b0ddf4cdcc360c917e61279b
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-10 11:26:36 +0330

    refactor: 816381 update the methods of generator of export

--- src/core/interfaces/export/exportGenerator.interface.ts
+++ src/core/interfaces/export/exportGenerator.interface.ts
@@ -1,3 +1,7 @@
-export interface IExportGenerator<TRow, TOutput> {
-    generate(rows: TRow): Promise<TOutput>;
+import type { IExportContext } from '@/core/types/exportContext.type';
+
+export interface IExportGenerator<TRow> {
+    initialize(): IExportContext;
+    addRows(context: IExportContext, rows: TRow[]): void;
+    generate(context: IExportContext): Promise<void>;
 }
