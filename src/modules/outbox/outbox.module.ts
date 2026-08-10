63950fce1e3a561b79301c535fffdbd7d4284e2d
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-10 18:00:44 +0330

    refactor: 816381 move the jobs into related folder

--- src/modules/outbox/outbox.module.ts
+++ src/modules/outbox/outbox.module.ts
@@ -3,14 +3,14 @@ import { Module } from '@nestjs/common';
 import { CqrsModule } from '@/infrastructure/cqrs/cqrs.module';
 import { CreateOutboxEventHandler } from '@/modules/outbox/applications/commands/createOutboxEvent/createOutboxEvent.handler';
 import { DeleteManyOutboxEventsByDateHandler } from '@/modules/outbox/applications/commands/deleteManyOutboxEventsByDate/deleteManyOutboxEventsByDate.handler';
-import { DeleteManyOutboxEventsService } from '@/modules/outbox/applications/services/deleteManyOutboxEvents.service';
+import { DeleteManyOutboxEventsJob } from '@/modules/outbox/applications/services/jobs/deleteManyOutboxEvents.job';
 import { OutboxEventPublisherService } from '@/modules/outbox/applications/services/outboxEventPublisher.service';
 import { OutboxEventRepository } from '@/modules/outbox/infrastructure/repositories/outboxEvent.repository';
 
 @Module({
     imports: [CqrsModule],
     providers: [
-        DeleteManyOutboxEventsService,
+        DeleteManyOutboxEventsJob,
         CreateOutboxEventHandler,
         DeleteManyOutboxEventsByDateHandler,
         OutboxEventRepository,
