7966c0e61eb71e700812c75e72d734d892b269a5
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-12 10:21:13 +0330

    chore: 865711 add the search sync for location

---
+++ src/modules/location/applications/services/search/locationSearchSync.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+
+import { MAX_LIST_LIMIT } from '@/core/core.constants';
+import { cursorIterator } from '@/core/utils/pagination/cursorIterator.util';
+import { ElasticSearchService } from '@/infrastructure/elasticsearch/elasticsearch.service';
+import { FindLocationListByUserIdService } from '@/modules/location/applications/services//findLocationListByUserId.service';
+
+import type { IElasticsearchSync } from '@/infrastructure/elasticsearch/elasticsearchSync.interface';
+import type { ISelectLocation } from '@/modules/location/infrastructure/schemas/location.schema';
+import type { TOutboxEventAggregateType } from '@/modules/outbox/domain/types/outboxEventAggregateType.type';
+import type { estypes } from '@elastic/elasticsearch';
+
+@Injectable()
+export class LocationSearchSyncService implements IElasticsearchSync {
+    constructor(
+        private readonly findLocationListByUserIdService: FindLocationListByUserIdService,
+        private readonly elasticsearchService: ElasticSearchService,
+    ) {}
+
+    async sync(userId: string): Promise<void> {
+        const index: TOutboxEventAggregateType = 'locations';
+
+        for await (const locations of cursorIterator((cursor) =>
+            this.findLocationListByUserIdService.execute({
+                userId,
+                query: {
+                    limit: MAX_LIST_LIMIT,
+                    cursor,
+                },
+            }),
+        )) {
+            const operations = locations.flatMap<estypes.BulkOperationContainer | ISelectLocation>(
+                (location) => [
+                    {
+                        index: {
+                            _index: index,
+                            _id: location.id,
+                        },
+                    },
+                    location,
+                ],
+            );
+            await this.elasticsearchService.bulk({
+                operations,
+                refresh: false,
+            });
+        }
+
+        await this.elasticsearchService.refresh(index);
+    }
+}
