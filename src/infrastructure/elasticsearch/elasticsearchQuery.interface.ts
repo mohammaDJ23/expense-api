18b31152f397884aee5210b4a4a99d5edda35091
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-12 14:53:39 +0330

    chore: 865711 update the elastic search query interface

--- src/infrastructure/elasticsearch/elasticsearchQuery.interface.ts
+++ src/infrastructure/elasticsearch/elasticsearchQuery.interface.ts
@@ -1,7 +1,7 @@
 import type { TOutboxEventAggregateType } from '@/modules/outbox/domain/types/outboxEventAggregateType.type';
 import type { estypes } from '@elastic/elasticsearch';
 
-export interface IElasticsearchQuery {
+export interface IElasticsearchQuery<TInput> {
     index: TOutboxEventAggregateType;
-    buildQuery(userId: string, query: string, size: number): estypes.SearchRequest;
+    buildQuery(input: TInput): estypes.SearchRequest;
 }
