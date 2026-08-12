de235082b68c5fc2ae7977b7ed03631aad8db1a0
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-12 15:35:49 +0330

    chore: 865711 add the delete ES docs for receivers

--- src/modules/receiver/applications/services/search/receiverSearchSync.service.ts
+++ src/modules/receiver/applications/services/search/receiverSearchSync.service.ts
@@ -4,6 +4,7 @@ import { MAX_LIST_LIMIT } from '@/core/core.constants';
 import { cursorIterator } from '@/core/utils/pagination/cursorIterator.util';
 import { ElasticSearchService } from '@/infrastructure/elasticsearch/elasticsearch.service';
 import { FindReceiverListByUserIdService } from '@/modules/receiver/applications/services/findReceiverListByUserId.service';
+import { ReceiverElasticsearchDeleteQuery } from '@/modules/receiver/infrastructure/elasticsearch/receiverElasticsearchDelete.query';
 
 import type { IElasticsearchSync } from '@/infrastructure/elasticsearch/elasticsearchSync.interface';
 import type { TOutboxEventAggregateType } from '@/modules/outbox/domain/types/outboxEventAggregateType.type';
@@ -15,10 +16,17 @@ export class ReceiverSearchSyncService implements IElasticsearchSync {
     constructor(
         private readonly findReceiverListByUserIdService: FindReceiverListByUserIdService,
         private readonly elasticsearchService: ElasticSearchService,
+        private readonly receiverElasticsearchDeleteQuery: ReceiverElasticsearchDeleteQuery,
     ) {}
 
     async sync(userId: string): Promise<void> {
         const index: TOutboxEventAggregateType = 'receivers';
+
+        await this.elasticsearchService.deleteByQuery(
+            this.receiverElasticsearchDeleteQuery.buildQuery({
+                userId,
+            }),
+        );
 
         for await (const receivers of cursorIterator((cursor) =>
             this.findReceiverListByUserIdService.execute({
