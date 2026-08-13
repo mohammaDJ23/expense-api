9bdeaa9206e1dbc994acdb07ec54ede0217f2e52
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-13 09:57:26 +0330

    chore: 244939 add the outbox publisher to the delete user service

--- src/modules/user/applications/services/deleteUser.service.ts
+++ src/modules/user/applications/services/deleteUser.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
+import { Transactional } from '@nestjs-cls/transactional';
 
+import { getCurrentUTCTimestamp } from '@/core/utils/getCurrentUTCTimestamp.util';
+import { OutboxEventPublisherService } from '@/modules/outbox/applications/services/outboxEventPublisher.service';
 import { DeleteUserCommand } from '@/modules/user/applications/commands/deleteUser/deleteUser.command';
 import { UserExistenceValidatorService } from '@/modules/user/applications/services/validators/userExistenceValidator.service';
 
@@ -17,8 +20,10 @@ export class DeleteUserService implements IService<IInput, IId> {
     constructor(
         private readonly commandBus: CommandBus,
         private readonly userExistenceValidatorService: UserExistenceValidatorService,
+        private readonly outboxEventPublisherService: OutboxEventPublisherService,
     ) {}
 
+    @Transactional()
     async execute(input: IInput): Promise<IId> {
         await this.userExistenceValidatorService.validate({ userId: input.userId });
 
@@ -27,6 +32,14 @@ export class DeleteUserService implements IService<IInput, IId> {
                 userId: input.userId,
             }),
         );
+
+        await this.outboxEventPublisherService.publish({
+            aggregateId: deletedUser.id,
+            aggregateType: 'users',
+            eventType: 'deleted',
+            payload: deletedUser,
+            createdAt: getCurrentUTCTimestamp(),
+        });
 
         return {
             id: deletedUser.id,
