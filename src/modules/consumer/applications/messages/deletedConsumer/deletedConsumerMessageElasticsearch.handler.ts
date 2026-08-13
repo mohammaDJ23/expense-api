33c079964b081ade05ae46a0b50625ea95c621d7
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-14 00:38:59 +0330

    refactor: 244939 update the message handlers of consumer module

--- src/modules/consumer/applications/messages/deletedConsumer/deletedConsumerMessageElasticsearch.handler.ts
+++ src/modules/consumer/applications/messages/deletedConsumer/deletedConsumerMessageElasticsearch.handler.ts
@@ -1,17 +1,19 @@
-import { Injectable } from '@nestjs/common';
-
+import { MessageHandler } from '@/core/features/message/messageHandler.decorator';
 import { ElasticSearchService } from '@/infrastructure/elasticsearch/elasticsearch.service';
 
 import type { IMessageBatch } from '@/core/features/message/messageBatch.type';
-import type { IMessageProcessor } from '@/core/features/message/messageProcessor.interface';
+import type { IMessageHandler } from '@/core/features/message/messageHandler.interface';
 import type { ISelectConsumer } from '@/modules/consumer/infrastructure/schemas/consumer.schema';
+import type { TOutboxEventRoute } from '@/modules/outbox/domain/types/outboxEventRoute.type';
 import type { estypes } from '@elastic/elasticsearch';
 
-@Injectable()
-export class DeleteConsumerMessageElasticsearchProcessor implements IMessageProcessor<ISelectConsumer> {
+@MessageHandler()
+export class DeletedConsumerMessageElasticsearchHandler implements IMessageHandler<ISelectConsumer> {
+    route: TOutboxEventRoute = 'consumers.deleted';
+
     constructor(private readonly elasticsearchService: ElasticSearchService) {}
 
-    async process(batch: IMessageBatch<ISelectConsumer>[]): Promise<void> {
+    async execute(batch: IMessageBatch<ISelectConsumer>[]): Promise<void> {
         const operations = batch.map<estypes.BulkOperationContainer>((item) => ({
             delete: {
                 _index: item.aggregateType,
