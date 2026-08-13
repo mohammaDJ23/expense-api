157d88051d961b60e6b1120dba2bfe756dd37f7f
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-14 00:18:41 +0330

    refactor: 244939 update the message handlers of bill module

--- src/modules/bill/applications/messages/deletedBill/deletedBillMessageElasticsearch.handler.ts
+++ src/modules/bill/applications/messages/deletedBill/deletedBillMessageElasticsearch.handler.ts
@@ -1,17 +1,19 @@
-import { Injectable } from '@nestjs/common';
-
+import { MessageHandler } from '@/core/features/message/messageHandler.decorator';
 import { ElasticSearchService } from '@/infrastructure/elasticsearch/elasticsearch.service';
 
 import type { IMessageBatch } from '@/core/features/message/messageBatch.type';
-import type { IMessageProcessor } from '@/core/features/message/messageProcessor.interface';
+import type { IMessageHandler } from '@/core/features/message/messageHandler.interface';
 import type { ISelectBill } from '@/modules/bill/infrastructure/schemas/bill.schema';
+import type { TOutboxEventRoute } from '@/modules/outbox/domain/types/outboxEventRoute.type';
 import type { estypes } from '@elastic/elasticsearch';
 
-@Injectable()
-export class DeleteBillMessageElasticsearchProcessor implements IMessageProcessor<ISelectBill> {
+@MessageHandler()
+export class DeletedBillMessageElasticsearchHandler implements IMessageHandler<ISelectBill> {
+    route: TOutboxEventRoute = 'bills.deleted';
+
     constructor(private readonly elasticsearchService: ElasticSearchService) {}
 
-    async process(batch: IMessageBatch<ISelectBill>[]): Promise<void> {
+    async execute(batch: IMessageBatch<ISelectBill>[]): Promise<void> {
         const operations = batch.map<estypes.BulkOperationContainer>((item) => ({
             delete: {
                 _index: item.aggregateType,
