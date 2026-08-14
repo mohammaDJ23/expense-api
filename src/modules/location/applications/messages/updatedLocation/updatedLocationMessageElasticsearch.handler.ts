02216086d10932f71440d0faa2008063f6e4145d
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-14 07:44:30 +0330

    refactor: 244939 import ES as a service

--- src/modules/location/applications/messages/updatedLocation/updatedLocationMessageElasticsearch.handler.ts
+++ src/modules/location/applications/messages/updatedLocation/updatedLocationMessageElasticsearch.handler.ts
@@ -1,8 +1,8 @@
 import { MessageHandler } from '@/core/features/message/messageHandler.decorator';
+import { ElasticSearchService } from '@/infrastructure/elasticsearch/elasticsearch.service';
 
 import type { IMessageBatch } from '@/core/features/message/messageBatch.type';
 import type { IMessageHandler } from '@/core/features/message/messageHandler.interface';
-import type { ElasticSearchService } from '@/infrastructure/elasticsearch/elasticsearch.service';
 import type { ISelectLocation } from '@/modules/location/infrastructure/schemas/location.schema';
 import type { TOutboxEventRoute } from '@/modules/outbox/domain/types/outboxEventRoute.type';
 import type { estypes } from '@elastic/elasticsearch';
