f2db783445a407faf86314464e140358b9f25bad
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-14 09:10:17 +0330

    chore: 173201 add FindBillListAndTotalByUserIdService

---
+++ src/modules/bill/applications/services/findBillListAndTotalByUserId.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { QueryBus } from '@nestjs/cqrs';
+
+import { FindTotalBillsByUserIdQuery } from '@/modules/bill/applications/queries/findTotalBillsByUserId/findTotalBillsByUserId.query';
+
+import { FindBillListByUserIdService } from './findBillListByUserId.service';
+
+import type { IService } from '@/core/interfaces/service.interface';
+import type { IListResultWithTotal } from '@/core/types/list/listResultWithTotal.type';
+import type { IBill } from '@/modules/bill/domain/types/bill.type';
+import type { FindBillListRequestDto } from '@/modules/bill/interface/dtos/findBillList.request.dto';
+
+interface IInput {
+    userId: string;
+    query: FindBillListRequestDto;
+}
+
+@Injectable()
+export class FindBillListAndTotalByUserIdService implements IService<
+    IInput,
+    IListResultWithTotal<IBill>
+> {
+    constructor(
+        private readonly queryBus: QueryBus,
+        private readonly findBillListByUserIdService: FindBillListByUserIdService,
+    ) {}
+
+    async execute(input: IInput): Promise<IListResultWithTotal<IBill>> {
+        const [billList, total] = await Promise.all([
+            this.findBillListByUserIdService.execute(input),
+            this.queryBus.execute<FindTotalBillsByUserIdQuery, number>(
+                new FindTotalBillsByUserIdQuery({
+                    userId: input.userId,
+                }),
+            ),
+        ]);
+
+        return {
+            ...billList,
+            total,
+        };
+    }
+}
