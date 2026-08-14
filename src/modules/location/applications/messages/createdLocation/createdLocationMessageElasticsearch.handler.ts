51aa85a20d6f566f44b66ed82f759ca56b9c9e1f
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-14 07:42:55 +0330

    refactor: 244939 update the message handlers of location module

--- src/modules/location/applications/messages/createdLocation/createdLocationMessageElasticsearch.handler.ts
+++ src/modules/location/applications/messages/createdLocation/createdLocationMessageElasticsearch.handler.ts
@@ -1,17 +1,19 @@
-import { Injectable } from '@nestjs/common';
-
+import { MessageHandler } from '@/core/features/message/messageHandler.decorator';
 import { ElasticSearchService } from '@/infrastructure/elasticsearch/elasticsearch.service';
 
 import type { IMessageBatch } from '@/core/features/message/messageBatch.type';
-import type { IMessageProcessor } from '@/core/features/message/messageProcessor.interface';
+import type { IMessageHandler } from '@/core/features/message/messageHandler.interface';
 import type { ISelectLocation } from '@/modules/location/infrastructure/schemas/location.schema';
+import type { TOutboxEventRoute } from '@/modules/outbox/domain/types/outboxEventRoute.type';
 import type { estypes } from '@elastic/elasticsearch';
 
-@Injectable()
-export class UpdateLocationMessageElasticsearchProcessor implements IMessageProcessor<ISelectLocation> {
+@MessageHandler()
+export class CreatedLocationMessageElasticsearchHandler implements IMessageHandler<ISelectLocation> {
+    route: TOutboxEventRoute = 'locations.created';
+
     constructor(private readonly elasticsearchService: ElasticSearchService) {}
 
-    async process(batch: IMessageBatch<ISelectLocation>[]): Promise<void> {
+    async execute(batch: IMessageBatch<ISelectLocation>[]): Promise<void> {
         const operations = batch.flatMap<estypes.BulkOperationContainer | ISelectLocation>(
             (item) => [
                 {
--- src/modules/location/applications/messages/updatedLocation/updatedLocationMessageElasticsearch.handler.ts
+++ src/modules/location/applications/messages/updatedLocation/updatedLocationMessageElasticsearch.handler.ts
@@ -1,17 +1,19 @@
-import { Injectable } from '@nestjs/common';
-
-import { ElasticSearchService } from '@/infrastructure/elasticsearch/elasticsearch.service';
+import { MessageHandler } from '@/core/features/message/messageHandler.decorator';
 
 import type { IMessageBatch } from '@/core/features/message/messageBatch.type';
-import type { IMessageProcessor } from '@/core/features/message/messageProcessor.interface';
+import type { IMessageHandler } from '@/core/features/message/messageHandler.interface';
+import type { ElasticSearchService } from '@/infrastructure/elasticsearch/elasticsearch.service';
 import type { ISelectLocation } from '@/modules/location/infrastructure/schemas/location.schema';
+import type { TOutboxEventRoute } from '@/modules/outbox/domain/types/outboxEventRoute.type';
 import type { estypes } from '@elastic/elasticsearch';
 
-@Injectable()
-export class CreateLocationMessageElasticsearchProcessor implements IMessageProcessor<ISelectLocation> {
+@MessageHandler()
+export class UpdatedLocationMessageElasticsearchHandler implements IMessageHandler<ISelectLocation> {
+    route: TOutboxEventRoute = 'locations.updated';
+
     constructor(private readonly elasticsearchService: ElasticSearchService) {}
 
-    async process(batch: IMessageBatch<ISelectLocation>[]): Promise<void> {
+    async execute(batch: IMessageBatch<ISelectLocation>[]): Promise<void> {
         const operations = batch.flatMap<estypes.BulkOperationContainer | ISelectLocation>(
             (item) => [
                 {
