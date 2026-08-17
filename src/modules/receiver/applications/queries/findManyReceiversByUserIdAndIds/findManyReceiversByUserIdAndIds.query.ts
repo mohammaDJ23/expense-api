117b46d63026fae124e98192615e13f66e87969d
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-17 09:15:08 +0330

    chore: 043321 add cache to FindManyReceiversByUserIdAndIdsQuery

--- src/modules/receiver/applications/queries/findManyReceiversByUserIdAndIds/findManyReceiversByUserIdAndIds.query.ts
+++ src/modules/receiver/applications/queries/findManyReceiversByUserIdAndIds/findManyReceiversByUserIdAndIds.query.ts
@@ -1,8 +1,17 @@
+import { Cacheable } from '@/core/features/cache/cacheable.decorator';
+import { CacheNamespace } from '@/core/features/cache/cacheNamespace.enum';
+
 interface IProps {
     userId: string;
     ids: string[];
 }
 
+@Cacheable<FindManyReceiversByUserIdAndIdsQuery>({
+    namespace: CacheNamespace.RECEIVER,
+    scope(query) {
+        return query.props.userId;
+    },
+})
 export class FindManyReceiversByUserIdAndIdsQuery {
     constructor(public readonly props: IProps) {}
 }
