7ac34d6bfdff901611bb378ff91b42abb4e05f3f
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-17 09:01:24 +0330

    chore: 043321 add cache to FindUserListQuery

--- src/modules/user/applications/queries/findUserList/findUserList.query.ts
+++ src/modules/user/applications/queries/findUserList/findUserList.query.ts
@@ -1,3 +1,6 @@
+import { Cacheable } from '@/core/features/cache/cacheable.decorator';
+import { CacheNamespace } from '@/core/features/cache/cacheNamespace.enum';
+
 import type { ICursor } from '@/core/utils/pagination/cursor.type';
 
 interface IProps {
@@ -5,6 +8,9 @@ interface IProps {
     limit: number;
 }
 
+@Cacheable({
+    namespace: CacheNamespace.USER,
+})
 export class FindUserListQuery {
     constructor(public readonly props: IProps) {}
 }
