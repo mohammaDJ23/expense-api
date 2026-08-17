6580f80e531d319b6b0a1121ec4f773aac5402d0
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-18 00:15:54 +0330

    chore: 043321 rename the handlers

---
+++ src/modules/bill/applications/messages/createdBill/createdBillElasticsearchIndexer.handler.ts
@@ -0,0 +1,20 @@
+import { MessageHandler } from '@/core/features/message/messageHandler.decorator';
+import { BillElasticsearchIndexerProcessor } from '@/modules/bill/applications/messages/billElasticsearchIndexer.processor';
+
+import type { IMessageBatch } from '@/core/features/message/messageBatch.type';
+import type { IMessageHandler } from '@/core/features/message/messageHandler.interface';
+import type { ISelectBill } from '@/modules/bill/infrastructure/schemas/bill.schema';
+import type { TOutboxEventRoute } from '@/modules/outbox/domain/types/outboxEventRoute.type';
+
+@MessageHandler()
+export class CreatedBillElasticsearchIndexerHandler implements IMessageHandler<ISelectBill> {
+    route: TOutboxEventRoute = 'bills.created';
+
+    constructor(
+        private readonly billElasticsearchIndexerProcessor: BillElasticsearchIndexerProcessor,
+    ) {}
+
+    async execute(batch: IMessageBatch<ISelectBill>[]): Promise<void> {
+        await this.billElasticsearchIndexerProcessor.process(batch);
+    }
+}
