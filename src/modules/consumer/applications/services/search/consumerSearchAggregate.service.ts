82c181d8a6669db95e51d0b2cc99b3eadafa8019
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-17 17:43:51 +0330

    chore: 043321 add cache to FindManyConsumersByUserIdAndIdsQuery

--- src/modules/consumer/applications/services/search/consumerSearchAggregate.service.ts
+++ src/modules/consumer/applications/services/search/consumerSearchAggregate.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { QueryBus } from '@nestjs/cqrs';
 
+import { QueryDispatcher } from '@/core/features/queryDispatcher/query.dispatcher';
 import { whenNotEmpty } from '@/core/utils/whenNotEmpty.util';
 import { FindManyConsumersByUserIdAndIdsQuery } from '@/modules/consumer/applications/queries/findManyConsumersByUserIdAndIds/findManyConsumersByUserIdAndIds.query';
 
@@ -9,11 +9,11 @@ import type { ISelectConsumer } from '@/modules/consumer/infrastructure/schemas/
 
 @Injectable()
 export class ConsumerSearchAggregateService implements IElasticsearchSearchAggregate<ISelectConsumer> {
-    constructor(private readonly queryBus: QueryBus) {}
+    constructor(private readonly queryDispatcher: QueryDispatcher) {}
 
     aggregate(userId: string, ids: string[]): Promise<ISelectConsumer[]> {
         return whenNotEmpty(ids, (ids) =>
-            this.queryBus.execute<FindManyConsumersByUserIdAndIdsQuery, ISelectConsumer[]>(
+            this.queryDispatcher.execute<FindManyConsumersByUserIdAndIdsQuery, ISelectConsumer[]>(
                 new FindManyConsumersByUserIdAndIdsQuery({
                     userId,
                     ids,
