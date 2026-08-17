c7028553f24fb003636eeb9f3a85f9fa78f15ff4
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-17 18:04:49 +0330

    chore: 043321 add cache to FindMostUsedLocationsQuery

--- src/modules/bill/applications/queries/findMostUsedLocations/findMostUsedLocations.query.ts
+++ src/modules/bill/applications/queries/findMostUsedLocations/findMostUsedLocations.query.ts
@@ -1,8 +1,17 @@
+import { Cacheable } from '@/core/features/cache/cacheable.decorator';
+import { CacheNamespace } from '@/core/features/cache/cacheNamespace.enum';
+
 interface IProps {
     userId: string;
     limit: number;
 }
 
+@Cacheable<FindMostUsedLocationsQuery>({
+    namespace: CacheNamespace.BILL,
+    scope(query) {
+        return query.props.userId;
+    },
+})
 export class FindMostUsedLocationsQuery {
     constructor(public readonly props: IProps) {}
 }
