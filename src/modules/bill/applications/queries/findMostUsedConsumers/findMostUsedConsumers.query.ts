d1f5287ae81bf89c48d355667a034291100860c6
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-17 18:01:52 +0330

    chore: 043321 add cache to FindMostUsedConsumersQuery

--- src/modules/bill/applications/queries/findMostUsedConsumers/findMostUsedConsumers.query.ts
+++ src/modules/bill/applications/queries/findMostUsedConsumers/findMostUsedConsumers.query.ts
@@ -1,8 +1,17 @@
+import { Cacheable } from '@/core/features/cache/cacheable.decorator';
+import { CacheNamespace } from '@/core/features/cache/cacheNamespace.enum';
+
 interface IProps {
     userId: string;
     limit: number;
 }
 
+@Cacheable<FindMostUsedConsumersQuery>({
+    namespace: CacheNamespace.BILL,
+    scope(query) {
+        return query.props.userId;
+    },
+})
 export class FindMostUsedConsumersQuery {
     constructor(public readonly props: IProps) {}
 }
