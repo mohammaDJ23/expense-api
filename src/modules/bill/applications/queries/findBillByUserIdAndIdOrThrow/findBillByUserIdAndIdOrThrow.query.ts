c7b54ccacfdc6101f6c197636c537be10869655f
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-17 17:50:01 +0330

    chore: 043321 add cache to FindBillByUserIdAndIdOrThrowQuery

--- src/modules/bill/applications/queries/findBillByUserIdAndIdOrThrow/findBillByUserIdAndIdOrThrow.query.ts
+++ src/modules/bill/applications/queries/findBillByUserIdAndIdOrThrow/findBillByUserIdAndIdOrThrow.query.ts
@@ -1,8 +1,17 @@
+import { Cacheable } from '@/core/features/cache/cacheable.decorator';
+import { CacheNamespace } from '@/core/features/cache/cacheNamespace.enum';
+
 interface IProps {
     userId: string;
     id: string;
 }
 
+@Cacheable<FindBillByUserIdAndIdOrThrowQuery>({
+    namespace: CacheNamespace.BILL,
+    scope(query) {
+        return query.props.userId;
+    },
+})
 export class FindBillByUserIdAndIdOrThrowQuery {
     constructor(public readonly props: IProps) {}
 }
