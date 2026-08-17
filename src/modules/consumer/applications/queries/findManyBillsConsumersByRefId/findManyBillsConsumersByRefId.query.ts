f91039ea0fb74ab839a3de994a3a895b93affb56
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-17 20:26:46 +0330

    chore: 043321 add cache to FindManyBillsConsumersByRefIdQuery

--- src/modules/consumer/applications/queries/findManyBillsConsumersByRefId/findManyBillsConsumersByRefId.query.ts
+++ src/modules/consumer/applications/queries/findManyBillsConsumersByRefId/findManyBillsConsumersByRefId.query.ts
@@ -1,7 +1,17 @@
+import { Cacheable } from '@/core/features/cache/cacheable.decorator';
+import { CacheNamespace } from '@/core/features/cache/cacheNamespace.enum';
+
 interface IProps {
     billId: string;
+    userId: string;
 }
 
+@Cacheable<FindManyBillsConsumersByRefIdQuery>({
+    namespace: CacheNamespace.BILL_CONSUMER,
+    scope(query) {
+        return query.props.userId;
+    },
+})
 export class FindManyBillsConsumersByRefIdQuery {
     constructor(public readonly props: IProps) {}
 }
