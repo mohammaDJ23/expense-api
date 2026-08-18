4f7cc84a2f64e6b2052b3ce6e6bc16eed3fc2d38
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-18 09:30:54 +0330

    chore: 043321 add the cache invalidator message handlers

---
+++ src/modules/receiver/applications/messages/updatedReceiver/updatedReceiverCacheInvalidator.handler.ts
@@ -0,0 +1,22 @@
+import { MessageHandler } from '@/core/features/message/messageHandler.decorator';
+import { ReceiverCacheInvalidatorProcessor } from '@/modules/receiver/applications/messages/receiverCacheInvalidator.processor';
+
+import type { IMessageBatch } from '@/core/features/message/messageBatch.type';
+import type { IMessageHandler } from '@/core/features/message/messageHandler.interface';
+import type { TOutboxEventRoute } from '@/modules/outbox/domain/types/outboxEventRoute.type';
+import type { ISelectReceiver } from '@/modules/receiver/infrastructure/schemas/receiver.schema';
+
+@MessageHandler()
+export class UpdatedReceiverCacheInvalidatorHandler implements IMessageHandler<ISelectReceiver> {
+    route: TOutboxEventRoute = 'receivers.updated';
+
+    constructor(
+        private readonly ReceiverCacheInvalidatorProcessor: ReceiverCacheInvalidatorProcessor,
+    ) {}
+
+    async execute(batch: IMessageBatch<ISelectReceiver>[]): Promise<void> {
+        await this.ReceiverCacheInvalidatorProcessor.process({
+            userIds: batch.map((item) => item.payload.userId),
+        });
+    }
+}
