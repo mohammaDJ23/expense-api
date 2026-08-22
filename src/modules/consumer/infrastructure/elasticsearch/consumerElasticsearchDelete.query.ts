7ede68d26e0fe26ca2924c574434c5606291fb89
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-22 22:33:09 +0330

    chore: 289667 remove index property

--- src/modules/consumer/infrastructure/elasticsearch/consumerElasticsearchDelete.query.ts
+++ src/modules/consumer/infrastructure/elasticsearch/consumerElasticsearchDelete.query.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 import { ConsumerResource } from '@/modules/consumer/consumer.enum';
 
 import type { IElasticsearchQuery } from '@/infrastructure/elasticsearch/elasticsearchQuery.interface';
-import type { TOutboxEventAggregateType } from '@/modules/outbox/domain/types/outboxEventAggregateType.type';
 import type { estypes } from '@elastic/elasticsearch';
 
 interface IInput {
@@ -15,11 +14,9 @@ export class ConsumerElasticsearchDeleteQuery implements IElasticsearchQuery<
     IInput,
     estypes.DeleteByQueryRequest
 > {
-    index: TOutboxEventAggregateType = ConsumerResource.CONSUMER;
-
     buildQuery(input: IInput): estypes.DeleteByQueryRequest {
         return {
-            index: this.index,
+            index: ConsumerResource.CONSUMER,
             query: {
                 term: {
                     userId: input.userId,
