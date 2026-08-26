cf512fb0e11aafe82ae098e43e0fcaf71e0089b3
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-26 07:16:40 +0330

    chore: 142074 add the schema tables of the email identity, local and oauth accounts

---
+++ src/modules/authentication/infrastructure/schemas/localAccount.schema.ts
@@ -0,0 +1,34 @@
+import { relations } from 'drizzle-orm';
+import { uuid, pgTable, timestamp, varchar } from 'drizzle-orm/pg-core';
+
+import { emailIdentities } from './emailIdentity.schema';
+
+export const localAccounts = pgTable('local_accounts', {
+    id: uuid('id').primaryKey().defaultRandom().notNull(),
+    hashedPassword: varchar('hashed_password', { length: 255 }).notNull(),
+    emailId: uuid('email_id')
+        .unique()
+        .notNull()
+        .references(() => emailIdentities.id, { onDelete: 'cascade' }),
+    lastLoginAt: timestamp('last_login_at', { withTimezone: true, mode: 'string' }),
+    verifiedAt: timestamp('verified_at', { withTimezone: true, mode: 'string' }),
+    createdAt: timestamp('created_at', { withTimezone: true, mode: 'string' })
+        .notNull()
+        .defaultNow(),
+    updatedAt: timestamp('updated_at', { withTimezone: true, mode: 'string' })
+        .notNull()
+        .defaultNow(),
+});
+
+export const localAccountsRelations = relations(localAccounts, ({ one }) => ({
+    email: one(emailIdentities, {
+        fields: [localAccounts.emailId],
+        references: [emailIdentities.id],
+    }),
+}));
+
+type TSelectLocalAccount = typeof localAccounts.$inferSelect;
+type TInsertLocalAccount = typeof localAccounts.$inferInsert;
+
+export interface ISelectLocalAccount extends TSelectLocalAccount {}
+export interface IInsertLocalAccount extends TInsertLocalAccount {}
