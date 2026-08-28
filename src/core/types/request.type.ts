eb31e5019635d81c194fe07f6ba6dca1565f02f7
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-28 22:49:48 +0330

    chore: 142074 consider the oauth current user type

--- src/core/types/request.type.ts
+++ src/core/types/request.type.ts
@@ -1,7 +1,8 @@
 import type { ICurrentUser } from '@/core/features/currentUser/currentUser.type';
+import type { IOauthCurrentUser } from '@/core/features/oauthCurrentUser/oauthCurrentUser.type';
 import type { Request } from 'express';
 
 export interface IRequest extends Request {
-    user?: ICurrentUser;
+    user?: ICurrentUser | IOauthCurrentUser;
     clientTimezone?: string;
 }
