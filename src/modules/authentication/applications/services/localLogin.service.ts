85f65830d72dff0ea431f1f6406b84ffe1fc5a8b
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-28 15:11:13 +0330

    feature: 142074 implement the local login process

--- src/modules/authentication/applications/services/localLogin.service.ts
+++ src/modules/authentication/applications/services/localLogin.service.ts
@@ -1,17 +1,26 @@
-import { ForbiddenException, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+    BadRequestException,
+    ForbiddenException,
+    Injectable,
+    NotFoundException,
+} from '@nestjs/common';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { Transactional } from '@nestjs-cls/transactional';
 
-import { LocalAuthProviderForbiddenException } from '@/core/exceptions/localAuthProviderForbidden.exception';
 import { ProcessFailedInternalServerErrorException } from '@/core/exceptions/processFailedInternalServerError.exception';
 import { AccessTokenService } from '@/core/features/accessToken/accessToken.service';
 import { QueryDispatcher } from '@/core/features/queryDispatcher/query.dispatcher';
 import { getCurrentUTCTimestamp } from '@/core/utils/getCurrentUTCTimestamp.util';
-import { FindUserByEmailOrNullQuery } from '@/modules/user/applications/queries/findUserByEmailOrNull/findUserByEmailOrNull.query';
-import { UpdateUserService } from '@/modules/user/applications/services/updateUser.service';
-import { AuthProvider } from '@/modules/user/domain/enums/authProvider.enum';
+import { UpdateLocalAccountCommand } from '@/modules/authentication/applications/commands/updateLocalAccount/updateLocalAccount.command';
+import { FindEmailIdentityByEmailOrNullQuery } from '@/modules/authentication/applications/queries/findEmailIdentityByEmailOrNull/findEmailIdentityByEmailOrNull.query';
+import { FindLocalAccountByEmailIdOrNullQuery } from '@/modules/authentication/applications/queries/findLocalAccountByEmailIdOrNull/findLocalAccountByEmailIdOrNull.query';
+import { FindUserByIdOrThrowQuery } from '@/modules/user/applications/queries/findUserByIdOrThrow/findUserByIdOrThrow.query';
 
 import { PasswordHasherService } from './passwordHasher.service';
 
 import type { IService } from '@/core/interfaces/service.interface';
+import type { ISelectEmailIdentity } from '@/modules/authentication/infrastructure/schemas/emailIdentity.schema';
+import type { ISelectLocalAccount } from '@/modules/authentication/infrastructure/schemas/localAccount.schema';
 import type { LocalLoginRequestDto } from '@/modules/authentication/interface/dtos/localLogin.request.dto';
 import type { ISelectUser } from '@/modules/user/infrastructure/schemas/user.schema';
 import type { Response } from 'express';
@@ -22,33 +31,44 @@ interface IInput {
 }
 
 @Injectable()
-export class LocalLoginService implements IService<IInput, ISelectUser> {
+export class LocalLoginService implements IService<IInput, boolean> {
     constructor(
+        private readonly queryBus: QueryBus,
+        private readonly commandBus: CommandBus,
         private readonly queryDispatcher: QueryDispatcher,
         private readonly passwordHasherService: PasswordHasherService,
         private readonly accessTokenService: AccessTokenService,
-        private readonly updateUserService: UpdateUserService,
     ) {}
 
-    async execute(input: IInput): Promise<ISelectUser> {
-        const user = await this.queryDispatcher.execute<
-            FindUserByEmailOrNullQuery,
-            ISelectUser | null
+    @Transactional()
+    async execute(input: IInput): Promise<boolean> {
+        const emailIdentity = await this.queryBus.execute<
+            FindEmailIdentityByEmailOrNullQuery,
+            ISelectEmailIdentity | null
         >(
-            new FindUserByEmailOrNullQuery({
+            new FindEmailIdentityByEmailOrNullQuery({
                 email: input.body.email,
             }),
         );
 
-        if (!user) {
-            throw new UnauthorizedException();
+        if (!emailIdentity) {
+            throw new BadRequestException();
         }
 
-        if (user.authProvider !== AuthProvider.LOCAL || !user.hashedPassword) {
-            throw new LocalAuthProviderForbiddenException();
+        const localAccount = await this.queryBus.execute<
+            FindLocalAccountByEmailIdOrNullQuery,
+            ISelectLocalAccount | null
+        >(
+            new FindLocalAccountByEmailIdOrNullQuery({
+                emailId: emailIdentity.id,
+            }),
+        );
+
+        if (!localAccount) {
+            throw new NotFoundException('You have not registered a local account before');
         }
 
-        if (!user.verifiedAt) {
+        if (!localAccount.verifiedAt) {
             throw new ForbiddenException();
         }
 
@@ -56,26 +76,42 @@ export class LocalLoginService implements IService<IInput, ISelectUser> {
             let isPasswordValid = false;
             try {
                 isPasswordValid = await this.passwordHasherService.verify(
-                    user.hashedPassword,
+                    localAccount.hashedPassword,
                     input.body.password,
                 );
             } catch {
                 throw new ProcessFailedInternalServerErrorException();
             }
+
             if (!isPasswordValid) {
                 throw new ForbiddenException();
             }
         }
 
         {
-            const token = this.accessTokenService.sign(user);
+            const user = await this.queryDispatcher.execute<FindUserByIdOrThrowQuery, ISelectUser>(
+                new FindUserByIdOrThrowQuery({
+                    id: emailIdentity.userId,
+                }),
+            );
+            const token = this.accessTokenService.sign({
+                id: user.id,
+                role: user.role,
+            });
             this.accessTokenService.setCookie(input.response, token);
         }
 
-        return this.updateUserService.execute({
-            id: user.id,
-            updatedAt: getCurrentUTCTimestamp(),
-            lastLoginAt: getCurrentUTCTimestamp(),
-        });
+        {
+            const creationTime = getCurrentUTCTimestamp();
+            await this.commandBus.execute<UpdateLocalAccountCommand, ISelectLocalAccount>(
+                new UpdateLocalAccountCommand({
+                    id: localAccount.id,
+                    updatedAt: creationTime,
+                    lastLoginAt: creationTime,
+                }),
+            );
+        }
+
+        return true;
     }
 }
