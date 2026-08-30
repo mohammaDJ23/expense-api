018ed34bf081080937d77462ef7037f5a101d289
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-08-30 09:33:36 +0330

    chore: 142074 update the logic of VerifiedVerificationMailerService

--- src/modules/authentication/applications/services/verifiedVerificationMailer.service.ts
+++ src/modules/authentication/applications/services/verifiedVerificationMailer.service.ts
@@ -2,36 +2,32 @@ import { Injectable } from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
 
 import type { IService } from '@/core/interfaces/service.interface';
-import type { ISelectUser } from '@/modules/user/infrastructure/schemas/user.schema';
+
+interface IInput {
+    email: string;
+}
 
 @Injectable()
-export class VerifiedVerificationMailerService implements IService<ISelectUser, void> {
+export class VerifiedVerificationMailerService implements IService<IInput, void> {
     constructor(private readonly mailerService: MailerService) {}
 
-    async execute(input: ISelectUser): Promise<void> {
-        const link = `${process.env.APP_URL}/authentication/login`;
-        const subject = 'Email verification was success';
-        const html = `
-            <div style="font-family: Arial, sans-serif;">
-                <p>Hello <strong>${input.email}</strong>,</p>
-                
-                <p>Your email has been verified please use the below link to enter to the app.</p>
-                
-                <p>
-                    Click here:<br/>
-                    <a href="${link}" style="font-weight: bold; color: #0066cc;">
-                        Login page
-                    </a>
-                </p>
-                
-                <p>Best regards,<br/>Your Team</p>
-            </div>
-        `;
-
+    async execute(input: IInput): Promise<void> {
         await this.mailerService.sendMail({
             to: input.email,
-            subject,
-            html,
+            subject: 'Email verification was success',
+            html: `
+                <div style="font-family: Arial, sans-serif;">
+                    <p>Hello <strong>${input.email}</strong>,</p>
+                    <p>Your email has been verified please use the below link to enter to the app.</p>
+                    <p>
+                        Click here:<br/>
+                        <a href="${process.env.APP_URL}/authentication/login" style="font-weight: bold; color: #0066cc;">
+                            Login page
+                        </a>
+                    </p>
+                    <p>Best regards,<br/>Your Team</p>
+                </div>
+            `,
         });
     }
 }
