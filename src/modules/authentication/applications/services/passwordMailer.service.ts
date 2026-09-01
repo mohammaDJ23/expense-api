62d3edac4fc7c8af69894631298d27dcb5e5febc
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-09-01 23:31:53 +0330

    chore: 142074 rename the path and service

--- src/modules/authentication/applications/services/passwordMailer.service.ts
+++ src/modules/authentication/applications/services/passwordMailer.service.ts
@@ -21,7 +21,7 @@ export class PasswordMailerService implements IService<IInput, void> {
                     <p>Hello <strong>${input.email}</strong>,</p>
                     <p>Thank you for registering! Please click on this link.</p>
                     <p>
-                        <a href="${process.env.APP_URL}/authentication/forgot-password/verification?token=${input.token}" style="font-weight: bold; color: #0066cc;">
+                        <a href="${process.env.APP_URL}/authentication/reset-password?token=${input.token}" style="font-weight: bold; color: #0066cc;">
                             Go to the forgot password page
                         </a>
                     </p>
