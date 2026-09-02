d59db5bbbfb08e01eee834b6d09db15bb13a5b62
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-09-02 23:05:02 +0330

    refactor: 142074 delete the cache when local signup was success

--- src/modules/authentication/authentication.enum.ts
+++ src/modules/authentication/authentication.enum.ts
@@ -1,5 +1,6 @@
 export enum AuthenticationResource {
     LOCAL_SIGNUP_INITIATION = 'local_signup_initiation',
+    LOCAL_SIGNUP = 'local_signup',
     LOCAL_RESET_PASSWORD = 'local_reset_password',
     LOCAL_FORGOT_PASSWORD = 'local_forgot_password',
 }
--- src/modules/outbox/domain/domain.constants.ts
+++ src/modules/outbox/domain/domain.constants.ts
@@ -19,4 +19,5 @@ export const OUTBOX_EVENT_ROUTES: TOutboxEventRoute[] = [
     'local_signup_initiation.created',
     'local_reset_password.created',
     'local_forgot_password.created',
+    'local_signup.created',
 ];
