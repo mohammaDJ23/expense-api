ae9ba5a6f1dfbc5450b928efca993b85f5b1c0b7
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-09-04 16:06:31 +0330

    chore: 062765 add the message events

---
+++ src/modules/location/domain/enums/locationMessageEvent.enum.ts
@@ -0,0 +1,5 @@
+export enum LocationMessageEvent {
+    CREATED_LOCATION = 'createdLocation',
+    UPDATED_LOCATION = 'updatedLocation',
+    DELETED_LOCATION = 'deletedLocation',
+}
---
+++ src/modules/receiver/domain/enums/receiverMessageEvent.enum.ts
@@ -0,0 +1,5 @@
+export enum ReceiverMessageEvent {
+    CREATED_RECEIVER = 'createdReceiver',
+    UPDATED_RECEIVER = 'updatedReceiver',
+    DELETED_RECEIVER = 'deletedReceiver',
+}
