93ff7d24358def3033230a48469e8ca3ef7eb3bf
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-09-04 19:53:17 +0330

    refactor: 062765 use new located resources

--- src/modules/search/infrastructure/elasticsearch/deleteDocsByUserIdsElasticsearch.query.ts
+++ src/modules/search/infrastructure/elasticsearch/deleteDocsByUserIdsElasticsearch.query.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common';
 
-import { BillResource } from '@/modules/bill/bill.enum';
-import { ConsumerResource } from '@/modules/consumer/consumer.enum';
-import { LocationResource } from '@/modules/location/location.enum';
-import { ReceiverResource } from '@/modules/receiver/receiver.enum';
+import { BillResource } from '@/modules/bill/domain/enums/bill.enum';
+import { ConsumerResource } from '@/modules/consumer/domain/enums/consumer.enum';
+import { LocationResource } from '@/modules/location/domain/enums/location.enum';
+import { ReceiverResource } from '@/modules/receiver/domain/enums/receiver.enum';
 
 import type { IElasticsearchQuery } from '@/infrastructure/elasticsearch/elasticsearchQuery.interface';
 import type { estypes } from '@elastic/elasticsearch';
