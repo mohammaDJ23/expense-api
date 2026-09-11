3cbbe761bba9b661c654bbd76e183da8c8309ab4
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-09-11 22:35:26 +0330

    perf: 106976 split the backup of db into another service

--- scripts/database/backup.sh
+++ scripts/database/backup.sh
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-export PGPASSWORD="$(cat /run/secrets/database_password)"
+wal-g backup-push /var/lib/postgresql/data
 
-wal-g backup-push /var/lib/postgresql/data && \
-    wal-g delete retain FIND_FULL 2 --confirm
+wal-g delete retain FIND_FULL 2 --confirm
