f25a3d0c1766a3232d2eca554c81eb2932fbecdf
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-09-11 22:35:26 +0330

    perf: 106976 split the backup of db into another service

--- scripts/database/backupEntrypoint.sh
+++ scripts/database/backupEntrypoint.sh
@@ -6,4 +6,4 @@ export PGPASSWORD="$(cat /run/secrets/database_password)"
 export AWS_ACCESS_KEY_ID="$(cat /run/secrets/arvan_access_key)"
 export AWS_SECRET_ACCESS_KEY="$(cat /run/secrets/arvan_secret_key)"
 
-exec /usr/local/bin/docker-entrypoint.sh "$@"
+exec "$@"
