8e8a0302150868e986995c9d2b183778f28a1b4f
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-09-16 22:09:59 +0330

    chore: 106976 remove unused scripts related to app running

---
+++ scripts/app/ciEntrypoint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source ./scripts/dockerComposeCommand.sh
+
+docker_compose \
+    -f docker-compose.production.yml \ 
+    -f docker-compose.ci.yml \
+    up \
+    -d \
+    --wait \
+    --wait-timeout 240
