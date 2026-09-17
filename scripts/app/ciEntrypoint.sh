f28c31a237687234f0a56d7491d13e34e65aae07
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-09-17 09:02:10 +0330

    chore: 106976 remove extra spaces

--- scripts/app/ciEntrypoint.sh
+++ scripts/app/ciEntrypoint.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 source ./scripts/dockerComposeCommand.sh
 
 docker_compose \
-    -f docker-compose.production.yml \ 
+    -f docker-compose.production.yml \
     -f docker-compose.ci.yml \
     up \
     --build \
