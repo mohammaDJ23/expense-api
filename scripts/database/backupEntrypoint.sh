5de5261ea8d714d8993229b89657a8e5e7d32e29
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-09-18 10:23:28 +0330

    chore: 106976 connect to postgres

--- scripts/database/backupEntrypoint.sh
+++ scripts/database/backupEntrypoint.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 
+export PGPASSWORD="$(cat /run/secrets/database_password)"
 export AWS_ACCESS_KEY_ID="$(cat /run/secrets/arvan_access_key)"
 export AWS_SECRET_ACCESS_KEY="$(cat /run/secrets/arvan_secret_key)"
 
