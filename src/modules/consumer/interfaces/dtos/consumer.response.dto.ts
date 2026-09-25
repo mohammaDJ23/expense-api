ae757839f3ee08c2957bcb594df956501eb999b2
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-09-25 21:46:42 +0330

    chore: 989202 add the api property of consumer dtos

--- src/modules/consumer/interfaces/dtos/consumer.response.dto.ts
+++ src/modules/consumer/interfaces/dtos/consumer.response.dto.ts
@@ -1,15 +1,32 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Expose } from 'class-transformer';
 
 export class ConsumerResponseDto {
     @Expose()
+    @ApiProperty({
+        type: 'string',
+        example: '9f466902-0a1d-4a24-b7bb-890434bc5adb',
+    })
     id: string;
 
     @Expose()
+    @ApiProperty({
+        type: 'string',
+        example: 'Mr test',
+    })
     name: string;
 
     @Expose()
+    @ApiProperty({
+        type: 'string',
+        example: '2026-09-25T08:16:10.528Z',
+    })
     createdAt: string;
 
     @Expose()
+    @ApiProperty({
+        type: 'string',
+        example: '2026-09-25T08:16:10.528Z',
+    })
     updatedAt: string;
 }
--- src/modules/consumer/interfaces/dtos/findConsumerList.response.dto.ts
+++ src/modules/consumer/interfaces/dtos/findConsumerList.response.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Expose, Type } from 'class-transformer';
 
 import { ConsumerResponseDto } from './consumer.response.dto';
@@ -5,14 +6,31 @@ import { ConsumerResponseDto } from './consumer.response.dto';
 export class FindConsumerListResponseDto {
     @Type(() => ConsumerResponseDto)
     @Expose()
+    @ApiProperty({
+        type: ConsumerResponseDto,
+        isArray: true,
+    })
     items: ConsumerResponseDto[];
 
     @Expose()
+    @ApiProperty({
+        type: 'number',
+        example: 10,
+    })
     total: number;
 
     @Expose()
+    @ApiProperty({
+        type: 'boolean',
+        example: true,
+    })
     hasNextPage: boolean;
 
     @Expose()
+    @ApiProperty({
+        type: 'string',
+        example: 'e25hbWU6ICJkZGQifQ==',
+        nullable: true,
+    })
     nextCursor: string | null;
 }
