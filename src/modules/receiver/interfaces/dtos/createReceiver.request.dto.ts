85f8d87da45b08262678bffd76a1174729da584d
Mohammad Nowresideh <mohammad.ha9bi8@gmail.com>
2026-09-26 08:13:11 +0330

    chore: 989202 add the dto's swagger decorators

--- src/modules/receiver/interfaces/dtos/createReceiver.request.dto.ts
+++ src/modules/receiver/interfaces/dtos/createReceiver.request.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, Length, Matches } from 'class-validator';
 
 export class CreateReceiverRequestDto {
@@ -5,5 +6,9 @@ export class CreateReceiverRequestDto {
     @Length(3, 50)
     // eslint-disable-next-line security/detect-unsafe-regex
     @Matches(/^[a-zA-Z_]+( [a-zA-Z_]+)*$/, { message: 'Invalid receiver' })
+    @ApiProperty({
+        type: 'string',
+        example: 'Mr test',
+    })
     name: string;
 }
--- src/modules/receiver/interfaces/dtos/findReceiverList.response.dto.ts
+++ src/modules/receiver/interfaces/dtos/findReceiverList.response.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Expose, Type } from 'class-transformer';
 
 import { ReceiverResponseDto } from './receiver.response.dto';
@@ -5,14 +6,31 @@ import { ReceiverResponseDto } from './receiver.response.dto';
 export class FindReceiverListResponseDto {
     @Type(() => ReceiverResponseDto)
     @Expose()
+    @ApiProperty({
+        type: ReceiverResponseDto,
+        isArray: true,
+    })
     items: ReceiverResponseDto[];
 
     @Expose()
+    @ApiProperty({
+        type: 'number',
+        example: 10,
+    })
     total: number;
 
     @Expose()
+    @ApiProperty({
+        type: 'boolean',
+        example: true,
+    })
     hasNextPage: boolean;
 
     @Expose()
+    @ApiProperty({
+        type: 'string',
+        example: 'e25hbWU6ICJkZGQifQ==',
+        nullable: true,
+    })
     nextCursor: string | null;
 }
